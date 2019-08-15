ce8529a1c92eddec0b71749991a025f0d4b39461
Roman Lutz <rolutz@microsoft.com>
2019-08-15 17:34:50 -0400

    initial files

--- fairlearn/post_processing/roc_curve_based_post_processing.py
+++ fairlearn/post_processing/roc_curve_based_post_processing.py
@@ -1,2 +1,237 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
+
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.colors
+
+tab10_norm  = matplotlib.colors.Normalize(vmin=0, vmax=7)
+tab10_scalarMap = cm.ScalarMappable(norm=tab10_norm, cmap='Dark2')
+debug_colors = [tab10_scalarMap.to_rgba(x) for x in range(10)]
+debug_ncolors = len(debug_colors)
+debug_colormap = {}
+
+debug_markers = "^vso<>"
+debug_nmarkers = len(debug_markers)
+debug_markermap = {}
+
+highlight_color = [0.95, 0.90, 0.40]
+
+def debug_marker(key):
+    if key not in debug_markermap:
+        marker = debug_markers[len(debug_markermap) % debug_nmarkers]
+        debug_markermap[key] = marker
+    return debug_markermap[key]
+
+def debug_has_marker(key):
+    return key in debug_markermap
+
+def debug_color(key):
+    if key not in debug_colormap:
+        color = debug_colors[len(debug_colormap) % debug_ncolors]
+        debug_colormap[key] = color
+    return debug_colormap[key]
+
+def debug_has_color(key):
+    return key in debug_colormap
+
+def pred_from_op(op, th):
+    """ encodes the threshold rule P_hat > t or P_hat < t
+    """
+    if op=='>':
+        return lambda x: x>th
+    elif op=='<':
+        return lambda x: x<th
+    else:
+        assert False, "Unrecognized operation:" +op
+
+def interpolated_pred(p_ignore, pred_const, p0, op0, p1, op1):
+    pred0 = pred_from_op(*op0)
+    pred1 = pred_from_op(*op1)
+    return (lambda x :
+        p_ignore*pred_const + (1-p_ignore)*(p0*pred0(x) + p1*pred1(x))
+        )
+
+def interpolate_curve(data, x_col, y_col, content_col, x_grid):
+    """Interpolates the data frame in "data" along the values in "x_grid".
+
+    Assumes: (1) data[y_col] is convex and non-decreasing in data[x_col]
+             (2) min and max in x_grid are below/above min and max in data[x_col]
+             (3) data is indexed 0,...,len(data)"""
+
+    data_tr = data.transpose()
+
+    i = 0
+    dict_list = []
+    x0 = data_tr[0][x_col]
+    while data_tr[i+1][x_col] == x0:
+        i += 1
+    for x in x_grid:
+        while x>data_tr[i+1][x_col]:
+            i += 1
+        p0 = (data_tr[i+1][x_col]-x)/(data_tr[i+1][x_col]-data_tr[i][x_col])
+        p1 = 1-p0
+        y = p0*data_tr[i][y_col] + p1*data_tr[i+1][y_col]
+        dict_list.append(
+            {x_col: x, y_col: y,
+             'p0': p0, content_col+'0': data_tr[i  ][content_col],
+             'p1': p1, content_col+'1': data_tr[i+1][content_col]})
+
+    return pd.DataFrame( dict_list )[[x_col, y_col, 'p0', content_col+'0', 'p1', content_col+'1']]
+
+def get_roc(data, x_grid, flip=True, debug=False, attr=None):
+    """Get ROC curve based on data columns 'score' and 'label'"""
+
+    attr_str = "attribute "+str(attr)
+    if debug:
+        color = debug_color(attr_str)
+            
+    data_sorted = data.sort_values(by='score', ascending=False)
+
+    scores = list(data_sorted['score'])
+    labels = list(data_sorted['label'])
+
+    n = len(labels)
+    npos = sum(labels)
+    nneg = n-npos
+
+    assert (npos>0) & (nneg>0), "Degenerate labels for "+attr_str
+    
+    scores.append(-np.inf)
+    labels.append( np.nan)
+    
+    x_list, y_list, op_list = [0], [0], [('>',np.inf)]
+    
+    i = 0
+    count = [0, 0]
+    while i<n:
+        th = scores[i]
+        while scores[i]==th:
+            count[labels[i]] += 1
+            i += 1
+        x, y = count[0]/nneg, count[1]/npos
+        th = (th+scores[i])/2
+        op = ('>', th)
+
+        # if flipping labels gives better accuracy try flipping
+        if flip & (x > y):
+            x, y = 1-x, 1-y
+            op = ('<', th)
+        x_list.append(x)
+        y_list.append(y)
+        op_list.append(op)
+    
+    roc_raw = pd.DataFrame({'x': x_list, 'y': y_list, 'op': op_list})
+    
+    roc_sorted = roc_raw.sort_values(by=['x', 'y'])
+    selected = []
+    for r2 in roc_sorted.itertuples():
+        while len(selected)>=2:
+            r1 = selected[-1]
+            r0 = selected[-2]
+            if (r1.y-r0.y)*(r2.x-r0.x) <= (r2.y-r0.y)*(r1.x-r0.x):
+                selected.pop()
+            else:
+                break
+        selected.append(r2)
+
+    roc_conv = pd.DataFrame(selected)[['x','y','op']]
+
+    roc_conv['sel'] = (nneg/n)*roc_conv['x'] + (npos/n)*roc_conv['y']
+    roc_conv['err'] = (nneg/n)*roc_conv['x'] + (npos/n)*(1-roc_conv['y'])
+    
+    roc_interp = interpolate_curve(roc_conv, 'x', 'y', 'op', x_grid)
+    sel_interp = interpolate_curve(roc_conv, 'sel', 'err', 'op', x_grid)
+
+    if debug:
+        print("")
+        print("#"*65)
+        print("")
+        print("-"*65)
+        print("Processing "+attr_str)
+        print("-"*65)
+        print("DATA")
+        print(data)
+        print("\nROC curve: initial")
+        #print(roc_raw)
+        print(roc_sorted)
+        print("\nROC curve: convex")
+        print(roc_conv)
+        print("\nROC curve: interpolated [just top]")
+        print(roc_interp.head())
+        plt.plot(roc_sorted['x'], roc_sorted['y'], c=color, ls='--', lw=1.0, label='_')
+        plt.plot(roc_conv['x'], roc_conv['y'], c=color, ls='-', lw=2.0, label='attribute '+str(attr))
+        
+    return roc_interp, sel_interp
+        
+def equalized_odds(attrs, labels, scores, flip=True, debug=False, gridsize=1000):
+    """ TODO: doc
+    flip means we consider negative weights as well
+    task: come up with threshold that turns scores into (good) labels
+    """
+    data = pd.DataFrame({'attr': attrs, 'score': scores, 'label': labels})
+    n = len(labels)
+    npos = sum(labels)
+    nneg = n-npos
+    assert n>0, "Empty dataset"
+
+    grouped = data.groupby('attr')
+    roc = {}
+    sel = {}
+    x_grid= np.linspace(0, 1, gridsize+1)
+    y_vals = pd.DataFrame()
+    err_given_sel = 0*x_grid
+    for attr, group in grouped:
+        p_attr = len(group)/n
+        roc[attr], sel[attr] = get_roc(group, x_grid, flip=flip, debug=debug, attr=attr)
+        y_vals[attr] = roc[attr]['y']
+        err_given_sel += p_attr * sel[attr]['err']
+
+    # EQUALIZED ODDS
+    y_min       = np.amin(y_vals, axis=1)
+    # conditional probabilities represented as x -> P[Y_hat=1 | Y=0]
+    # and                                      y -> P[Y_hat=1 | Y=1]
+    err_given_x = (nneg/n) * x_grid + (npos/n) * (1-y_min)
+    i_best_EO   = err_given_x.idxmin()
+    x_best = x_grid[i_best_EO]
+    y_best = y_min[i_best_EO]
+
+    # DEMOGRAPHIC PARITY
+    # TODO: separate this out of this method, or at least rename the method
+    i_best_DP = err_given_sel.idxmin()
+    sel_best  = x_grid[i_best_DP]
+    
+    # create the solution as interpolation of multiple points with a separate predictor per protected attribute
+    pred_EO_by_attr = {}
+    pred_DP_by_attr = {}
+    for attr in roc.keys():
+        r = roc[attr].transpose()[i_best_EO]
+        # p_ignore is the probability at which we're ignoring the score, i.e. on the diagonal of the ROC curve
+        if r.y==r.x:
+            p_ignore = 0
+        else:
+            p_ignore = (r.y-y_best)/(r.y-r.x)
+        pred_EO_by_attr[attr] = interpolated_pred(p_ignore, x_best, r.p0, r.op0, r.p1, r.op1)
+
+        # for DP we already have the predictor directly without complex interpolation. no p_ignore
+        r = sel[attr].transpose()[i_best_DP]
+        pred_DP_by_attr[attr] = interpolated_pred(0, 0, r.p0, r.op0, r.p1, r.op1)
+
+    pred_EO = lambda a,x : pred_EO_by_attr[a](x)
+    pred_DP = lambda a,x : pred_DP_by_attr[a](x)    
+
+    if debug:
+        print("-"*65)
+        print("From ROC curves")
+        print("Best EO: error=%.3f, FP rate=%.3f, TP rate=%.3f" % (err_given_x[i_best_EO], x_best, y_best) )
+        print("Best DP: error=%.3f, selection rate=%.3f" % (err_given_sel[i_best_DP], sel_best) )
+        print("-"*65)
+        line, = plt.plot(x_grid, y_min, color=highlight_color, lw=8, label='overlap')
+        line.zorder -= 1
+        plt.plot(x_best, y_best, 'm*', ms=10, label='EO solution') 
+        plt.legend()
+
+    return pred_EO, pred_DP
--- tests/test_post_processing.py
+++ tests/test_post_processing.py
@@ -1,2 +1,53 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
+
+from fairlearn.post_processing.roc_curve_based_post_processing import equalized_odds
+
+ex_attrs1 = [x      for x in 'AAAAAAA' 'BBBBBBB' 'CCCCCC']
+ex_attrs2 = [x      for x in 'xxxYYYY' 'xYYYYYx' 'YYYYYY']
+ex_labels = [int(x) for x in '0110100' '0010111' '000111']
+ex_scores = [int(x) for x in '0011233' '0001111' '011112']
+
+
+def test(test_id=1):
+    print("STARTING TEST")
+    if test_id==1:
+        ex_attrs, flip = ex_attrs1, True
+    elif test_id==2:
+        ex_attrs, flip = list(zip(ex_attrs1, ex_attrs2)), True
+    elif test_id==3:
+        ex_attrs, flip = ex_attrs1, False
+    elif test_id==4:
+        ex_attrs, flip = ex_attrs2, False
+    
+    pred_EO, pred_DP = equalized_odds(ex_attrs, ex_labels, ex_scores, debug=True, flip=flip)
+    ex_preds_EO = []
+    ex_preds_DP = []
+    for i in range(len(ex_attrs)):
+        ex_preds_EO.append( pred_EO(ex_attrs[i], ex_scores[i]) )
+        ex_preds_DP.append( pred_DP(ex_attrs[i], ex_scores[i]) )
+    ex_data = pd.DataFrame({'attr': ex_attrs, 'score': ex_scores, 'label': ex_labels, 'pred_EO': ex_preds_EO, 'pred_DP': ex_preds_DP})
+    ex_data['error_EO'] = np.absolute(ex_data['label']-ex_data['pred_EO'])
+    ex_data['error_DP'] = np.absolute(ex_data['label']-ex_data['pred_DP'])
+    #print("DATA")
+    #print( ex_data[['attr','label','pred']] )
+    print("APPLYING EO PREDICTOR")
+    print("")
+    print(ex_data.groupby( ['attr','label'] ).mean()[['pred_EO']] )
+    print("")
+    print("error_EO=%.3f" % ex_data['error_EO'].mean() )
+
+    print("-"*65)
+
+    print("APPLYING DP PREDICTOR")
+    print("")
+    print(ex_data.groupby( ['attr'] ).mean()[['pred_DP']] )
+    print("")
+    print("error_DP=%.3f" % ex_data['error_DP'].mean() )
+    plt.show()
+
+if __name__ == '__main__':
+    test(1)
+    test(2)
+    test(3)
+    test(4)
