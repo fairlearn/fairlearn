9acecae07d4560b1b8f428e26b18a68a3e76772d
Richard Edgar <riedgar@microsoft.com>
2019-08-15 12:21:34 -0400

    Adding checks into the main grid search algorithm

--- fairlearn/grid_search.py
+++ fairlearn/grid_search.py
@@ -34,24 +34,44 @@ def _generate_p0_p1(Y):
 
     return p0, p1
 
-def grid_search_classification_binary_protected_1d(learner, X, Y, A):
+def classification_binary_protected_1d(learner, X, Y, A, Ls=None, num_Ls = 11):
     """Function to generate a 1d set of models for a classification problem with
     a single binary protected attribute
-
     """
+    # Must specify Ls or num_Ls
+    if not( Ls is None ^ num_Ls is None):
+        raise RuntimeError("Must specify either Ls or num_Ls")
 
     # Check that X, Y and A have the same number of rows
-
     # Check that Y and A are both 1d vectors
+    if len(Y.shape) != 1:
+        raise RuntimeError("Supplied Y not 1d vector")
+
+    if len(A.shape) != 1:
+        raise RuntimeError("Supplied A not 1d vector")
+
+    if A.shape[0] != Y.shape[0]:
+        raise RuntimeError("Supplied A and Y not same length")
+
+    if X.shape[0] != Y.shape[0]:
+        raise RuntimeError("Supplied X and Y do not have same number of rows")
 
     # Check that A only has values 0 and 1
-    
+    uniqueA = np.unique(A)
+    if not np.array_equal(uniqueA, [0,1]):
+        raise RuntimeError("Supplied A labels not 0 or 1")
+
     # Compute p0 and p1
     # This will also check that Y is binary with values
     # 0 and 1
     p0, p1 = _generate_p0_p1(Y)
 
     # If not supplied, generate array of trial Lagrangian multipliers
+    if Ls is None:
+        limit = 1
+        if p0/p1 > 1:
+            limit = p0/p1
+        Ls = np.linspace(-2*limit, 2*limit, num_Ls)
 
     for L in Ls:
         # Generate W array
