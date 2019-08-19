74556cbfc7e1af2a0b43736eed66f653c8dbf18f
Richard Edgar <riedgar@microsoft.com>
2019-08-19 15:43:39 -0400

    Ensure X, Y and A can be lists, numpy arrays or pandas Series/DataFrames

--- test/test_grid_search.py
+++ test/test_grid_search.py
@@ -11,16 +11,63 @@ import simple_learners
 import pytest
 
 class TestGridSearch:
-    def _quick_data(self, number_samples=32):
-        # Making the default number of samples a power of two helps
-        # ensure that some things work out exactly later
+    def _quick_data(self, number_samples=8):
         feature_1 = np.random.randint(2, size=number_samples)
         feature_2 = np.random.randint(6, size=number_samples)
         feature_3 = np.random.randint(3, size=number_samples)
-        X = pd.DataFrame({"feature_1": feature_1, "feature_2": feature_2, "feature_3": feature_3})
-        Y = np.random.randint(2, size=number_samples)
-        A = np.random.randint(2, size=number_samples)
+        X = np.stack((feature_1, feature_2, feature_3), -1).tolist() # pd.DataFrame({"feature_1": feature_1, "feature_2": feature_2, "feature_3": feature_3})
+        Y = np.random.randint(2, size=number_samples).tolist()
+        A = np.random.randint(2, size=number_samples).tolist()
         return X, Y, A
+
+    def _smoke_core(self, X, Y, A):
+        target = gs.BinaryClassificationGridSearch()
+        result = target.grid_search_binary_protected_attribute(simple_learners.LeastSquaresBinaryClassifierLearner(), X, Y, A, number_lagrange_multipliers=11)
+        assert len(result) == 11
+
+        # 'Central' multiplier of generated set should be 0
+        # In practice, we might get a small error
+        assert abs(result[5]["lagrange_multiplier"]) < 1e-15
+
+        # Generated multipliers should be unique and in ascending order
+        multipliers = [x["lagrange_multiplier"] for x in result]
+        assert len(np.unique(multipliers)) == len(result)
+        assert sorted(multipliers)
+
+    
+    def test_grid_smoke(self):
+        X, Y, A = self._quick_data()
+        self._smoke_core(X, Y, A)
+
+    def test_grid_smoke_Y_numpy(self):
+        X, Y, A = self._quick_data()
+        Y_numpy = np.array(Y)
+        self._smoke_core(X, Y_numpy, A)
+
+    def test_grid_smoke_Y_pandas(self):
+        X, Y, A = self._quick_data()
+        Y_pandas = pd.Series(Y)
+        self._smoke_core(X, Y_pandas, A)
+
+    def test_grid_smoke_A_numpy(self):
+        X, Y, A = self._quick_data()
+        A_numpy = np.array(A)
+        self._smoke_core(X, Y, A_numpy)
+
+    def test_grid_smoke_A_pandas(self):
+        X, Y, A = self._quick_data()
+        A_pandas = pd.Series(A)
+        self._smoke_core(X, Y, A_pandas)
+
+    def test_grid_smoke_X_numpy(self):
+        X, Y, A = self._quick_data()
+        X_numpy = np.array(X)
+        self._smoke_core(X_numpy, Y, A)
+
+    def test_grid_smoke_X_pandas(self):
+        X, Y, A = self._quick_data()
+        X_pandas = pd.DataFrame(X, columns=["feature_1", "feature_2", "feature_3"])
+        self._smoke_core(X_pandas, Y, A)
 
     def test_generate_p0_p1_smoke(self):
         number_labels = 32
@@ -47,23 +94,6 @@ class TestGridSearch:
         W = target._generate_weights(Y, A, L, p_ratio)
 
         assert np.array_equal(W_expect, W), str(W)+str(W_expect)
-
-    def test_grid_smoke(self):
-        X, Y, A = self._quick_data()
-        target = gs.BinaryClassificationGridSearch()
-        result = target.grid_search_binary_protected_attribute(simple_learners.LeastSquaresBinaryClassifierLearner(), X, Y, A, number_lagrange_multipliers=11)
-        assert len(result) == 11
-
-        # 'Central' multiplier of generated set should be 0
-        # In practice, the generated value might not be exactly zero
-        # but having the number of samples equal to a power of two should
-        # Allow this equality
-        assert result[5]["lagrange_multiplier"] == 0
-
-        # Generated multipliers should be unique and in ascending order
-        multipliers = [x["lagrange_multiplier"] for x in result]
-        assert len(np.unique(multipliers)) == len(result)
-        assert sorted(multipliers)
 
     def test_grid_bad_lagrange_multiplier_specification(self):
         X, Y, A = self._quick_data()
