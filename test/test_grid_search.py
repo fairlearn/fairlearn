350b896c0c0fef1ae5c36c37f7c56638ec71077c
Richard Edgar <riedgar@microsoft.com>
2019-08-19 14:51:49 -0400

    Various small test improvements

--- test/test_grid_search.py
+++ test/test_grid_search.py
@@ -11,24 +11,27 @@ import simple_learners
 import pytest
 
 class TestGridSearch:
-    def _quick_data(self):
-        feature_1 = [int(x) for x in '0110101' '0111101' '001011']
-        feature_2 = [int(x) for x in '0000100' '0000011' '111111']
-        feature_3 = [int(x) for x in '1111111' '1111111' '111111']
+    def _quick_data(self, number_samples=32):
+        # Making the default number of samples a power of two helps
+        # ensure that some things work out exactly later
+        feature_1 = np.random.randint(2, size=number_samples)
+        feature_2 = np.random.randint(6, size=number_samples)
+        feature_3 = np.random.randint(3, size=number_samples)
         X = pd.DataFrame({"feature_1": feature_1, "feature_2": feature_2, "feature_3": feature_3})
-        Y = pd.Series([int(x) for x in '0110100' '0010111' '001111'])
-        A = pd.Series([int(x) for x in '0000000' '1111111' '010101'])
+        Y = np.random.randint(2, size=number_samples)
+        A = np.random.randint(2, size=number_samples)
         return X, Y, A
 
     def test_generate_p0_p1_smoke(self):
-        Y = [int(x) for x in '0110101' '0111101' '001011']
+        number_labels = 32
+        Y = np.random.randint(2, size=number_labels)
 
         target = gs.BinaryClassificationGridSearch()
 
         p0, p1 = target._generate_p0_p1(Y)
 
-        assert p0 == 8/20
-        assert p1 == 12/20
+        assert p0 == 1 - sum(Y) / number_labels
+        assert p1 == sum(Y) / number_labels
 
     def test_generate_weights(self):
         # Set up sample data
@@ -50,7 +53,14 @@ class TestGridSearch:
         target = gs.BinaryClassificationGridSearch()
         result = target.grid_search_binary_protected_attribute(simple_learners.LeastSquaresBinaryClassifierLearner(), X, Y, A, number_lagrange_multipliers=11)
         assert len(result) == 11
+
+        # 'Central' multiplier of generated set should be 0
+        # In practice, the generated value might not be exactly zero
+        # but having the number of samples equal to a power of two should
+        # Allow this equality
         assert result[5]["lagrange_multiplier"] == 0
+
+        # Generated multipliers should be unique and in ascending order
         multipliers = [x["lagrange_multiplier"] for x in result]
         assert len(np.unique(multipliers)) == len(result)
         assert sorted(multipliers)
@@ -69,7 +79,7 @@ class TestGridSearch:
         X, Y, _ = self._quick_data()
         message = r"Supplied protected_attribute labels not 0 or 1"
 
-        bad_protected_attribute = pd.Series([int(x) for x in '0000000' '2222222' '020202'])
+        bad_protected_attribute = [0, 2, 2]
 
         target = gs.BinaryClassificationGridSearch()
         with pytest.raises(RuntimeError, match=message):
