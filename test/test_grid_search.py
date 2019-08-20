36e16687ef041b9e971b235442d12e931bdfd650
Richard Edgar <riedgar@microsoft.com>
2019-08-20 14:31:55 -0400

    Fixes for flake8

--- test/test_grid_search.py
+++ test/test_grid_search.py
@@ -16,7 +16,6 @@ class TestBinaryProtectedAttribute:
         feature_1 = np.random.randint(2, size=number_samples)
         feature_2 = np.random.randint(6, size=number_samples)
         feature_3 = np.random.randint(3, size=number_samples)
-        # pd.DataFrame({"feature_1": feature_1, "feature_2": feature_2, "feature_3": feature_3})
         X = np.stack((feature_1, feature_2, feature_3), -1).tolist()
         Y = np.random.randint(2, size=number_samples).tolist()
         A = np.random.randint(2, size=number_samples).tolist()
@@ -25,7 +24,9 @@ class TestBinaryProtectedAttribute:
     def _smoke_core(self, X, Y, A):
         target = gs.BinaryProtectedAttribute()
         result = target.demographic_parity_binary_classification(
-            simple_learners.LeastSquaresBinaryClassifierLearner(), X, Y, A, number_lagrange_multipliers=11)
+            simple_learners.LeastSquaresBinaryClassifierLearner(),
+            X, Y, A,
+            number_lagrange_multipliers=11)
         assert len(result) == 11
 
         # 'Central' multiplier of generated set should be 0
@@ -121,15 +122,20 @@ class TestBinaryProtectedAttribute:
 
     def test_grid_bad_lagrange_multiplier_specification(self):
         X, Y, A = self._quick_data()
-        message = r"Must specify either lagrange_multipliers or number_lagrange_multipliers"
+        message = str("Must specify either lagrange_multipliers "
+                      "or number_lagrange_multipliers")
 
         target = gs.BinaryProtectedAttribute()
         with pytest.raises(RuntimeError, match=message):
             _ = target.demographic_parity_binary_classification(
-                simple_learners.LeastSquaresBinaryClassifierLearner(), X, Y, A, None, None)
+                simple_learners.LeastSquaresBinaryClassifierLearner(),
+                X, Y, A,
+                None, None)
         with pytest.raises(RuntimeError, match=message):
             _ = target.demographic_parity_binary_classification(
-                simple_learners.LeastSquaresBinaryClassifierLearner(), X, Y, A, np.random.randint(10, size=3), 3)
+                simple_learners.LeastSquaresBinaryClassifierLearner(),
+                X, Y, A,
+                np.random.randint(10, size=3), 3)
 
     def test_non_binary_protected_attribute(self):
         X, Y, _ = self._quick_data()
@@ -140,7 +146,8 @@ class TestBinaryProtectedAttribute:
         target = gs.BinaryProtectedAttribute()
         with pytest.raises(RuntimeError, match=message):
             _ = target.demographic_parity_binary_classification(
-                simple_learners.LeastSquaresBinaryClassifierLearner(), X, Y, bad_protected_attribute)
+                simple_learners.LeastSquaresBinaryClassifierLearner(),
+                X, Y, bad_protected_attribute)
 
     def test_non_binary_labels(self):
         X, _, A = self._quick_data(8)
@@ -150,7 +157,8 @@ class TestBinaryProtectedAttribute:
         target = gs.BinaryProtectedAttribute()
         with pytest.raises(RuntimeError, match=message):
             _ = target.demographic_parity_binary_classification(
-                simple_learners.LeastSquaresBinaryClassifierLearner(), X, bad_labels, A)
+                simple_learners.LeastSquaresBinaryClassifierLearner(),
+                X, bad_labels, A)
 
     def test_labels_not_0_1(self):
         X, _, A = self._quick_data(8)
@@ -160,11 +168,13 @@ class TestBinaryProtectedAttribute:
         target = gs.BinaryProtectedAttribute()
         with pytest.raises(RuntimeError, match=message):
             _ = target.demographic_parity_binary_classification(
-                simple_learners.LeastSquaresBinaryClassifierLearner(), X, bad_labels, A)
+                simple_learners.LeastSquaresBinaryClassifierLearner(),
+                X, bad_labels, A)
 
     def test_lagrange_multiplier_generation(self):
-        # Check that the Lagrange multiplier values are being generated for the correct
-        # range. We do not do full checks on np.linspace
+        # Check that the Lagrange multiplier values are being
+        # generated for the correct range. We do not do full checks
+        # on np.linspace
         X, Y, A = self._quick_data(32)
 
         # Reimplement the multiplier computation
@@ -176,7 +186,9 @@ class TestBinaryProtectedAttribute:
 
         target = gs.BinaryProtectedAttribute()
         results = target.demographic_parity_binary_classification(
-            simple_learners.LeastSquaresBinaryClassifierLearner(), X, Y, A, number_lagrange_multipliers=3)
+            simple_learners.LeastSquaresBinaryClassifierLearner(),
+            X, Y, A,
+            number_lagrange_multipliers=3)
 
         expected_multipliers = [-2 * limit, 0, 2 * limit]
         actual_multipliers = [r["lagrange_multiplier"] for r in results]
@@ -197,36 +209,45 @@ class TestBinaryProtectedAttribute:
         assert a_offset == int(a_offset)
         assert a_scale == int(a_scale)
 
-        # Create a 'rigged' dataset which while it contains the protected attribute, actually doesn't depend on it
+        # Create a 'rigged' dataset which while it contains the protected
+        # attribute, actually doesn't depend on it
 
         # Attribute array is evenly divided between two values
         A_raw = pd.Series(np.concatenate(
-            (np.zeros(num_samples_each), np.ones(num_samples_each)), axis=None))
+            (np.zeros(num_samples_each), np.ones(num_samples_each)),
+            axis=None))
         A = a_scale * A_raw + a_offset
 
         # Generate a set of scores which we threshold to get the label
         scores = np.linspace(0, 1, num_samples_each)
 
-        # Need two copies of the scores, one for each value of the protected features
+        # Need two copies of the scores, one for each value of the protected
+        # features
         actual_feature = pd.Series(np.concatenate((scores, scores), axis=None))
 
         # Generate the labels by thresholding
         Y = pd.Series([x > 0.5 for x in actual_feature])
 
         # Assemble the dataframe of samples
-        # Need a extra 'ones' column to allow our simple least squares learner to work properly
+        # Need a extra 'ones' column to allow our simple least squares
+        # learner to work properly
         X = pd.DataFrame({"actual_feature": actual_feature,
-                          "protected_attribute_feature": A, "constant_ones_feature": np.ones(len(Y))})
+                          "protected_attribute_feature": A,
+                          "constant_ones_feature": np.ones(len(Y))})
 
         target = gs.BinaryProtectedAttribute()
         result = target.demographic_parity_binary_classification(
-            simple_learners.LeastSquaresBinaryClassifierLearner(), X, Y, A, number_lagrange_multipliers=5)
+            simple_learners.LeastSquaresBinaryClassifierLearner(),
+            X, Y, A, number_lagrange_multipliers=5)
         assert len(result) == 5
 
-        # Check the weights for each of the models returned against the expected values
-        # Note that the 'middle' value in each array corresponds to the Lagrange multiplier being zero
+        # Check the weights for each of the models returned against
+        # the expected values
+        # Note that the 'middle' value in each array corresponds to the
+        # Lagrange multiplier being zero
         expected_actual_feature_weights = [0, 0, 1.5, 0, 0]
-        expected_protected_attribute_feature_weights = np.array([-1, -1, 0, 1, 1]) / a_scale
+        expected_protected_attribute_feature_weights = np.array(
+            [-1, -1, 0, 1, 1]) / a_scale
         expected_constant_ones_feature_weights = [1, 1, -0.25, 0, 0]
         result_actual_feature_weights = [
             x["model"].weights["actual_feature"] for x in result]
@@ -236,12 +257,18 @@ class TestBinaryProtectedAttribute:
             x["model"].weights["constant_ones_feature"] for x in result]
 
         assert np.allclose(expected_actual_feature_weights,
-                           result_actual_feature_weights, rtol=1e-3)
+                           result_actual_feature_weights,
+                           rtol=1e-3)
         assert np.allclose(expected_protected_attribute_feature_weights,
-                           result_protected_attribute_feature_weights, rtol=1e-3)
+                           result_protected_attribute_feature_weights,
+                           rtol=1e-3)
 
         # Have not quite figured out functional form for constant_ones feature
-        assert expected_constant_ones_feature_weights[2] == pytest.approx(result_constant_ones_feature_weights[2], rel=1e-3)
-        if a_scale==1 and a_offset==0:
-            # Do know what the unrescaled constant_ones feature weight should be
-            assert np.allclose(expected_constant_ones_feature_weights, result_constant_ones_feature_weights)
+        assert expected_constant_ones_feature_weights[2] == pytest.approx(
+            result_constant_ones_feature_weights[2], rel=1e-3)
+        if a_scale == 1 and a_offset == 0:
+            # For the unscaled case, we do know what the unrescaled
+            # constant_ones feature weights should be
+            assert np.allclose(
+                expected_constant_ones_feature_weights,
+                result_constant_ones_feature_weights)
