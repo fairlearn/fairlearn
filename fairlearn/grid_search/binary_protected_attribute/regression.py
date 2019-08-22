b98181cffa6dcf07eb38f55c420bf1888af7c541
Richard Edgar <riedgar@microsoft.com>
2019-08-22 12:54:08 -0400

    Add first draft of documentation for the regression grid search

--- fairlearn/grid_search/binary_protected_attribute/regression.py
+++ fairlearn/grid_search/binary_protected_attribute/regression.py
@@ -18,6 +18,54 @@ def sweep_bounded_group_loss(learner,
                              x, y, protected_attribute,
                              tradeoffs=None,
                              number_of_tradeoffs=11):
+    """Function to generate a list of models for a binary
+    regression problem with a single binary protected attribute.
+    The trade off parameter controls how much weight is put on
+    the '0' labels of the protected attribute vs. the '1' labels
+    of the attribute. The trade off parameter varies between
+    zero and one, the extremes corresponding to performing the
+    regression only with the specified label. This sequence
+    of models enables selection for the best bounded group loss.
+
+    :param learner: An object which can be used to fit a model to
+    features, labels and weights. A deep copy of this is made for each
+    value of the lagrange multiplier used
+    :type learner: Must implement a fit(x, y, sample_weight) method
+
+    :param x: The array of training data features (which may or may
+    not contain the protected attribute). Must have as many rows
+    as y and protected_attribute
+    :type x: Nested list or numpy array with two dimensions or
+    pandas dataframe
+
+    :param y: The true regression values. Must contain the same
+    number of entries as rows in x.
+    :type y: List or Numpy array with one dimension or pandas Series
+
+    :param protected_attribute: The binary protected attribute
+    corresponding to each row of x. We do not consider it an error
+    if only one attribute value is present
+    :type protected_attribute: List or Numpy array with one
+    dimension or pandas Series
+
+    :param tradeoffs: User specified set of trade offs to use for
+    the sweep. If this is set then number_of_tradeoffs must be None.
+    The result array will be equal in length to this array
+    :type tradeoffs: List of real numbers from [0, 1]
+
+    :param number_of_tradeoffs: Specifies the number of trade offs
+    to use for this sweep. They will be automatically generated in
+    the range [0, 1]. If this is set then tradeoffs must be None.
+    The result array will have as many entries as specified here
+
+    :return: The models corresponding to each value of the trade offs
+    tested
+    :rtype: List of dictionaries. Each dictionary has fields
+    "tradeoff" and "model." Each model will correspond to
+    the input parameter learner after calling 'fit' on it (a deep
+    copy is made). The user is responsible for converting these
+    objects to an actual model, if further processing is required.
+    """
     # Must specify either an array of tradeoffs or the number
     # of tradeoffs to generated
     if bool(tradeoffs is None) == bool(number_of_tradeoffs is None):
@@ -42,5 +90,5 @@ def sweep_bounded_group_loss(learner,
         current_learner.fit(x, y, sample_weight=weights)
 
         result.append({"model": current_learner,
-                       "trade_off": trade_off})
+                       "tradeoff": trade_off})
     return result
