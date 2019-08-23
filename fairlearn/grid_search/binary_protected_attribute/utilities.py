6e7f4cf0773c1f087fd33bb8fce224d5a9d50d1f
Richard Edgar <riedgar@microsoft.com>
2019-08-23 07:56:17 -0400

    Actually, we do consider having only a single protected attribute value to be an error....

--- fairlearn/grid_search/binary_protected_attribute/utilities.py
+++ fairlearn/grid_search/binary_protected_attribute/utilities.py
@@ -12,12 +12,6 @@ def generate_protected_attribute_info(protected_attribute):
                            "have exactly two unique values")
 
     p0 = counts[0] / len(protected_attribute)
-    # It is possible that our data only contain one value for
-    # the protected attribute
-    # We do not consider this an error
-    if len(counts) > 1:
-        p1 = counts[1] / len(protected_attribute)
-    else:
-        p1 = 0
+    p1 = counts[1] / len(protected_attribute)
 
     return p0, p1, unique_labels[0]
