289d7ade8b1c839f5b3cba0c4aa4a47431478b2e
Richard Edgar <riedgar@microsoft.com>
2019-09-04 13:35:41 -0400

    Fix oversight in noqa tag

--- fairlearn/metrics/__init__.py
+++ fairlearn/metrics/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-from fairlearn.metrics.demographic_parity import DemographicParity  # noqa F401
+from fairlearn.metrics.demographic_parity import DemographicParity  # noqa: F401
