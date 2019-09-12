3acf24262cda22f80256e7c36d96e5ac31db82cf
Richard Edgar <riedgar@microsoft.com>
2019-09-12 11:40:38 -0400

    Taking metrics classes from Roman

---
+++ fairlearn/metrics/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+
+from .metric import FairnessMetric   # noqa: F401
+from .demographic_parity import DemographicParity  # noqa: F401
+from .equalized_odds import EqualizedOdds  # noqa: F401
---
+++ fairlearn/metrics/demographic_parity.py
@@ -0,0 +1,10 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+
+from .metric import FairnessMetric
+
+
+class DemographicParity(FairnessMetric):
+
+    pass
