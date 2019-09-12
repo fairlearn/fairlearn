1beb2b9c243588e7f84e60f8117879ad12ba463c
Richard Edgar <riedgar@microsoft.com>
2019-09-12 12:58:38 -0400

    Trim import statement

--- fairlearn/reductions/__init__.py
+++ fairlearn/reductions/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-from fairlearn.reductions.grid_search.grid_search import GridSearch  # noqa: F401
+from .grid_search.grid_search import GridSearch  # noqa: F401
