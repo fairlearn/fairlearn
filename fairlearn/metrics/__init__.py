55575daa7877e63baa39a99534968f440b58f710
Richard Edgar <riedgar@microsoft.com>
2019-09-12 11:45:44 -0400

    Add BGL to init

--- fairlearn/metrics/__init__.py
+++ fairlearn/metrics/__init__.py
@@ -5,3 +5,4 @@
 from .metric import FairnessMetric   # noqa: F401
 from .demographic_parity import DemographicParity  # noqa: F401
 from .equalized_odds import EqualizedOdds  # noqa: F401
+from .bounded_group_loss import BoundedGroupLoss  # noqa: F401
