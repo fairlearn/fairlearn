e3101b1b245a65f05417c91ac48002fce7beb832
Richard Edgar <riedgar@microsoft.com>
2019-09-12 12:42:57 -0400

    Metrics Skeleton (#27)
    Adding skeleton `DisparityMetric` (development of previous work by @romanlutz )

---
+++ fairlearn/metrics/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+
+from .metric import DisparityMetric   # noqa: F401
+from .demographic_parity import DemographicParity  # noqa: F401
+from .equalized_odds import EqualizedOdds  # noqa: F401
+from .bounded_group_loss import BoundedGroupLoss  # noqa: F401
---
+++ fairlearn/metrics/bounded_group_loss.py
@@ -0,0 +1,9 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+
+from .metric import DisparityMetric
+
+
+class BoundedGroupLoss(DisparityMetric):
+    pass
