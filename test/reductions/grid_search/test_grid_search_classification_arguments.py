f42f0a10f6e8614823d5fc740d55589abda695d3
Richard Edgar <riedgar@microsoft.com>
2019-09-18 12:11:37 -0400

    Trying to figure out the input data tests

---
+++ test/reductions/grid_search/test_grid_search_classification_arguments.py
@@ -0,0 +1,57 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import logging
+import numpy as np
+import pandas as pd
+import pytest
+from sklearn.linear_model import LogisticRegression
+
+from fairlearn.metrics import DemographicParity
+from fairlearn.reductions import GridSearch
+from fairlearn.reductions.grid_search.simple_quality_metrics import SimpleClassificationQualityMetric  # noqa: E501
+
+
+def identity(X):
+    return X
+
+
+def pandasdf(X):
+    return pd.DataFrame(X)
+
+
+class ArgumentTypesTest:
+    def _quick_data(self, number_samples=8):
+        feature_1 = np.random.randint(2, size=number_samples)
+        feature_2 = np.random.randint(6, size=number_samples)
+        feature_3 = np.random.randint(3, size=number_samples)
+        X = np.stack((feature_1, feature_2, feature_3), -1)
+        Y = np.random.randint(2, size=number_samples)
+        A = np.random.randint(2, size=number_samples)
+        return X, Y, A
+
+    Xtransform = [identity, pandasdf]
+    Ytransform = [identity, pandasdf]
+    Atransform = [identity, pandasdf]
+
+    def run_smoke(self, X, Y, A):
+        gs = GridSearch(self.learner, self.disparity_criterion, self.quality_metric)
+
+        gs.fit(X, Y, aux_data=A, number_of_lagrange_multipliers=11)
+
+        assert len(gs.all_results) == 11
+
+    @pytest.mark.parametrize("transformX", Xtransform)
+    @pytest.mark.parametrize("transformY", Ytransform)
+    @pytest.mark.parametrize("transformA", Atransform)
+    def test_inputs(self, transformX, transformY, transformA):
+        X, Y, A = self._quick_data()
+        self.run_smoke(transformX(X), transformY(Y), transformA(A))
+
+
+class TestDemographicParity(ArgumentTypesTest):
+    def setup_method(self, method):
+        logging.info("setup_method      method:%s" % method.__name__)
+        self.learner = LogisticRegression()
+        self.disparity_criterion = DemographicParity()
+        self.quality_metric = SimpleClassificationQualityMetric()
