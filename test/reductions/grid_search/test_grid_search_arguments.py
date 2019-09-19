0267ac3e1163d9513eb67751430dd6f47f848d6b
Richard Edgar <riedgar@microsoft.com>
2019-09-19 10:24:39 -0400

    Hard code randomised data

--- test/reductions/grid_search/test_grid_search_arguments.py
+++ test/reductions/grid_search/test_grid_search_arguments.py
@@ -54,13 +54,14 @@ Atransform = [identity, pandasdf, pandasseries, ndarray2d]
 # Tests which must be passed by all calls to the GridSearch
 # go here
 class ArgumentTests:
-    def _quick_data(self, number_samples=48):
-        feature_1 = np.random.randint(2, size=number_samples)
-        feature_2 = np.random.randint(6, size=number_samples)
-        feature_3 = np.random.randint(3, size=number_samples)
+    def _quick_data(self):
+        # Data are random and do not matter for these tests
+        feature_1 = [0, 1, 2, 3, 4, 5, 6, 7]
+        feature_2 = [5, 4, 3, 2, 7, 8, 3, 4]
+        feature_3 = [9, 2, 4, 2, 9, 3, 1, 8]
         X = np.stack((feature_1, feature_2, feature_3), -1)
-        Y = np.random.randint(2, size=number_samples)
-        A = np.random.randint(2, size=number_samples)
+        Y = np.array([0, 1, 0, 1, 1, 1, 1, 0])
+        A = np.array([1, 0, 0, 0, 0, 1, 1, 1])
         return X, Y, A
 
     # ----------------------------
@@ -83,7 +84,7 @@ class ArgumentTests:
     @pytest.mark.parametrize("transformY", Ytransform)
     def test_X_is_None(self, transformY, transformA):
         gs = GridSearch(self.learner, self.disparity_criterion, self.quality_metric)
-        _, Y, A = self._quick_data(8)
+        _, Y, A = self._quick_data()
 
         message = str("Must supply X")
         with pytest.raises(ValueError) as execInfo:
@@ -98,7 +99,7 @@ class ArgumentTests:
     @pytest.mark.parametrize("transformX", Xtransform)
     def test_Y_is_None(self, transformX, transformA):
         gs = GridSearch(self.learner, self.disparity_criterion, self.quality_metric)
-        X, _, A = self._quick_data(8)
+        X, _, A = self._quick_data()
 
         message = str("Must supply y")
         with pytest.raises(ValueError) as execInfo:
@@ -116,9 +117,8 @@ class ArgumentTests:
     @pytest.mark.parametrize("transformX", Xtransform)
     def test_X_Y_different_rows(self, transformX, transformY, transformA):
         gs = GridSearch(self.learner, self.disparity_criterion, self.quality_metric)
-        num_rows = 8
-        X, _, A = self._quick_data(num_rows)
-        Y = np.random.randint(2, size=num_rows+1)
+        X, _, A = self._quick_data()
+        Y = np.random.randint(2, size=len(A)+1)
 
         message = str("X and y must have same number of rows")
         with pytest.raises(RuntimeError) as execInfo:
@@ -134,9 +134,8 @@ class ArgumentTests:
     @pytest.mark.parametrize("transformX", Xtransform)
     def test_X_A_different_rows(self, transformX, transformY, transformA):
         gs = GridSearch(self.learner, self.disparity_criterion, self.quality_metric)
-        num_rows = 8
-        X, Y, A = self._quick_data(num_rows)
-        A = np.random.randint(2, size=num_rows+1)
+        X, Y, _ = self._quick_data()
+        A = np.random.randint(2, size=len(Y)+1)
 
         message = str("X and the target attribute must have same number of rows")
         with pytest.raises(RuntimeError) as execInfo:
@@ -154,7 +153,7 @@ class ArgumentTests:
     @pytest.mark.parametrize("transformX", Xtransform)
     def test_aux_data_non_binary(self, transformX, transformY, transformA):
         gs = GridSearch(self.learner, self.disparity_criterion, self.quality_metric)
-        X, Y, A = self._quick_data(8)
+        X, Y, A = self._quick_data()
         A[0] = 0
         A[1] = 1
         A[2] = 2
@@ -174,7 +173,7 @@ class ArgumentTests:
     @pytest.mark.parametrize("transformX", Xtransform)
     def test_Y_df_bad_columns(self, transformX, transformA):
         gs = GridSearch(self.learner, self.disparity_criterion, self.quality_metric)
-        X, Y, A = self._quick_data(8)
+        X, Y, A = self._quick_data()
 
         Y_two_col_df = pd.DataFrame({"a": Y, "b": Y})
         message = str("y is a DataFrame with more than one column")
@@ -190,7 +189,7 @@ class ArgumentTests:
     @pytest.mark.parametrize("transformX", Xtransform)
     def test_Y_ndarray_bad_columns(self, transformX, transformA):
         gs = GridSearch(self.learner, self.disparity_criterion, self.quality_metric)
-        X, Y, A = self._quick_data(8)
+        X, Y, A = self._quick_data()
 
         Y_two_col_ndarray = np.stack((Y, Y), -1)
         message = str("y is an ndarray with more than one column")
@@ -208,7 +207,7 @@ class ArgumentTests:
     @pytest.mark.parametrize("transformX", Xtransform)
     def test_A_df_bad_columns(self, transformX, transformY):
         gs = GridSearch(self.learner, self.disparity_criterion, self.quality_metric)
-        X, Y, A = self._quick_data(8)
+        X, Y, A = self._quick_data()
 
         A_two_col_df = pd.DataFrame({"a": A, "b": A})
         message = str("aux_data is a DataFrame with more than one column")
@@ -224,7 +223,7 @@ class ArgumentTests:
     @pytest.mark.parametrize("transformX", Xtransform)
     def test_A_ndarray_bad_columns(self, transformX, transformY):
         gs = GridSearch(self.learner, self.disparity_criterion, self.quality_metric)
-        X, Y, A = self._quick_data(8)
+        X, Y, A = self._quick_data()
 
         A_two_col_ndarray = np.stack((A, A), -1)
         message = str("aux_data is an ndarray with more than one column")
@@ -250,7 +249,7 @@ class TestDemographicParity(ArgumentTests):
     @pytest.mark.parametrize("transformX", Xtransform)
     def test_Y_ternary(self, transformX, transformY, transformA):
         gs = GridSearch(self.learner, self.disparity_criterion, self.quality_metric)
-        X, Y, A = self._quick_data(8)
+        X, Y, A = self._quick_data()
         Y[0] = 0
         Y[1] = 1
         Y[2] = 2
@@ -269,7 +268,7 @@ class TestDemographicParity(ArgumentTests):
     @pytest.mark.parametrize("transformX", Xtransform)
     def test_Y_not_0_1(self, transformX, transformY, transformA):
         gs = GridSearch(self.learner, self.disparity_criterion, self.quality_metric)
-        X, Y, A = self._quick_data(8)
+        X, Y, A = self._quick_data()
         Y = Y + 1
 
         message = str("Supplied y labels are not 0 or 1")
