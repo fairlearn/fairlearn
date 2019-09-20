4e26dfc8b4d26a219b438b84d9e6663f0c62358d
Richard Edgar <riedgar@microsoft.com>
2019-09-20 16:25:09 -0400

    Rename ReductionsLearner to ReductionsEstimator

--- fairlearn/reductions/__init__.py
+++ fairlearn/reductions/__init__.py
@@ -2,4 +2,4 @@
 # Licensed under the MIT License.
 
 from .grid_search.grid_search import GridSearch  # noqa: F401
-from .reductions_learner import ReductionsLearner  # noqa: F401
+from .reductions_estimator import ReductionsEstimator  # noqa: F401
