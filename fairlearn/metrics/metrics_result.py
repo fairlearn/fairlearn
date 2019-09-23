97a105e6801407ce7246101ea69c469b3b29b266
Richard Edgar <riedgar@microsoft.com>
2019-09-23 14:59:45 -0400

    Add comment about the expected contents

--- fairlearn/metrics/metrics_result.py
+++ fairlearn/metrics/metrics_result.py
@@ -4,5 +4,10 @@
 
 class MetricsResult:
     def __init__(self):
+        # The 'metric' property is the given metric evaluated without regard to group
         self.metric = None
+        # The 'group_metric' array contains one entry for each group passed to the
+        # evaluation routine. The groups are required to be integers, starting from
+        # zero. group_metric[i] contains the metric evaluated just for members of
+        # the group marked with i
         self.group_metric = []
