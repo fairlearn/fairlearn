176f8a025251ac6c9c46a9673b5848b6b8d5150b
Richard Edgar <riedgar@microsoft.com>
2019-09-23 11:18:34 -0400

    Why bother with putting 'all' into the dictionary?

--- fairlearn/metrics/true_positive_rate.py
+++ fairlearn/metrics/true_positive_rate.py
@@ -7,28 +7,28 @@ from . import MetricsResult
 
 
 def true_positive_rate(y_actual, y_predict, group_id):
-    all_dict_key = 'all-9d52d0d8'
-
     result = MetricsResult()
 
     groups = np.unique(group_id)
 
-    positives = {all_dict_key: 0}
-    true_positives = {all_dict_key: 0}
+    positives_all = 0
+    true_positives_all = 0
 
+    positives = {}
+    true_positives = {}
     for group in groups:
         positives[group] = 0
         true_positives[group] = 0
 
     for y, yhat, a in zip(y_actual, y_predict, group_id):
         if y == 1:
-            positives[all_dict_key] += 1
+            positives_all += 1
             positives[a] += 1
             if yhat == 1:
-                true_positives[all_dict_key] += 1
+                true_positives_all += 1
                 true_positives[a] += 1
 
-    result.metric = true_positives[all_dict_key] / positives[all_dict_key]
+    result.metric = true_positives_all / positives_all
     for group in groups:
         result.group_metric[group] = true_positives[group] / positives[group]
 
