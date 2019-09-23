786d3ae69572796a57d2f147a9cbe1bcb5aeaec1
Richard Edgar <riedgar@microsoft.com>
2019-09-23 15:13:43 -0400

    Fix spacing oversight

--- test/test_true_positive_rate.py
+++ test/test_true_positive_rate.py
@@ -21,7 +21,7 @@ def test_true_positive_rate_smoke():
 def test_true_positive_rate_smoke_chars():
     y_actual = [0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0]
     y_predict = [0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1]
-    group_ids = [0,0,0,0,1,1,1,1,2,2,2,2]
+    group_ids = [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2]
 
     result = metrics.true_positive_rate(y_actual, y_predict, group_ids)
 
