9a95ff9fc6d4a29deb8bbea728c315cc42c7b0de
Richard Edgar <riedgar@microsoft.com>
2019-09-27 11:43:30 -0400

    Figure out how to support sklearn metrics which don't support sample_weight

--- fairlearn/metrics/__init__.py
+++ fairlearn/metrics/__init__.py
@@ -11,6 +11,13 @@ from .bounded_group_loss import BoundedGroupLoss  # noqa: F401
 from .group_metric_result import GroupMetricResult  # noqa: F401
 from .metrics_engine import metric_by_group, make_group_metric  # noqa: F401
 
+# Classification metrics
 group_accuracy_score = make_group_metric(skm.accuracy_score)
 group_precision_score = make_group_metric(skm.precision_score)
 group_recall_score = make_group_metric(skm.recall_score)
+group_zero_one_loss = make_group_metric(skm.zero_one_loss)
+
+# Regression metrics
+group_max_error = make_group_metric(skm.max_error)
+group_mean_absolute_error = make_group_metric(skm.mean_absolute_error)
+group_mean_squared_error = make_group_metric(skm.mean_squared_error)
--- test/metrics/test_group_sklearn_wrappers.py
+++ test/metrics/test_group_sklearn_wrappers.py
@@ -1,6 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+import copy
 import pytest
 
 import fairlearn.metrics as metrics
@@ -16,20 +17,29 @@ weight = [1, 2, 3, 1, 2, 3, 4, 2]
 
 # Define as a dictionary so that the actual name can be seen
 # when pytest builds the tests
-supported_metrics = {"group_accuracy_score": metrics.group_accuracy_score,
-                     "group_precision_score": metrics.group_precision_score,
-                     "group_recall_score": metrics.group_recall_score}
+supported_metrics_weighted = {"group_accuracy_score": metrics.group_accuracy_score,
+                              "group_precision_score": metrics.group_precision_score,
+                              "group_recall_score": metrics.group_recall_score,
+                              "group_zero_one_loss": metrics.group_zero_one_loss,
+                              "group_mean_squared_error": metrics.group_mean_squared_error}
+
+metrics_no_sample_weights = {"group_max_error": metrics.group_max_error,
+                             "group_mean_absolute_error": metrics.group_mean_absolute_error}
+
+supported_metrics_unweighted = {**supported_metrics_weighted, **metrics_no_sample_weights}
 
 # =======================================================
 
 
-@pytest.mark.parametrize("name, group_metric_func", supported_metrics.items())
+@pytest.mark.parametrize("name, group_metric_func", supported_metrics_unweighted.items())
 def test_metric_unweighted(name, group_metric_func):
     result = group_metric_func(Y_true, Y_pred, groups)
+    # We don't really care about the numbers (sklearn is responsible)
+    # We just want to make sure we got a result
     assert len(result.by_group) == 5
 
 
-@pytest.mark.parametrize("name, group_metric_func", supported_metrics.items())
+@pytest.mark.parametrize("name, group_metric_func", supported_metrics_weighted.items())
 def test_metric_weighted(name, group_metric_func):
     result = group_metric_func(Y_true, Y_pred, groups, weight)
     assert len(result.by_group) == 5
