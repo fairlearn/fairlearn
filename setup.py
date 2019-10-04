2adf1dc3bf08d98674cc876f8bbfeb455356f996
Richard Edgar <riedgar@microsoft.com>
2019-10-04 08:42:54 -0400

    Group Wrappers for sklearn.metrics (#58)
    Set up group metric wrappers for some `scikit-learn` metrics
    Tweak naming of fields and handle case where returned metric is not a scalar

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Microsoft/fairlearn",
     packages=setuptools.find_packages(),
-    install_requires=["numpy", "scipy", "pandas"],
+    install_requires=["numpy", "scipy", "pandas", "scikit-learn"],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
