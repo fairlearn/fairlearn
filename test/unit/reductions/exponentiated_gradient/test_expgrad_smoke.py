9ceee820c04d5acd6d8237e32532e43f9c0a2ced
Richard Edgar <riedgar@microsoft.com>
2019-10-07 12:47:58 -0400

    Fix reference to simple_learners

--- test/unit/reductions/exponentiated_gradient/test_expgrad_smoke.py
+++ test/unit/reductions/exponentiated_gradient/test_expgrad_smoke.py
@@ -16,7 +16,7 @@ class TestExpgradSmoke:
         self.X = pd.DataFrame({"X1": X1, "X2": X2, "X3": X3})
         self.y = pd.Series(labels)
         self.A = pd.Series(group_data)
-        self.learner = simple_learners.LeastSquaresBinaryClassifierLearner()
+        self.learner = LeastSquaresBinaryClassifierLearner()
         self._PRECISION = 1e-6
 
     smoke_test_data = [{"cons_class": moments.DemographicParity, "eps": 0.100,
