ec1bf15f3539ca6a6f705ed3341202c9554b486c
Richard Edgar <riedgar@microsoft.com>
2019-10-21 08:24:20 -0400

    Get the regression tests into new pattern

--- test/unit/metrics/test_group_sklearn_wrappers.py
+++ test/unit/metrics/test_group_sklearn_wrappers.py
@@ -35,27 +35,36 @@ supported_metrics_weighted = [(skm.accuracy_score, metrics.group_accuracy_score)
 # The following only work with binary data when called with their default arguments
 supported_metrics_weighted_binary = [(skm.precision_score, metrics.group_precision_score),
                                      (skm.recall_score, metrics.group_recall_score),
-                                     (skm.roc_auc_score, metrics.group_roc_auc_score)]
+                                     (skm.roc_auc_score, metrics.group_roc_auc_score),
+                                     (skm.mean_squared_error, metrics.group_mean_squared_error)]
 supported_metrics_weighted_binary = supported_metrics_weighted_binary + supported_metrics_weighted
 
 
-metrics_no_sample_weights = [metrics.group_max_error,
-                             metrics.group_mean_absolute_error,
-                             metrics.group_mean_squared_log_error,
-                             metrics.group_median_absolute_error]
+metrics_no_sample_weights = [(skm.max_error, metrics.group_max_error),
+                             (skm.mean_absolute_error, metrics.group_mean_absolute_error),
+                             (skm.mean_squared_log_error, metrics.group_mean_squared_log_error),
+                             (skm.median_absolute_error, metrics.group_median_absolute_error)]
 
-supported_metrics_unweighted = metrics_no_sample_weights
-# supported_metrics_weighted + metrics_no_sample_weights
+supported_metrics_unweighted = metrics_no_sample_weights + supported_metrics_weighted_binary
 
 # =======================================================
 
 
-@pytest.mark.parametrize("group_metric_func", supported_metrics_unweighted)
-def test_metric_unweighted(group_metric_func):
+@pytest.mark.parametrize("func_tuple", supported_metrics_unweighted)
+def test_metric_unweighted(func_tuple):
+    metric_func = func_tuple[0]
+    group_metric_func = func_tuple[1]
+
     result = group_metric_func(Y_true, Y_pred, groups)
     # We don't really care about the numbers (sklearn is responsible)
     # We just want to make sure we got a result
     assert len(result.by_group) == 5
+
+    expected_overall = metric_func(Y_true, Y_pred)
+    if isinstance(expected_overall, np.ndarray):
+        assert np.array_equal(expected_overall, result.overall)
+    else:
+        assert expected_overall == result.overall
 
 
 @pytest.mark.parametrize("func_tuple", supported_metrics_weighted_binary)
