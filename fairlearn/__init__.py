405f0d89b414e12671e40ad473b8c354bb8fd19e
Roman Lutz <rolutz@microsoft.com>
2019-10-23 11:21:13 -0400

    typo

--- fairlearn/__init__.py
+++ fairlearn/__init__.py
@@ -7,7 +7,7 @@ __version__ = "0.3.0-alpha"
 _KW_SENSITIVE_FEATURES = "sensitive_features"
 
 
-# Setup logging infrustructure
+# Setup logging infrastructure
 import logging  # noqa: E402
 import os  # noqa: E402
 # Only log to disk if environment variable specified
