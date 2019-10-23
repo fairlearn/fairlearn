c289ee48c3f12f5f31de64654e896f5e4a3a4e7e
Roman Lutz <rolutz@microsoft.com>
2019-10-23 11:16:19 -0400

    add logging infra

--- fairlearn/__init__.py
+++ fairlearn/__init__.py
@@ -1,4 +1,17 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 __name__ = "fairlearn"
 __version__ = "0.3.0-alpha"
 
 _KW_SENSITIVE_FEATURES = "sensitive_features"
+
+
+# Setup logging infrustructure
+import logging
+import os
+# Only log to disk if environment variable specified
+fairlearn_logs = os.environ.get('FAIRLEARN_LOGS')
+if fairlearn_logs is not None:
+    logging.basicConfig(filename=fairlearn_logs, level=logging.INFO)
+    logging.info('Initializing logging file for fairlearn')
