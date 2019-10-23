0c813091279ac481e7e5f3061da25304859f683f
Roman Lutz <rolutz@microsoft.com>
2019-10-22 22:32:20 -0400

    flake8 and minor adjustments after merge with master

--- test/perf/conftest.py
+++ test/perf/conftest.py
@@ -1,9 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import pytest
-
-from tempeh.configurations import datasets, models
 from fairlearn.post_processing import ThresholdOptimizer
 from fairlearn.reductions import ExponentiatedGradient, GridSearch
 
--- test/perf/test_performance.py
+++ test/perf/test_performance.py
@@ -2,7 +2,6 @@
 # Licensed under the MIT License.
 
 import logging
-import numpy as np
 import pytest
 from time import time
 from tempeh.configurations import models, datasets
@@ -11,7 +10,6 @@ from fairlearn.post_processing import ThresholdOptimizer
 from fairlearn.post_processing.threshold_optimizer import DEMOGRAPHIC_PARITY
 from fairlearn.reductions import ExponentiatedGradient, GridSearch
 from fairlearn.reductions.moments import DemographicParity
-from fairlearn.reductions.grid_search.simple_quality_metrics import SimpleClassificationQualityMetric
 
 from conftest import get_all_perf_test_configurations
 
@@ -22,12 +20,13 @@ all_perf_test_configurations_descriptions = \
 
 logging.basicConfig(level=logging.DEBUG)
 
+
 @pytest.mark.parametrize("perf_test_configuration", all_perf_test_configurations,
                          ids=all_perf_test_configurations_descriptions)
 def test_perf(perf_test_configuration):
     print("Downloading dataset")
     dataset = datasets[perf_test_configuration.dataset]()
-    print("Downloaded dataset")
+    print("Done downloading dataset")
 
     if perf_test_configuration.dataset == "adult_uci":
         # sensitive feature is 8th column (sex)
@@ -51,9 +50,10 @@ def test_perf(perf_test_configuration):
                          .format(perf_test_configuration.dataset))
 
     print("Fitting estimator")
-    unconstrained_estimator = models[perf_test_configuration.predictor]()
+    estimator = models[perf_test_configuration.predictor]()
     unconstrained_predictor = models[perf_test_configuration.predictor]()
     unconstrained_predictor.fit(dataset.X_train, dataset.y_train)
+    print("Done fitting estimator")
 
     start_time = time()
     if perf_test_configuration.mitigator == ThresholdOptimizer.__name__:
@@ -61,19 +61,15 @@ def test_perf(perf_test_configuration):
                                        parity_criteria=DEMOGRAPHIC_PARITY,
                                        random_state=1)
     elif perf_test_configuration.mitigator == ExponentiatedGradient.__name__:
-        mitigator = ExponentiatedGradient(estimator=unconstrained_estimator,
+        mitigator = ExponentiatedGradient(estimator=estimator,
                                           constraints=DemographicParity())
     elif perf_test_configuration.mitigator == GridSearch.__name__:
-        mitigator = GridSearch(learner=unconstrained_estimator,
-                               disparity_metric=DemographicParity(),
-                               quality_metric=SimpleClassificationQualityMetric())
+        mitigator = GridSearch(estimator=estimator,
+                               constraints=DemographicParity())
     else:
         raise Exception("Unknown mitigation technique.")
 
     print("Fitting mitigator")
-    
-    print(dataset.X_train.shape)
-    print(dataset.y_train.shape)
 
     mitigator.fit(dataset.X_train, dataset.y_train, sensitive_features=sensitive_features_train)
 
