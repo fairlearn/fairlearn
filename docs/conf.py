24948cda0642e2e678f1b0d53a9062e712f0099a
Richard Edgar <riedgar@microsoft.com>
2019-10-24 10:22:08 -0400

    Script for building the docs (#105)
    Add a Powershell script which can build the documentation
    Follows the pattern from AzureMLCli

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -35,8 +34,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.intersphinx",
-    "sphinx.ext.extlinks",
-    "docfx_yaml.extension"
+    "sphinx.ext.extlinks"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -59,7 +57,7 @@ master_doc = 'index'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
