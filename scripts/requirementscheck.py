bb0f898b59fa2c72cfe18b00cc835dbb6907c22d
Richard Edgar <riedgar@microsoft.com>
2019-10-25 19:49:24 -0400

    Refactoring and simplifying python script

--- scripts/requirementscheck.py
+++ scripts/requirementscheck.py
@@ -2,78 +2,136 @@
 # and makes sure that they match up to '>=' vs '=='
 
 import argparse
-# import importlib.util
-import os
-import re
 import requirements
 import sys
 
+error_prefix = "##[error]"
 
-desc = "Checks setup.py and pip requirements files for consistency"
+
+def print_ado_error(msg):
+    print("{0}{1}".format(error_prefix, msg))
+
+
+def check_requirement_specs(requirement):
+    """Ensures that the requirement is '=='
+    or '>=' since we're interested in pinning
+    lower bounds
+    """
+    specs_good = True
+
+    if len(requirement.specs) > 1:
+        msg = "Too many specs for {0}".format(requirement.name)
+        print_ado_error(msg)
+        specs_good = False
+    elif len(requirement.specs) == 1:
+        allowed = {">=", "=="}
+        cmp = requirement.specs[0][0]
+        if requirement.specs[0][0] not in allowed:
+            msg = "Comparator {0} not allowed for {1}".format(cmp, requirement.name)
+            print_ado_error(msg)
+            specs_good = False
+
+    return specs_good
+
+
+def load_requirements_file(filename):
+    """Loads the specified requirements file
+    and checks that the specs are OK by themselves
+    """
+
+    print("Loading {0}".format(filename))
+
+    good_requirements = True
+    with open(filename, 'r') as fd:
+        reqs = sorted(list(requirements.parse(fd)), key=lambda x: x.name)
+        for r in reqs:
+            good_requirements = good_requirements and check_requirement_specs(r)
+
+    return good_requirements, reqs
+
+
+def compare_specs(spec1, spec2):
+    """Compare two specifications assuming that there
+    is a maximum of one comparator. If present this is
+    assumed to be == or >=
+    """
+    specs_match = True
+    # Quick sanity check
+    assert spec1.name == spec2.name
+
+    have1 = len(spec1.specs) > 0
+    have2 = len(spec2.specs) > 0
+
+    if have1 != have2:
+        print("{0}Only one spec on {1}",
+              error_prefix,
+              spec1.name)
+        specs_match = False
+    elif not have1:
+        # We have two empty lists
+        pass
+    else:
+        if spec1.specs[0][1] != spec2.specs[0][1]:
+            print("{0}Version mismatch on {1}",
+                  error_prefix,
+                  spec1.name)
+            specs_match = False
+
+    return specs_match
+
+
+def compare_requirements(req1, req2):
+    """Compare the two requirements, assuming that
+    check_requirement_specs has already been run and
+    hence that we don't need to worry about the
+    comparator
+    """
+    names1 = [x.name for x in req1]
+    names2 = [x.name for x in req1]
+
+    all_match = True
+    if names1 != names2:
+        msg = "List of requirement names does not match"
+        print_ado_error(msg)
+        return False
+    else:
+        all_match = True
+        for i in range(len(names1)):
+            # No need to check the spec again
+            spec1 = req1[i]
+            spec2 = req2[i]
+            all_match = all_match and compare_specs(spec1, spec2)
+
+    return all_match
+
+
+desc = "Checks two requirements files for consistency"
 
 parser = argparse.ArgumentParser(description=desc)
-parser.add_argument('directory')
+parser.add_argument('--fixed')
+parser.add_argument('--lowerbound')
 
 args = parser.parse_args()
 
-target_dir = os.path.abspath(args.directory)
+fixed_filename = args.fixed
+lowerbound_filename = args.lowerbound
 
-print("Examining {0}".format(target_dir))
+requirement_match_good = True
 
-file_pattern = r"requirements.*\.txt"
+good_fixed, fixed_specs = load_requirements_file(fixed_filename)
+good_lb, lb_specs = load_requirements_file(lowerbound_filename)
 
-files = [f for f in os.listdir(target_dir) if re.match(file_pattern, f)]
-
-print("Found requirements files: {0}".format(files))
-
-dependencies = {}
-for f in files:
-    print("Parsing file: {0}".format(f))
-    target_file = os.path.join(target_dir, f)
-    dep_dict = {}
-    with open(f, 'r') as fd:
-        for req in requirements.parse(fd):
-            req_name = req.name
-            if len(req.specs) > 1:
-                print("Spec too complicated", file=sys.stderr)
-                sys.exit(2)
-            if len(req.specs) == 1:
-                dep_dict[req_name] = req.specs[0][1]
-            else:
-                # Nothing specified so add a fake version
-                dep_dict[req_name] = -1
-
-    dependencies[f] = dep_dict
-print("All requirements files parsed")
-
-truth_file = list(dependencies.keys())[0]
-print("Taking {0} as ground truth".format(truth_file))
-truth_deps = dependencies[truth_file]
+print()
+print("Files loaded")
 print()
 
-found_mismatch = False
-for f, d in dependencies.items():
-    if f == truth_file:
-        continue
-    print("Examining {0}".format(f))
-    if sorted(d.keys()) != sorted(truth_deps.keys()):
-        print(" Dependency list does not match!")
-        found_mismatch = True
-    else:
-        for dep, ver in truth_deps.items():
-            if ver != d[dep]:
-                print("  Mismatched versions for {0}".format(dep))
-                found_mismatch = True
-    print("Examination of {0} completed".format(f))
-    print()
+requirement_match_good = good_fixed and good_lb
 
-# spec = importlib.util.spec_from_file_location("setup", os.path.join(target_dir, "setup.py"))
-# module = importlib.util.module_from_spec(spec)
-# spec.loader.exec_module(module)
-# print(module)
+requirement_match_good = requirement_match_good and compare_requirements(fixed_specs, lb_specs)
 
-if found_mismatch:
-    print("##[error]Found mismatches", file=sys.stderr)
+if not requirement_match_good:
+    print_ado_error("Found mismatches")
     sys.exit(1)
 else:
+    print("No mismatches found")
     sys.exit(0)
