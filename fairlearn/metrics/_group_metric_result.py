1dbe77fa7059a1dc109f034e6d5762bed8e4ce37
Richard Edgar <riedgar@microsoft.com>
2019-10-24 20:21:34 -0400

    Converting GroupMetricResult to properties

--- fairlearn/metrics/_group_metric_result.py
+++ fairlearn/metrics/_group_metric_result.py
@@ -5,43 +5,18 @@
 class GroupMetricResult:
     """Class to hold the result of a grouped metric, produced by calling
     fairlearn.metrics.metric_by_group.
-
-    :members:
-
-    *overall* The result of applying the metric to all the data
-
-    *by_group* A dictionary of the metric applied to each subgroup. The
-    keys the unique values of the `group_membership` array supplied
-    to the metric.
-
-    If the result of the metric is a scalar, then the following
-    will also be defined:
-
-    *minimum* The minimum value of the metric from the `by_group`
-    dictionary
-
-    *maximum* The maximum value of the metric from the `by_group`
-    dictionary
-
-    *argmin_set* The set of groups which correspond to the `minimum`
-
-    *argmax_set* The set of groups which correspond to the `maximum`
-
-    *range* The value of `maximum` - `minimum`
-
-    *range_ratio* The value of `minimum` / `maximum`
     """
 
     def __init__(self):
         # The 'overall' property is the given metric evaluated without regard to group
-        self.overall = None
+        self._overall = None
         # The 'by_group' dictionary contains the metric for each group found in the
         # input
-        self.by_group = {}
+        self._by_group = {}
         # The following two properties list the minimum and maximum metric values in
         # the by_group dictionary
-        self.minimum = None
-        self.maximum = None
+        self._minimum = None
+        self._maximum = None
         # The following two properties are the set of groups which have the minimum
         # and maximum values for the metric
         self.argmin_set = None
@@ -50,3 +25,53 @@ class GroupMetricResult:
         self.range = None
         # The value of minimum / maximum
         self.range_ratio = None
+
+    @property
+    def overall(self):
+        """Gets the value of the metric calculated
+        over the entire dataset
+        """
+        return self._overall
+
+    @overall.setter
+    def overall(self, value):
+        self._overall = value
+
+    @property
+    def by_group(self):
+        """Gets the value of the metric calculated for each sub-group
+        in the dataset.
+        This is a dictionary whose keys are the unique members of
+        the ``group_membership`` data. The corresponding values are
+        the result of applying the metric function to the set of
+        ``y_true`` and ``y_pred`` entries for each key.
+        """
+        return self._by_group
+
+    @by_group.setter
+    def by_group(self, value):
+        self._by_group = value
+
+    @property
+    def minimum(self):
+        """Gets the minimum value of the metric found in the
+        ``by_group`` dictionary, if the value is a scalar.
+        Otherwise, this will not be set.
+        """
+        return self._minimum
+
+    @minimum.setter
+    def minimum(self, value):
+        self._minimum = value
+
+    @property
+    def maximum(self):
+        """Gets the maxumum value of the metric found in the
+        ``by_group`` dictionary, if the values is a scalar.
+        Otherwise, this will not be set
+        """
+        return self._maximum
+
+    @maximum.setter
+    def maximum(self, value):
+        self._maximum = value
