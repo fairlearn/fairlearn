a0ae2f961c9a91b65165a4d94e872c60efcf3ca3
Richard Edgar <riedgar@microsoft.com>
2019-10-25 09:52:04 -0400

    More __all__ tinkering

--- fairlearn/reductions/__init__.py
+++ fairlearn/reductions/__init__.py
@@ -2,5 +2,34 @@
 # Licensed under the MIT License.
 
 from .reduction import Reduction  # noqa: F401
-from .grid_search.grid_search import GridSearch  # noqa: F401
-from .exponentiated_gradient.exponentiated_gradient import ExponentiatedGradient  # noqa: F401
+
+from .exponentiated_gradient import ExponentiatedGradient  # noqa: F401
+from .exponentiated_gradient import ExponentiatedGradientResult  # noqa: F401
+from .grid_search import GridSearch, GridSearchResult  # noqa: F401
+from .moments import AbsoluteLoss, Moment, ConditionalSelectionRate  # noqa: F401
+from .moments import DemographicParity, EqualizedOdds, ErrorRate   # noqa: F401
+from .moments import GroupLossMoment, SquareLoss, ZeroOneLoss  # noqa: F401
+
+_exponentiated_gradient = [
+    "ExponentiatedGradient",
+    "ExponentiatedGradientResult"
+]
+
+_grid_search = [
+    "GridSearch",
+    "GridSearchResult"
+]
+
+_moments = [
+    "AbsoluteLoss",
+    "Moment",
+    "ConditionalSelectionRate",
+    "DemographicParity",
+    "EqualizedOdds",
+    "ErrorRate",
+    "GroupLossMoment",
+    "SquareLoss",
+    "ZeroOneLoss"
+]
+
+__all__ = ["Reduction"] + _exponentiated_gradient + _grid_search + _moments
