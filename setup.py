2e95eee2568aa54b28915b1113d2dfbe26b4bf7c
Richard Edgar <riedgar@microsoft.com>
2019-10-27 15:59:15 -0400

    Add explanatory comment

--- setup.py
+++ setup.py
@@ -5,4 +5,8 @@ import setuptools
 
 import fairlearn  # noqa:F401
 
+# The following will read in the configuration from the
+# setup.cfg file
+# To read in the setup.cfg file as a dictionary see
+# https://setuptools.readthedocs.io/en/latest/setuptools.html#configuration-api
 setuptools.setup()
