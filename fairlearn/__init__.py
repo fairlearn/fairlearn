c263f2dfc7af2a9055b34b9a93f35e175713869f
Richard Edgar <riedgar@microsoft.com>
2019-10-28 14:39:18 -0400

    Add some top-level information

--- fairlearn/__init__.py
+++ fairlearn/__init__.py
@@ -1,3 +1,10 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+"""The fairlearn package contains a number of tools for
+analysing and mitigating disparity in Machine Learning models.
+"""
+
 __name__ = "fairlearn"
 __version__ = "0.3.0-alpha"
 
