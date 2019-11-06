ebf75f994c6d602935464a544e40cf8120335d3d
Roman Lutz <rolutz@microsoft.com>
2019-11-06 11:00:26 -0500

    adjust comment line wrap
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- fairlearn/__init__.py
+++ fairlearn/__init__.py
@@ -1,8 +1,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-"""The fairlearn package contains a number of tools for
-analyzing and mitigating disparity in Machine Learning models.
+"""The fairlearn package contains a number of tools for analyzing and mitigating disparity in
+Machine Learning models.
 """
 
 __name__ = "fairlearn"
