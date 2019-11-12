d6d75cd442db0ade2c300b2b24fe4b7addeeaafa
Richard Edgar <riedgar@microsoft.com>
2019-11-12 11:13:34 -0500

    Doing mean predictions still
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- fairlearn/metrics/_mean_predictions.py
+++ fairlearn/metrics/_mean_predictions.py
@@ -3,36 +3,19 @@
 
 import numpy as np
 
+from ._input_manipulations import _ensure_1d_ndarray
+
 
 def mean_prediction(y_true, y_pred, sample_weight=None):
     """Returns the (weighted) mean prediction. The true
     values are ignored, but required as an argument in order
     to maintain a consistent interface
-
-    :Example:
-
-    Without weights
-
-    .. literalinclude:: /../test/unit/metrics/test_mean_predictions.py
-        :linenos:
-        :language: python
-        :lines: 8-13
-        :dedent: 4
-
-    With weights
-
-    .. literalinclude:: /../test/unit/metrics/test_mean_predictions.py
-        :linenos:
-        :language: python
-        :lines: 17-23
-        :dedent: 4
-
     """
 
-    y_p = np.squeeze(np.asarray(y_pred))
+    y_p = _ensure_1d_ndarray(y_pred, "y_pred")
     s_w = np.ones(len(y_p))
     if sample_weight is not None:
-        s_w = np.squeeze(np.asarray(sample_weight))
+        s_w = _ensure_1d_ndarray(sample_weight, "sample_weight")
 
     return np.dot(y_p, s_w) / s_w.sum()
 
@@ -43,11 +26,11 @@ def mean_overprediction(y_true, y_pred, sample_weight=None):
     (i.e. underpredictions) are set to zero
     """
 
-    y_t = np.squeeze(np.asarray(y_true))
-    y_p = np.squeeze(np.asarray(y_pred))
+    y_t = _ensure_1d_ndarray(y_true, "y_true")
+    y_p = _ensure_1d_ndarray(y_pred, "y_pred")
     s_w = np.ones(len(y_p))
     if sample_weight is not None:
-        s_w = np.squeeze(np.asarray(sample_weight))
+        s_w = _ensure_1d_ndarray(sample_weight, "sample_weight")
 
     err = y_p - y_t
     err[err < 0] = 0
--- test/unit/metrics/test_mean_predictions.py
+++ test/unit/metrics/test_mean_predictions.py
@@ -13,6 +13,15 @@ def test_mean_prediction_unweighted():
     assert result == 2
 
 
+def test_mean_prediction_single():
+    y_pred = [42]
+    y_true = None
+
+    result = metrics.mean_prediction(y_true, y_pred)
+
+    assert result == 42
+
+
 def test_mean_prediction_weighted():
     y_pred = [0, 1, 2, 3, 4]
     y_true = None
@@ -23,9 +32,28 @@ def test_mean_prediction_weighted():
     assert result == 1
 
 
+def test_mean_prediction_weighted_single():
+    y_pred = [42]
+    y_true = None
+    weight = [2]
+
+    result = metrics.mean_prediction(y_true, y_pred, weight)
+
+    assert result == 42
+
+
 def test_mean_overprediction_unweighted():
     y_pred = [0, 1, 2, 3, 4]
     y_true = [1, 1, 5, 0, 2]
+
+    result = metrics.mean_overprediction(y_true, y_pred)
+
+    assert result == 1
+
+
+def test_mean_overprediction_unweighted_single():
+    y_pred = [1]
+    y_true = [0]
 
     result = metrics.mean_overprediction(y_true, y_pred)
 
@@ -40,6 +68,16 @@ def test_mean_overprediction_weighted():
     result = metrics.mean_overprediction(y_true, y_pred, weight)
 
     assert result == 0.5
+
+
+def test_mean_overprediction_weighted_single():
+    y_pred = [1]
+    y_true = [0]
+    weight = [1]
+
+    result = metrics.mean_overprediction(y_true, y_pred, weight)
+
+    assert result == 1
 
 
 def test_mean_underprediction_unweighted():
