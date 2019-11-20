342e2f94dda46e747de7135848fa2e51dca384aa
Richard Edgar <riedgar@microsoft.com>
2019-11-20 10:44:28 -0500

    Correct merge and add some more checking logic
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- fairlearn/__init__.py
+++ fairlearn/__init__.py
@@ -8,17 +8,21 @@ Machine Learning models.
 import os
 import sys
 
+# Finesse the version
 _FAIRLEARN_DEV_VERSION_ENV_VAR = "FAIRLEARN_DEV_VERSION"
 
-
 _base_version = "0.4.0a0"
-_rc_version = ""
+_dev_version = ""
 
 if _FAIRLEARN_DEV_VERSION_ENV_VAR in os.environ.keys():
     dev_version_string = os.environ[_FAIRLEARN_DEV_VERSION_ENV_VAR]
     try:
         dev_version_value = int(dev_version_string)
-        _dev_version = ".dev{0}".format(dev_version_value)
+        if dev_version_value >= 0:
+            _dev_version = ".dev{0}".format(dev_version_value)
+        else:
+            msg = "Value of {0} was not greater than or equal to zero. Ignoring"
+            print(msg.format(_FAIRLEARN_DEV_VERSION_ENV_VAR), file=sys.stderr)
     except ValueError:
         msg = "Value of {0} in {1} did not parse to integer. Ignoring"
         print(msg.format(dev_version_string, _FAIRLEARN_DEV_VERSION_ENV_VAR), file=sys.stderr)
@@ -28,6 +32,7 @@ __name__ = "fairlearn"
 __version__ = "{0}{1}".format(_base_version, _dev_version)
 
 
+# Common strings
 _NO_PREDICT_BEFORE_FIT = "Must call fit before attempting to make predictions"
 
 
