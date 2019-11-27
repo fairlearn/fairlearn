01e9f8cbfb69419cc0a05f713b0a883e4d3f1e04
Richard Edgar <riedgar@microsoft.com>
2019-11-27 09:02:12 -0500

    Standardise some file headers
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- fairlearn/widget/__init__.py
+++ fairlearn/widget/__init__.py
@@ -1,6 +1,5 @@
-# ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
+# Licensed under the MIT License.
 
 """Module for Fairlearn Dashboard widget."""
 
