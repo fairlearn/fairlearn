5f41ecbbfe3537048bf82c9f0955fc3cbfdce635
Richard Edgar <riedgar@microsoft.com>
2019-11-27 11:00:11 -0500

    Try changing path for RTD
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../fairlearn'))
 
 
 # -- Project information -----------------------------------------------------
@@ -22,7 +22,7 @@ copyright = '2019, Microsoft'
 author = 'Microsoft'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0-pre-alpha-interim'
+release = '0.4.0a1'
 
 
 # -- General configuration ---------------------------------------------------
