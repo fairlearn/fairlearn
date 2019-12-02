28963cdcd38b72002f04f4bb39a8aa68ab5770ec
Ilya Matiach <ilmat@microsoft.com>
2019-12-02 14:51:04 -0500

    update logging to use FileHandler instead of basicConfig (#175)
    Signed-off-by: Ilya Matiach <ilmat@microsoft.com>
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- fairlearn/__init__.py
+++ fairlearn/__init__.py
@@ -38,8 +38,19 @@ _NO_PREDICT_BEFORE_FIT = "Must call fit before attempting to make predictions"
 
 # Setup logging infrastructure
 import logging  # noqa: E402
-# Only log to disk if environment variable specified
+import atexit  # noqa: E402
+# Only log to disk if environment variable FAIRLEARN_LOGS specified
 fairlearn_logs = os.environ.get('FAIRLEARN_LOGS')
 if fairlearn_logs is not None:
-    logging.basicConfig(filename=fairlearn_logs, level=logging.INFO)
-    logging.info('Initializing logging file for fairlearn')
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    os.makedirs(os.path.dirname(fairlearn_logs), exist_ok=True)
+    handler = logging.FileHandler(fairlearn_logs, mode='w')
+    handler.setLevel(logging.INFO)
+    logger.addHandler(handler)
+    logger.info('Initializing logging file for fairlearn')
+
+    def close_handler():
+        handler.close()
+        logger.removeHandler(handler)
+    atexit.register(close_handler)
