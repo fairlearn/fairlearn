06b76ea20a29becc704688c18f8990bcc1b8d02c
Richard Edgar <riedgar@microsoft.com>
2019-12-04 15:35:45 -0500

    Tweak text
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- fairlearn/widget/__init__.py
+++ fairlearn/widget/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-"""Module for Fairlearn Dashboard widget."""
+"""Package for the fairlearn Dashboard widget."""
 
 from ._fairlearn_dashboard import FairlearnDashboard
 
