3057e6b5a3b05b1d5c03c7a6d2c1713c0bcd69c1
Richard Edgar <riedgar@microsoft.com>
2019-12-13 12:56:20 -0500

    Fix line length warning
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- scripts/process_readme.py
+++ scripts/process_readme.py
@@ -7,7 +7,7 @@ import sys
 logger = logging.getLogger(__name__)
 
 _BASE_URI_FORMAT = "https://github.com/fairlearn/fairlearn/tree/v{0}"
-_CURRENT_RELEASE_PATTERN = r"\[fairlearn v(\S+)\]\(https://github.com/fairlearn/fairlearn/tree/v\1\)"
+_CURRENT_RELEASE_PATTERN = r"\[fairlearn v(\S+)\]\(https://github.com/fairlearn/fairlearn/tree/v\1\)"  # noqa: E501
 _OTHER_MD_REF_PATTERN = r"\]\(\./(\w+\.md)"
 _SAME_MD_REF_PATTERN = r"\]\((#.+)\)"
 
