576d8c99b1b9d32e81beeaa49f3645db8fd84d49
Richard Edgar <riedgar@microsoft.com>
2019-12-16 12:58:37 -0500

    ReadMe Processor for Releases (#206)
    Create a python script to translate `ReadMe.md` from GitHub to PyPI. This will avoid the need to create a branch to do a release.
    
    This script is slightly dependent on the structure of the file, so if there are substantial changes to that, this script will require updating. It also assumes that a tag `v(fairlearn.__version__)` exists in the repo.
    
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- scripts/build-wheels.ps1
+++ scripts/build-wheels.ps1
@@ -20,16 +20,36 @@ if( Test-Path env:FAIRLEARN_DEV_VERSION )
     throw "Environment variable FAIRLEARN_DEV_VERSION must not be set"
 }
 
+# Install ourselves
+pip install -e .
+
+# Update the ReadMe file
+# Do this before setting FAIRLEARN_DEV_VERSION so that the links
+# on Test-PyPI are still correct
+Write-Host
+Write-Host "Updating ReadMe file"
+$readMeScript = Join-Path -resolve scripts process_readme.py
+$target = Join-Path -resolve $(Get-Location) README.md
+python $readMeScript --input $target --output $target --loglevel INFO
+if ($LASTEXITCODE -ne 0)
+{
+    throw "process_readme.py failed with result code $LASTEXITCODE"
+}
+
+# Set the environment variable for test if required
 if( $targetType -eq "Test" )
 {
     $Env:FAIRLEARN_DEV_VERSION = $devVersion
 }
 
-
-# Store fairlearn version (including FAIRLEARN_DEV_VERSION) in the file
-pip install .
+# Store fairlearn version (including FAIRLEARN_DEV_VERSION) in the specified file
+Write-Host "Storing fairlearn version i $versionFilename"
 $versionScript = Join-Path -resolve scripts fairlearn_version.py
 python $versionScript > $versionFilename
+if ($LASTEXITCODE -ne 0)
+{
+    throw "fairlearn_version.py failed with result code $LASTEXITCODE"
+}
 
 # Create the packages
 Write-Host
