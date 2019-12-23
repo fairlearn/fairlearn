1fcf2ee7187aea0e578480ae986140971d132857
Roman Lutz <rolutz@microsoft.com>
2019-12-23 17:00:29 -0500

    perf test through Azure ML (#180)
    * perf test first version through Azure ML
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * move some code to tempeh
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * add missing files
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * perf tests that get auth details through Azure Keyvault
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * upgrade to alpha tempeh version
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * flake8
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * exclude D100 and D103 for script generation python file
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * move variables into nightly-perf.yaml
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * azureml sdk requirement for perf test
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * remove powershell syntax
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * add cwd for tests
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * print cwd
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * remove special working directory condition
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * fix directory handling based on ADO
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * tempeh bump to a2
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * print message for debugging
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * try upper case variables
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * remove extraneous dash
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * print env var names
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * try explicitly adding variables
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * use variables directly, tempeh bump
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * add hardcoced data as variables
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * use windows instead of linux because some of the UI packages aren't available in linux
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * add wheel dependency
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * fake dashboard files
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * pass parameters for perf tests via args
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * yaml fix
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * yaml fix
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * remove waiting for run to complete
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * refactor to submit all jobs without waiting for result
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * remove obsolete gitignore line
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * tempeh bump to 0.1.11
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * azureml-sdk warning
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * pipeline improvements to use keyvault tasks
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * logically separate script generation into steps
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * simplify writing long string of = signs
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * flake8
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * replace incorrect variables in yaml
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * flake8
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * use importerror instead of modulenotfounderror for py3.5
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * add PR trigger for changes to test/perf directory
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * remove output from notebook
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * quotes for yaml variables
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * correct parameter in yaml
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

---
+++ test/perf/environment_setup.py
@@ -0,0 +1,49 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+import shutil
+import subprocess
+import time
+
+
+def build_package():
+    print('removing build directory')
+    shutil.rmtree("build", True)
+    print('removing fairlearn.egg-info')
+    shutil.rmtree("fairlearn.egg-info", True)
+    print('removing dist directory')
+    shutil.rmtree("dist", True)
+
+    # perf tests don't need dashboard, so we're faking the required files
+    required_files = [
+        os.path.join(os.getcwd(), 'fairlearn', 'widget', 'static', 'extension.js'),
+        os.path.join(os.getcwd(), 'fairlearn', 'widget', 'static', 'extension.js.map'),
+        os.path.join(os.getcwd(), 'fairlearn', 'widget', 'static', 'index.js'),
+        os.path.join(os.getcwd(), 'fairlearn', 'widget', 'static', 'index.js.map'),
+        os.path.join(os.getcwd(), 'fairlearn', 'widget', 'js', 'fairlearn_widget', 'labextension',
+                     'fairlearn-widget-0.1.0.tgz')
+    ]
+    for file_path in required_files:
+        directory = os.path.dirname(file_path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        if not os.path.exists(file_path):
+            with open(file_path, 'w') as new_file:
+                new_file.write('')
+
+    print('running python setup.py bdist_wheel')
+    subprocess.Popen(["python", "setup.py", "bdist_wheel"], cwd=os.getcwd(), shell=True).wait()
+    for root, dirs, files in os.walk("dist"):
+        for file in files:
+            if file.endswith(".whl"):
+                print("Found wheel {}".format(file))
+                # change wheel name to be unique for every run
+                src = os.path.join("dist", file)
+                dst = os.path.join("dist", "fairlearn-v{}-py3-none-any.whl"
+                                   .format(time.time()))
+                shutil.copy(src, dst)
+                return dst
+
+    raise Exception("Couldn't find wheel file.")
