d11d031ac8478d33221cf4c13781b64fda59d24f
Richard Edgar <riedgar@microsoft.com>
2019-12-23 18:11:57 -0500

    Documentation and flake8 (#215)
    Various updates for the documentation:
    - Remove another `flake8` global suppression
    - Add explanations for remaining `flake8` suppressions
    - Replace the `:any:` references in the documentation with appropriate ones
    - Make some file-level suppressions (which may have actually turned `flake8` off entirely on the file) specific to the appropriate lines
    
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>
    Signed-off-by: Abdul Hannan Kanji <akanji@umass.edu>

--- fairlearn/reductions/_grid_search/grid_search_result.py
+++ fairlearn/reductions/_grid_search/grid_search_result.py
@@ -1,9 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-# Class is just for holding results so 'imperative mode' makes
-# little sense
-# flake8: noqa: D401
 
 class GridSearchResult:
     """Class to hold a single result from the :class:`GridSearch` class."""
@@ -16,12 +13,12 @@ class GridSearchResult:
 
     @property
     def predictor(self):
-        """The predictor trained for this particular result from :class:`GridSearch`."""
+        """Return the predictor trained for this particular result from :class:`GridSearch`."""
         return self._predictor
 
     @property
     def lambda_vec(self):
-        """The Lagrange multiplier corresponding to this result.
+        """Return the Lagrange multiplier corresponding to this result.
 
         The exact contents of this are defined by the `constraints`
         argument passed to :class:`GridSearch`
@@ -30,10 +27,10 @@ class GridSearchResult:
 
     @property
     def objective(self):
-        """Description goes here."""
+        """Description goes here."""  # noqa: D401
         return self._objective
 
     @property
     def gamma(self):
-        """Description goes here."""
+        """Description goes here."""  # noqa: D401
         return self._gamma
