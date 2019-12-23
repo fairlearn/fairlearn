098bc15b88feba59f013470233590eff08365737
Roman Lutz <rolutz@microsoft.com>
2019-12-23 17:00:29 -0500

    perf test through Azure ML (#180)
    * perf test first version through Azure ML
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * move some code to tempeh
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * add missing files
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * perf tests that get auth details through Azure Keyvault
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * upgrade to alpha tempeh version
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * flake8
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * exclude D100 and D103 for script generation python file
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * move variables into nightly-perf.yaml
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * azureml sdk requirement for perf test
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * remove powershell syntax
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * add cwd for tests
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * print cwd
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * remove special working directory condition
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * fix directory handling based on ADO
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * tempeh bump to a2
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * print message for debugging
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * try upper case variables
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * remove extraneous dash
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * print env var names
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * try explicitly adding variables
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * use variables directly, tempeh bump
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * add hardcoced data as variables
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * use windows instead of linux because some of the UI packages aren't available in linux
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * add wheel dependency
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * fake dashboard files
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * pass parameters for perf tests via args
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * yaml fix
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * yaml fix
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * remove waiting for run to complete
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * refactor to submit all jobs without waiting for result
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * remove obsolete gitignore line
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * tempeh bump to 0.1.11
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * azureml-sdk warning
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * pipeline improvements to use keyvault tasks
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * logically separate script generation into steps
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * simplify writing long string of = signs
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * flake8
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * replace incorrect variables in yaml
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * flake8
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * use importerror instead of modulenotfounderror for py3.5
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * add PR trigger for changes to test/perf directory
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * remove output from notebook
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * quotes for yaml variables
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * correct parameter in yaml
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

---
+++ test/perf/script_generation.py
@@ -0,0 +1,110 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+
+from fairlearn.postprocessing import ThresholdOptimizer
+from fairlearn.reductions import ExponentiatedGradient, GridSearch
+
+
+def generate_script(request, perf_test_configuration, script_name, script_directory):
+    if not os.path.exists(script_directory):
+        os.makedirs(script_directory)
+
+    script_lines = []
+    add_imports(script_lines)
+    script_lines.append("")
+    script_lines.append("run = Run.get_context()")
+    add_dataset_setup(script_lines, perf_test_configuration)
+    add_unconstrained_estimator_fitting(script_lines, perf_test_configuration)
+    script_lines.append('start_time = time()')
+    add_mitigation(script_lines, perf_test_configuration)
+    add_evaluation(script_lines)
+    script_lines.append("")
+
+    print("\n\n{}\n\n".format("="*100))
+
+    with open(os.path.join(script_directory, script_name), 'w') as script_file:  # noqa: E501
+        script_file.write("\n".join(script_lines))
+
+
+def add_imports(script_lines):
+    script_lines.append('from time import time')
+    script_lines.append('from tempeh.configurations import models, datasets')
+    script_lines.append('from fairlearn.postprocessing import ThresholdOptimizer')
+    script_lines.append('from fairlearn.reductions import ExponentiatedGradient, GridSearch')
+    script_lines.append('from fairlearn.reductions import DemographicParity, EqualizedOdds')
+    script_lines.append('from azureml.core.run import Run')
+
+
+def add_dataset_setup(script_lines, perf_test_configuration):
+    script_lines.append('print("Downloading dataset")')
+    script_lines.append('dataset = datasets["{}"]()'.format(perf_test_configuration.dataset))
+    script_lines.append('X_train, X_test = dataset.get_X()')
+    script_lines.append('y_train, y_test = dataset.get_y()')
+    script_lines.append('print("Done downloading dataset")')
+
+    if perf_test_configuration.dataset == "adult_uci":
+        # sensitive feature is 8th column (sex)
+        script_lines.append('sensitive_features_train = X_train[:, 7]')
+        script_lines.append('sensitive_features_test = X_test[:, 7]')
+    elif perf_test_configuration.dataset == "diabetes_sklearn":
+        # sensitive feature is 2nd column (sex)
+        # features have been scaled, but sensitive feature needs to be str or int
+        script_lines.append('sensitive_features_train = X_train[:, 1].astype(str)')
+        script_lines.append('sensitive_features_test = X_test[:, 1].astype(str)')
+        # labels can't be floats as of now
+        script_lines.append('y_train = y_train.astype(int)')
+        script_lines.append('y_test = y_test.astype(int)')
+    elif perf_test_configuration.dataset == "compas":
+        # sensitive feature is either race or sex
+        # TODO add another case where we use sex as well, or both (?)
+        script_lines.append('sensitive_features_train, sensitive_features_test = dataset.get_sensitive_features("race")')
+        script_lines.append('y_train = y_train.astype(int)')
+        script_lines.append('y_test = y_test.astype(int)')
+    else:
+        raise ValueError("Sensitive features unknown for dataset {}"
+                         .format(perf_test_configuration.dataset))
+
+
+def add_unconstrained_estimator_fitting(script_lines, perf_test_configuration):
+    script_lines.append('print("Fitting estimator")')
+    script_lines.append('estimator = models["{}"]()'.format(perf_test_configuration.predictor))
+    script_lines.append('unconstrained_predictor = models["{}"]()'.format(perf_test_configuration.predictor))
+    script_lines.append('unconstrained_predictor.fit(X_train, y_train)')
+    script_lines.append('print("Done fitting estimator")')
+
+
+def add_mitigation(script_lines, perf_test_configuration):
+    if perf_test_configuration.mitigator == ThresholdOptimizer.__name__:
+        script_lines.append('mitigator = ThresholdOptimizer('
+                            'unconstrained_predictor=unconstrained_predictor, '
+                            'constraints="{}")'.format(perf_test_configuration.disparity_metric))
+    elif perf_test_configuration.mitigator == ExponentiatedGradient.__name__:
+        script_lines.append('mitigator = ExponentiatedGradient('
+                            'estimator=estimator, '
+                            'constraints={}())'.format(perf_test_configuration.disparity_metric))
+    elif perf_test_configuration.mitigator == GridSearch.__name__:
+        script_lines.append('mitigator = GridSearch(estimator=estimator, '
+                            'constraints={}())'.format(perf_test_configuration.disparity_metric))
+    else:
+        raise Exception("Unknown mitigation technique.")
+
+    script_lines.append('print("Fitting mitigator")')
+    script_lines.append('mitigator.fit(X_train, y_train, sensitive_features=sensitive_features_train)')
+
+    if perf_test_configuration.mitigator == ThresholdOptimizer.__name__:
+        script_lines.append('mitigator.predict('
+                            'X_test, '
+                            'sensitive_features=sensitive_features_test, '
+                            'random_state=1)')
+    else:
+        script_lines.append('predictions = mitigator.predict(X_test)')
+
+
+def add_evaluation(script_lines):
+    # TODO evaluate accuracy/fairness tradeoff
+
+    script_lines.append('total_time = time() - start_time')
+    script_lines.append('run.log("total_time", total_time)')
+    script_lines.append('print("Total time taken: {}s".format(total_time))')
