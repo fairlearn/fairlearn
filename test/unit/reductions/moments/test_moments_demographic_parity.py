62ad9ad1238c73378785df70622af72607119692
Richard Edgar <riedgar@microsoft.com>
2019-12-27 11:42:32 -0500

    Continuing to plod through the constructor
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- test/unit/reductions/moments/test_moments_demographic_parity.py
+++ test/unit/reductions/moments/test_moments_demographic_parity.py
@@ -63,12 +63,29 @@ def test_construct_and_load():
     assert len(dp.prob_event.index) == 1
     assert dp.prob_event.loc['all'] == 1
 
-    # Examine the prob_group_event DP
+    # Examine the prob_group_event DF
     # There's only an 'all' event but this records the fractions
     # of each label in the population
     assert len(dp.prob_group_event.index) == 2
     assert dp.prob_group_event.loc[('all', a0_label)] == num_samples_a0 / num_samples
     assert dp.prob_group_event.loc[('all', a1_label)] == num_samples_a1 / num_samples
 
-    print(dp.neg_basis)
-    assert len(dp.neg_basis.index)==10
+    # Examine the neg_basis DF
+    # This is obviously looking at the \lambda_{-} values in some way
+    assert len(dp.neg_basis.index) == 4
+    assert dp.neg_basis[0]['+', 'all', a0_label] == 0
+    assert dp.neg_basis[0]['+', 'all', a1_label] == 0
+    assert dp.neg_basis[0]['-', 'all', a0_label] == 1
+    assert dp.neg_basis[0]['-', 'all', a1_label] == 0
+
+    # Examine the pos_basis DF
+    # This is looking at the \lambda_{+} values and picking out the
+    # one associated with the first label
+    assert len(dp.pos_basis.index) == 4
+    assert dp.pos_basis[0]['+', 'all', a0_label] == 1
+    assert dp.pos_basis[0]['+', 'all', a1_label] == 0
+    assert dp.pos_basis[0]['-', 'all', a0_label] == 0
+    assert dp.pos_basis[0]['-', 'all', a1_label] == 0
+
+    print(dp.neg_basis_present)
+    assert False
