7e33dd3b5e63128c6a16eede1ef2a99848fadca5
Richard Edgar <riedgar@microsoft.com>
2020-01-06 14:29:47 -0500

    Moving the scrapbook code to the test
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- test/notebooks/test_notebooks.py
+++ test/notebooks/test_notebooks.py
@@ -1,15 +1,33 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import nbformat as nbf
 import papermill as pm
-import scrapbook as sb
 import pytest
+import scrapbook as sb
 
 
 @pytest.mark.notebooks
 def test_group_metrics_notebook():
     notebookname = "Group Metrics"
     input_notebook = "notebooks/" + notebookname + ".ipynb"
+    processed_notebook = "./test/notebooks/" + notebookname + ".processed.ipynb"
     output_notebook = "./test/notebooks/" + notebookname + ".output.ipynb"
 
-    pm.execute_notebook(input_notebook, output_notebook)
+    notebook = nbf.read(input_notebook, as_version=nbf.NO_CONVERT)
+    # sb.glue("overall_recall", group_metrics.overall)
+    # sb.glue("recall_by_groups", results.by_group)
+    scrapbook_cells = []
+    scrapbook_cells.append(nbf.v4.new_code_cell(source="import scrapbook as sb"))
+    scrapbook_cells.append(nbf.v4.new_code_cell(
+        source="sb.glue(\"overall_recall\", group_metrics.overall)"))
+    scrapbook_cells.append(nbf.v4.new_code_cell(
+        source="sb.glue(\"recall_by_groups\", results.by_group)"))
+    [notebook['cells'].append(c) for c in scrapbook_cells]
+
+    nbf.write(notebook, processed_notebook)
+
+    pm.execute_notebook(processed_notebook, output_notebook)
 
     nb = sb.read_notebook(output_notebook)
 
