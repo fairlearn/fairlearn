55acae044cfdb907a02a3b1cc8e41e2762acaf08
Richard Edgar <riedgar@microsoft.com>
2020-01-06 14:00:46 -0500

    Rename file
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- test/notebooks/test_notebooks.py
+++ test/notebooks/test_notebooks.py
@@ -12,8 +12,9 @@ def test_group_metrics_notebook():
     pm.execute_notebook(input_notebook, output_notebook)
 
     nb = sb.read_notebook(output_notebook)
-    print(nb.scraps)  # print a dict of all scraps by name
+
     expected_overall_recall = 0.5
     assert nb.scraps['overall_recall'].data == expected_overall_recall
+
     expected_by_groups = {'a': 0.0, 'b': 0.5, 'c': 0.75, 'd': 0.0}
     assert nb.scraps['recall_by_groups'].data == expected_by_groups
