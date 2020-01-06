c7598c74f4b1ce07c17e6cbbcfb798e809c52334
Richard Edgar <riedgar@microsoft.com>
2020-01-06 13:10:15 -0500

    Figuring out how to test things from the notebook
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- test/notebooks/test_group_metrics_nb.py
+++ test/notebooks/test_group_metrics_nb.py
@@ -7,9 +7,13 @@ import pytest
 def test_group_metrics_notebook():
     notebookname = "Group Metrics"
     input_notebook = "notebooks/" + notebookname + ".ipynb"
-    output_notebook = "./test/notebooks" + notebookname + ".output.ipynb"
+    output_notebook = "./test/notebooks/" + notebookname + ".output.ipynb"
 
     pm.execute_notebook(input_notebook, output_notebook)
 
     nb = sb.read_notebook(output_notebook)
     print(nb.scraps)  # print a dict of all scraps by name
+    expected_overall_recall = 0.5
+    assert nb.scraps['overall_recall'].data == expected_overall_recall
+    expected_by_groups = {'a': 0.0, 'b': 0.5, 'c': 0.75, 'd': 0.0}
+    assert nb.scraps['recall_by_groups'].data == expected_by_groups
