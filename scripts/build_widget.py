05e00d6de27e7e642be54dbfd119b9634ac4a838
Roman Lutz <rolutz@microsoft.com>
2020-01-16 13:58:47 -0500

    build_widget finalization plus add ls commands to find yarn installation in ADO
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- scripts/build_widget.py
+++ scripts/build_widget.py
@@ -12,6 +12,14 @@ _logger = logging.getLogger(__file__)
 logging.basicConfig(level=logging.INFO)
 
 _widget_js_directory = os.path.join("fairlearn", "widget", "js")
+_widget_generated_files = [
+    'fairlearn/widget/static/extension.js',
+    'fairlearn/widget/static/extension.js.map',
+    'fairlearn/widget/static/index.js',
+    'fairlearn/widget/static/index.js.map',
+    'jupyter-config/nbconfig/notebook.d/fairlearn-widget.json',
+    'fairlearn/widget/js/fairlearn_widget/labextension/fairlearn-widget-0.1.0.tgz'
+]
 
 
 def build_argument_parser():
@@ -22,6 +30,11 @@ def build_argument_parser():
     parser.add_argument("--yarn-path",
                         help="The full path to the yarn executable.",
                         required=True)
+    parser.add_argument("--assert-no-changes",
+                        help="Assert that the generated files did not change.",
+                        required=False,
+                        default=False,
+                        action='store_true')
 
     return parser
 
@@ -43,6 +56,15 @@ def main(argv):
         shutil.rmtree(os.path.join(_widget_js_directory, "dist"))
         shutil.rmtree(os.path.join(_widget_js_directory, "lib"))
         shutil.rmtree(os.path.join(_widget_js_directory, "node_modules"))
+
+    if args.assert_no_changes:
+        with LogWrapper("comparison between old and newly generated widget files."):
+            for file_path in _widget_generated_files:
+                diff_result = subprocess.check_output(["git", "diff", os.path.abspath(file_path)])
+                if diff_result == b'':
+                    _logger.info("File {} has not changed.".format(file_path))
+                else:
+                    _logger.error("File {} has changed unexpectedly.".format(file_path))
 
 
 if __name__ == "__main__":
