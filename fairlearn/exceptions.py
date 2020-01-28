82366688942051ca6315dd788ca9fc9c23d5ccde
Adrin Jalali <adrin.jalali@gmail.com>
2020-01-28 20:54:04 +0100

    MNT use sklearn's NotFittedError instead of NotFittedException (#259)
    * MNT use sklearn's NotFittedError instead of NotFittedException
    
    Signed-off-by: adrinjalali <adrin.jalali@gmail.com>
    
    * add to the changelog
    
    Signed-off-by: adrinjalali <adrin.jalali@gmail.com>
    Signed-off-by: Abdul Hannan Kanji <akanji@umass.edu>

--- fairlearn/exceptions.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
-"""Module holding specialised exceptions for fairlearn."""
-
-
-class NotFittedException(ValueError):
-    """Exception to use if :code:`predict()` is called before :code:`fit()`."""
