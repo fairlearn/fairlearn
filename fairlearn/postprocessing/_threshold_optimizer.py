e5f9ef9e7c4eccf7d2e4676078f50dd0ef904ca7
Roman Lutz <rolutz@microsoft.com>
2020-01-30 14:37:20 -0500

    multiple sensitive features (passing tests)
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- fairlearn/postprocessing/_threshold_optimizer.py
+++ fairlearn/postprocessing/_threshold_optimizer.py
@@ -94,8 +94,8 @@ class ThresholdOptimizer(PostProcessing):
         :type sensitive_features: currently 1D array as numpy.ndarray, list, pandas.DataFrame,
             or pandas.Series
         """
-        _, _, sensitive_feature_vector = _validate_and_reformat_input(
-            X, y, sensitive_features=sensitive_features, enforce_binary_labels=True)
+        _validate_and_reformat_input(X, y, sensitive_features=sensitive_features,
+                                     enforce_binary_labels=True)
 
         # postprocessing can't handle 0/1 as floating point numbers, so this converts it to int
         if type(y) in [np.ndarray, pd.DataFrame, pd.Series]:
@@ -142,9 +142,9 @@ class ThresholdOptimizer(PostProcessing):
             random.seed(random_state)
 
         self._validate_post_processed_predictor_is_fitted()
-        _, _, sensitive_feature_vector = _validate_and_reformat_input(
-            X, y=None, sensitive_features=sensitive_features, expect_y=False,
-            enforce_binary_labels=True)
+        _validate_and_reformat_input(X, y=None, sensitive_features=sensitive_features,
+                                     expect_y=False,
+                                     enforce_binary_labels=True)
         unconstrained_predictions = self._unconstrained_predictor.predict(X)
 
         positive_probs = _vectorized_prediction(
@@ -167,9 +167,9 @@ class ThresholdOptimizer(PostProcessing):
         :rtype: numpy.ndarray
         """
         self._validate_post_processed_predictor_is_fitted()
-        _, _, sensitive_feature_vector = _validate_and_reformat_input(
-            X, y=None, sensitive_features=sensitive_features, expect_y=False,
-            enforce_binary_labels=True)
+        _validate_and_reformat_input(X, y=None, sensitive_features=sensitive_features,
+                                     expect_y=False,
+                                     enforce_binary_labels=True)
         positive_probs = _vectorized_prediction(
             self._post_processed_predictor_by_sensitive_feature, sensitive_feature_vector,
             self._unconstrained_predictor.predict(X))
@@ -178,31 +178,6 @@ class ThresholdOptimizer(PostProcessing):
     def _validate_post_processed_predictor_is_fitted(self):
         if not self._post_processed_predictor_by_sensitive_feature:
             raise NotFittedError(PREDICT_BEFORE_FIT_ERROR_MESSAGE)
-
-    def _validate_input_data(self, X, sensitive_features, y=None):
-        allowed_input_types = [list, np.ndarray, pd.DataFrame, pd.Series]
-        if type(X) not in allowed_input_types or \
-                type(sensitive_features) not in allowed_input_types or \
-                (y is not None and type(y) not in allowed_input_types):
-            raise TypeError(INPUT_DATA_FORMAT_ERROR_MESSAGE
-                            .format(type(X).__name__,
-                                    type(y).__name__,
-                                    type(sensitive_features).__name__))
-
-        if len(X) == 0 or len(sensitive_features) == 0 or (y is not None and len(y) == 0):
-            raise ValueError(EMPTY_INPUT_ERROR_MESSAGE)
-
-        if y is None:
-            if len(X) != len(sensitive_features) or (y is not None and len(X) != len(y)):
-                raise ValueError(DIFFERENT_INPUT_LENGTH_ERROR_MESSAGE
-                                 .format("X and sensitive_features"))
-        else:
-            if len(X) != len(sensitive_features) or (y is not None and len(X) != len(y)):
-                raise ValueError(DIFFERENT_INPUT_LENGTH_ERROR_MESSAGE
-                                 .format("X, sensitive_features, and y"))
-
-        if set(np.unique(y)) > set([0, 1]):
-            raise ValueError(NON_BINARY_LABELS_ERROR_MESSAGE)
 
 
 def _threshold_optimization_demographic_parity(sensitive_features, labels, scores, grid_size=1000,
