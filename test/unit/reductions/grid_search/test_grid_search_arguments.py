5d6315ebfa4d1d175091c862953cdd6d9cbccf3f
Roman Lutz <rolutz@microsoft.com>
2020-02-06 14:55:49 -0500

    flake8
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- test/unit/reductions/grid_search/test_grid_search_arguments.py
+++ test/unit/reductions/grid_search/test_grid_search_arguments.py
@@ -27,9 +27,11 @@ candidate_X_transforms = [ensure_ndarray, ensure_dataframe]
 candidate_Y_transforms = conversions_for_1d
 candidate_A_transforms = conversions_for_1d
 
+
 def _skip_if_invalid_transformation(A, transform):
     if len(A.shape) > 1 and A.shape[1] > 1 and transform in [ensure_list, ensure_series]:
         pytest.skip()
+
 
 # Base class for tests
 # Tests which must be passed by all calls to the GridSearch
@@ -148,7 +150,7 @@ class ArgumentTests:
         gs = GridSearch(self.estimator, self.disparity_criterion)
         X, Y, A = self._quick_data(A_two_dim)
         _skip_if_invalid_transformation(A, transformA)
-        
+
         if A_two_dim:
             A[0][0] = 0
             A[0][1] = 0
