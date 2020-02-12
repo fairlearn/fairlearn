2aa528f56267164e7f517eefb30fc0ddb20ffa0d
Adrin Jalali <adrin.jalali@gmail.com>
2020-02-12 23:41:19 +0100

    ThresholdOptimizer: use Mixins instead of our own parent class (#292)
    * Use Mixins instead of our own parent class
    
    Signed-off-by: adrinjalali <adrin.jalali@gmail.com>
    
    * remove unused imports
    
    Signed-off-by: adrinjalali <adrin.jalali@gmail.com>

--- fairlearn/postprocessing/__init__.py
+++ fairlearn/postprocessing/__init__.py
@@ -7,10 +7,8 @@ The predictor's output is adjusted to fulfill specified parity constraints. The 
 learn how to adjust the predictor's output from the training data.
 """
 
-from ._postprocessing import PostProcessing  # noqa: F401
 from ._threshold_optimizer import ThresholdOptimizer  # noqa: F401
 
 __all__ = [
-    "PostProcessing",
     "ThresholdOptimizer"
 ]
