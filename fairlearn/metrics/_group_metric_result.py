29792eb958af31e9baf33140d2d5bc053251c448
Richard Edgar <riedgar@microsoft.com>
2020-02-18 14:45:03 -0500

    Revert "Metrics Updates (#297)"
    This reverts commit 728562ae66a5b08771eec6050dc85cb914266df8.
    
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- fairlearn/metrics/_group_metric_result.py
+++ fairlearn/metrics/_group_metric_result.py
@@ -1,16 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import copy
 import numpy as np
-import pandas as pd
-
-_OVERALL = 'overall'
-_BY_GROUP = 'by_group'
-_MAX = 'maximum'
-_MIN = 'minimum'
-_RANGE = 'range'
-_RANGE_RATIO = 'range_ratio'
 
 
 class GroupMetricResult:
@@ -99,50 +90,6 @@ class GroupMetricResult:
     @range_ratio.setter
     def range_ratio(self, value):
         self._range_ratio = value
-
-    def to_dict(self, include_overall=True, include_by_group=True, include_derived=False):
-        """Return a dictionary representation of this object."""
-        result = dict()
-        if include_overall:
-            result[_OVERALL] = copy.deepcopy(self.overall)
-        if include_by_group:
-            result[_BY_GROUP] = copy.deepcopy(self.by_group)
-        if include_derived:
-            result[_MAX] = copy.deepcopy(self.maximum)
-            result[_MIN] = copy.deepcopy(self.minimum)
-            result[_RANGE] = copy.deepcopy(self.range)
-            result[_RANGE_RATIO] = copy.deepcopy(self.range_ratio)
-        return result
-
-    def to_pandas_series(self,
-                         title=None,
-                         include_overall=True,
-                         include_by_group=True,
-                         include_derived=False):
-        """Construct a :class:`pandas:pandas.Series` from the current object.
-
-        If the `by_group` data are included, then the keys are converted to strings
-        prior to insertion into the result. Users are trusted to avoid key
-        collisions.
-        """
-        s = pd.Series()
-        s.name = title
-        if include_by_group:
-            for k, v in self.by_group.items():
-                s[str(k)] = v
-        if include_derived:
-            s['maximum'] = self.maximum
-            s['minimum'] = self.minimum
-            s['range'] = self.range
-            s['range_ratio'] = self.range_ratio
-        if include_overall:
-            s['overall'] = self.overall
-
-        return s
-
-    def __repr__(self):
-        """Generate string representation of a `GroupMetricResult`."""
-        return repr(self.to_dict())
 
     def __eq__(self, other):
         """Compare two `GroupMetricResult` objects for equality."""
