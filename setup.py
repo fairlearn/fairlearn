0a78d86f0854ec6eb26602e1ce44ec359d3fe38b
Roman Lutz <rolutz@microsoft.com>
2020-02-21 12:30:58 -0500

    separate out requirements for py3.5
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Development Status :: 3 - Alpha"
