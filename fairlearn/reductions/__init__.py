c2a886f17aa8ae4035a1e276432879a12146ad46
Abdul Hannan Kanji <hannanabdul55@gmail.com>
2020-02-25 21:04:36 -0500

    equal opprtunity Signed-off-by: Abdul Hannan Kanji <hannanabdul55@gmail.com>

--- fairlearn/reductions/__init__.py
+++ fairlearn/reductions/__init__.py
@@ -11,7 +11,7 @@ learning training.
 from ._exponentiated_gradient import ExponentiatedGradient  # noqa: F401
 from ._grid_search import GridSearch, GridSearchResult  # noqa: F401
 from ._moments import AbsoluteLoss, Moment, ConditionalSelectionRate  # noqa: F401
-from ._moments import DemographicParity, EqualizedOdds, ErrorRate   # noqa: F401
+from ._moments import DemographicParity, EqualizedOdds, ErrorRate, EqualOpportunity   # noqa: F401
 from ._moments import GroupLossMoment, SquareLoss, ZeroOneLoss  # noqa: F401
 from ._moments import ClassificationMoment, LossMoment  # noqa: F401
 
@@ -35,7 +35,8 @@ _moments = [
     "GroupLossMoment",
     "LossMoment",
     "SquareLoss",
-    "ZeroOneLoss"
+    "ZeroOneLoss",
+    "EqualOpportunity"
 ]
 
 __all__ = [] + _exponentiated_gradient + _grid_search + _moments
