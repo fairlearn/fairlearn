4bff3fb5acd69020e519e2ff766ed1beb04a7159
Roman Lutz <rolutz@microsoft.com>
2020-03-04 09:51:46 -0800

    update description of requirements_override.py
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- scripts/requirements_override.py
+++ scripts/requirements_override.py
@@ -1,6 +1,10 @@
 """Script for copying python version-specific requirements files to override requirements.txt.
 
 This is for pipelines only and should not be run by developers on their machines.
+The script checks for a requirements file that's specific to the python minor version, e.g. 3.5,
+and uses those requirements, or if no such file exists the script creates that file based on
+the generic requirements.txt file. Our pipelines rely on the existence of a
+requirements-{major}-{minor}.txt file.
 """
 import argparse
 import os
@@ -11,7 +15,8 @@ from _utils import _LogWrapper
 
 
 def build_argument_parser():
-    desc = "Copy appropriate requirements file"
+    desc = "Copy appropriate requirements file (either generic or python version specific) " \
+           "to requirements-{major}-{minor}.txt"
 
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument("--output", help="Path to store the copied file", required=True)
