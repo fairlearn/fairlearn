ec01cefc8804c889c3eb2070d952a9530af61588
Roman Lutz <rolutz@microsoft.com>
2020-03-05 07:29:56 -0800

    add comment about issue 320
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- test/unit/postprocessing/test_plots.py
+++ test/unit/postprocessing/test_plots.py
@@ -3,7 +3,7 @@
 
 # On MacOS we need to use TKAgg before importing matplotlib.pyplot.
 # This used to work on all platforms until matplotlib 3.2.0 broke it on Linux.
-# Consider removing the OS-based if after a future release of matplotlib.
+# Consider removing the OS-based if after a future release of matplotlib (issue #320).
 import platform
 if platform.system() == "Darwin":
     import matplotlib
