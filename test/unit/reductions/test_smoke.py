406b781f24ce2235764bf878a0040be6c6635736
Roman Lutz <rolutz@microsoft.com>
2020-03-09 11:41:08 -0700

    macos ignore lightgbm
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- test/unit/reductions/test_smoke.py
+++ test/unit/reductions/test_smoke.py
@@ -1,9 +1,9 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-from lightgbm import LGBMClassifier
 import numpy as np
 import pandas as pd
+import platform
 import pytest
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
@@ -13,10 +13,17 @@ from fairlearn.reductions import ExponentiatedGradient, GridSearch, DemographicP
     EqualizedOdds
 
 
+_ESTIMATORS = [LogisticRegression, SVC, DecisionTreeClassifier]
+
+if platform.system() != "Darwin":
+    # MacOS requires extra steps to install lightgbm properly, skipping for now
+    from lightgbm import LGBMClassifier
+    _ESTIMATORS.append(LGBMClassifier)
+
+
 @pytest.mark.parametrize("Mitigator", [ExponentiatedGradient, GridSearch])
 @pytest.mark.parametrize("Constraints", [DemographicParity, EqualizedOdds])
-@pytest.mark.parametrize("Estimator", [LGBMClassifier, LogisticRegression, SVC,
-                                       DecisionTreeClassifier])
+@pytest.mark.parametrize("Estimator", _ESTIMATORS)
 def test_smoke(Mitigator, Constraints, Estimator):
     # This test case ensures that input validation doesn't remove metadata from the input
     # matrix X, as described at https://github.com/fairlearn/fairlearn/issues/312
