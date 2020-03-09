ee5db4266bffd16df5fdd5d09f48490b3643486a
Roman Lutz <rolutz@microsoft.com>
2020-03-09 11:21:55 -0700

    add a few more types of models to smoke test
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- test/unit/reductions/test_smoke.py
+++ test/unit/reductions/test_smoke.py
@@ -1,17 +1,22 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import lightgbm
+from lightgbm import LGBMClassifier
 import numpy as np
 import pandas as pd
 import pytest
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
 from fairlearn.reductions import ExponentiatedGradient, GridSearch, DemographicParity, \
     EqualizedOdds
 
+
 @pytest.mark.parametrize("Mitigator", [ExponentiatedGradient, GridSearch])
 @pytest.mark.parametrize("Constraints", [DemographicParity, EqualizedOdds])
-def test_smoke(Mitigator, Constraints):
+@pytest.mark.parametrize("Estimator", [LGBMClassifier, LogisticRegression, SVC, DecisionTreeClassifier])
+def test_smoke(Mitigator, Constraints, Estimator):
     # This test case ensures that input validation doesn't remove metadata from the input
     # matrix X, as described at https://github.com/fairlearn/fairlearn/issues/312
     np.random.seed(0)
@@ -24,6 +29,6 @@ def test_smoke(Mitigator, Constraints):
     # Set X1 as categorical
     df['X1'] = df['X1'].astype('category')
 
-    mitigator = Mitigator(lightgbm.LGBMClassifier(), Constraints())
+    mitigator = Mitigator(Estimator(), Constraints())
     mitigator.fit(df, Y, sensitive_features=A)
     mitigator.predict(df)
