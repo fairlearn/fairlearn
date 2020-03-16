8b1b6858dfc3f7cf2876579f098c8a82046c7f43
Richard Edgar <riedgar@microsoft.com>
2020-03-16 11:47:24 -0400

    Add a more complicated test
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- test/unit/metrics/test_dashboard_dictionary.py
+++ test/unit/metrics/test_dashboard_dictionary.py
@@ -13,7 +13,7 @@ def test_create_dashboard_dictionary_smoke():
     g_name = "my group"
     model_type = 'binary_classification'
 
-    result = create_dashboard_dictionary(
+    actual = create_dashboard_dictionary(
         model_type,
         y_true,
         {m_name: y_pred},
@@ -26,4 +26,38 @@ def test_create_dashboard_dictionary_smoke():
                                        model_titles=[m_name],
                                        sensitive_feature_names=[g_name])
 
-    assert result == expected
+    assert expected == actual
+
+
+def test_create_dashboard_dictionary_multiple_models_multiple_sensitive_features():
+    y_true = [0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0]
+
+    y_p1 = [0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1]
+    y_p2 = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1]
+    y_p3 = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0]
+    m_1 = "M1"
+    m_2 = "M2"
+    m_3 = "M3"
+    models = {m_1: y_p1, m_2: y_p2, m_3: y_p3}
+
+    sf_1 = ['a', 'b', 'b', 'a', 'b', 'b', 'b', 'a', 'b', 'b', 'b']
+    sf_2 = [4, 5, 6, 6, 5, 4, 4, 5, 5, 6, 6]
+    n_1 = "sf 1"
+    n_2 = "sf 2"
+    sensitive_features = {n_1: sf_1, n_2: sf_2}
+
+    model_type = 'binary_classification'
+
+    actual = create_dashboard_dictionary(model_type,
+                                         y_true,
+                                         models,
+                                         sensitive_features)
+
+    expected = create_group_metric_set(model_type,
+                                       y_true,
+                                       [y_p1, y_p2, y_p3],
+                                       [sf_1, sf_2],
+                                       model_titles=[m_1, m_2, m_3],
+                                       sensitive_feature_names=[n_1, n_2])
+
+    assert expected == actual
