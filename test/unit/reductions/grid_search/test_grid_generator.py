1940b1ebf1656be7a692e250cd782102d9e6457f
Roman Lutz <rolutz@microsoft.com>
2020-03-23 12:18:18 -0700

    generalize tests to parametrize them
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- test/unit/reductions/grid_search/test_grid_generator.py
+++ test/unit/reductions/grid_search/test_grid_generator.py
@@ -11,22 +11,25 @@ from fairlearn.reductions import DemographicParity
 from test.unit.reductions.grid_search import _utilities
 
 
-@pytest.mark.parametrize("grid_size", [10, 11])
-def test_grid_generator(grid_size):
+@pytest.mark.parametrize("grid_size", [5, 6, 10, 11, 100, 101, 102])
+@pytest.mark.parametrize("grid_limit", [1.0, 2.0, 4.0, 10.0])
+def test_grid_generator(grid_size, grid_limit):
     X, y, A = _utilities._quick_data()
 
     disparity_moment = DemographicParity()
     disparity_moment.load_data(X, y, sensitive_features=A)
 
-    grid = _GridGenerator(grid_size, 2.0, disparity_moment.pos_basis, disparity_moment.neg_basis,
+    grid = _GridGenerator(grid_size, grid_limit,
+                          disparity_moment.pos_basis, disparity_moment.neg_basis,
                           disparity_moment.neg_basis_present, False).grid
 
     expected_index = pd.MultiIndex.from_product([['+', '-'], ['all'], [0, 1]],
                                                 names=['sign', 'event', 'group_id'])
     assert (expected_index == grid.index).all()
     expected_grid = pd.DataFrame()
+    step_size = 2 * grid_limit / (int(grid_size / 2) * 2)
     for i in range(grid_size):
         expected_grid[i] = pd.Series(0.0, index=expected_index)
-        expected_grid[i]['-', 'all', 1] = max(2 - 0.4 * i, 0)
-        expected_grid[i]['+', 'all', 1] = max(-2 + 0.4 * i, 0)
+        expected_grid[i]['-', 'all', 1] = max(grid_limit - step_size * i, 0)
+        expected_grid[i]['+', 'all', 1] = max(-grid_limit + step_size * i, 0)
     assert np.isclose(expected_grid.values, grid.values).all()
