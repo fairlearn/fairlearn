ef4f5d40c160914822998ae71d13e237a0f209a7
Parul Gupta <parul100495@gmail.com>
2020-04-14 19:28:06 -0400

    Fix flake8 test.
    Signed-off-by: Parul Gupta <parul100495@gmail.com>

--- fairlearn/reductions/_moments/__init__.py
+++ fairlearn/reductions/_moments/__init__.py
@@ -7,8 +7,7 @@ from .moment import Moment  # noqa: F401
 from .moment import ClassificationMoment, LossMoment  # noq: F401
 
 from .conditional_selection_rate import ConditionalSelectionRate  # noqa: F401
-from .conditional_selection_rate import DemographicParity, EqualizedOdds, \
-    TruePositiveRateDifference, ErrorRateRatio  # noqa: F401
+from .conditional_selection_rate import DemographicParity, EqualizedOdds, TruePositiveRateDifference, ErrorRateRatio  # noqa: F401,E501
 from .error_rate import ErrorRate   # noqa: F401
 from .bounded_group_loss import GroupLossMoment   # noqa: F401
 from .bounded_group_loss import SquareLoss, AbsoluteLoss, ZeroOneLoss  # noqa: F401
