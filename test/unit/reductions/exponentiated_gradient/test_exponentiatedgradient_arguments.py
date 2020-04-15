e6db93649744a73eff9e370c6f6d3a6e29c71c67
Roman Lutz <rolutz@microsoft.com>
2020-04-14 18:21:25 -0700

    flake8
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- test/unit/reductions/exponentiated_gradient/test_exponentiatedgradient_arguments.py
+++ test/unit/reductions/exponentiated_gradient/test_exponentiatedgradient_arguments.py
@@ -1,10 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import numpy as np
-import pandas as pd
 import pytest
-
 
 from fairlearn.reductions import ExponentiatedGradient
 from fairlearn.reductions import DemographicParity
