ea249f6a4694e2be08568cbc3613dcd64c3fdfc7
Richard Edgar <riedgar@microsoft.com>
2020-04-29 19:15:29 -0400

    More experimenting
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- fairlearn/__init__.py
+++ fairlearn/__init__.py
@@ -11,7 +11,7 @@ from .show_versions import show_versions  # noqa: F401
 # Finesse the version
 _FAIRLEARN_DEV_VERSION_ENV_VAR = "FAIRLEARN_DEV_VERSION"
 
-_base_version = "0.4.5.dev4102"
+_base_version = "0.4.5.dev4103"
 _dev_version = ""
 
 if _FAIRLEARN_DEV_VERSION_ENV_VAR in os.environ.keys():
