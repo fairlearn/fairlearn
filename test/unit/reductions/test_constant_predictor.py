6d45c1fcb9e8680df06f5bc61be81f26eaadd399
Richard Edgar <riedgar@microsoft.com>
2020-05-04 14:29:44 -0400

    Add a check or two more
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- test/unit/reductions/test_constant_predictor.py
+++ test/unit/reductions/test_constant_predictor.py
@@ -16,3 +16,4 @@ def test_smoke():
     assert cp.predict(np.zeros(10)) == const_value
     assert cp.predict(X="a", sample_weights=[1, 2, 3]) == const_value
     assert cp.predict([1, 2, 3], sample_weights=[4, 5, 6]) == const_value
+    assert cp.predict(c=np.zeros(10), d='a string', e=1.0) == const_value
