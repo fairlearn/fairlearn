2cd434d48e9bd6a761c5717849c5469a4183a950
Lauren Pendo <lpendo@stanford.edu>
2020-05-27 20:47:08 -0700

    updated group loss to specify kwargs

--- fairlearn/reductions/_moments/bounded_group_loss.py
+++ fairlearn/reductions/_moments/bounded_group_loss.py
@@ -91,7 +91,7 @@ class GroupLossMoment(ConditionalLossMoment):
     """Moment for Group Loss."""
 
     def __init__(self, loss, upper_bound=None):
-        super().__init__(loss, upper_bound, no_groups=False)
+        super().__init__(loss, upper_bound=upper_bound, no_groups=False)
 
 
 class SquareLoss:
