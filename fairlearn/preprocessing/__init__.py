ca7f847860f39107e6998dc6af4f42ea376bc760
Vincent <vincentwarmerdam@gmail.com>
2020-06-06 13:31:15 +0200

    flake8
    Signed-off-by: Vincent <vincentwarmerdam@gmail.com>

--- fairlearn/preprocessing/__init__.py
+++ fairlearn/preprocessing/__init__.py
@@ -1,3 +1,10 @@
+# Copyright (c) Microsoft Corporation and contributors.
+# Licensed under the MIT License.
+
+"""This module contains algorithms implementing preprocessing steps that might help
+filter information that correlates with sensitive attributes.
+"""
+
 from ._informationfilter import InformationFilter
 
 __all__ = ["InformationFilter"]
