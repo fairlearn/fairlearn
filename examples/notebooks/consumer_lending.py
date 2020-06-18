aeab3377a99bafc52be45b6537287ef3e3e11c83
kevinrobinson <kevin.robinson.0@gmail.com>
2020-06-18 16:23:53 -0400

    Notebook: First draft towards consumer lending
    Signed-off-by: kevinrobinson <kevin.robinson.0@gmail.com>

---
+++ examples/notebooks/consumer_lending.py
@@ -0,0 +1,22 @@
+"""
+=======================================
+DRAFT: Consumer lending
+=======================================
+"""
+print(__doc__)
+
+###############################################################################
+# Context
+# ------------------------------
+# You're working at an established financial services company that makes consumer
+# auto loans.  There are many existing processes in place, and the environment is
+# highly regulated.  Your manager has asked you to collaborate on assessing
+# fairness in the existing approach, and to help with mitigating any issues that come
+# up.
+# 
+# The existing screening system is staffed by a team of human reviewers.  The initial
+# reviewers make a "loan adjudication" decisions to reject or accept the application.
+# They can also flag a small percentage of applications for addition scrutiny by more
+# senior reviewers.
+#
+# ... this is still just a first draft! ...
