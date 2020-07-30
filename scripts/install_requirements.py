c02f51c4dc98b0c6b9d87dd292cf120abed9246a
Richard Edgar <riedgar@microsoft.com>
2020-07-30 09:51:24 -0400

    Roughing out installation program
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

---
+++ scripts/install_requirements.py
@@ -0,0 +1,95 @@
+"""Script to install the requirements files, optionally pinning versions."""
+
+import argparse
+import logging
+import os
+import sys
+
+from _utils import _LogWrapper
+
+_REQUIREMENTS_STEMS = [
+    "requirements",
+    "requirements-customplots",
+    "requirements-dev"
+]
+
+_INSERTION_FIXED = "-fixed"
+
+_REQUIREMENTS_EXTENSION = "txt"
+
+
+_logger = logging.getLogger(__file__)
+logging.basicConfig(level=logging.INFO)
+
+
+def _build_argument_parser():
+    desc = "Install requirements using pip"
+
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument("--fixed",
+                        help="Whether to convert '>=' to '==' in files",
+                        type=lambda x: (str(x).lower() == 'true'),
+                        required=True)
+    parser.add_argument("--loglevel",
+                        help="Set log level",
+                        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'])
+
+    return parser
+
+
+def _process_line(src_line):
+    return src_line.replace('>=', '==')
+
+
+def _pin_requirements(src_file, dst_file):
+    with _LogWrapper("Pinning {0} into {1}".format(src_file, dst_file)):
+
+        _logger.debug("Reading file %s", src_file)
+        text_lines = []
+        with open(src_file, 'r') as f:
+            text_lines = f.readlines()
+
+        result_lines = [_process_line(current_line) for current_line in text_lines]
+
+        _logger.debug("Writing file %s", dst_file)
+        with open(dst_file, 'w') as f:
+            f.writelines(result_lines)
+
+
+def _install_requirements_file(file_stem, fix_requirements):
+    _logger.info("Processing %s", file_stem)
+
+    if fix_requirements:
+        source_file = "{0}.{1}".format(file_stem, _REQUIREMENTS_EXTENSION)
+        requirements_file = "{0}{1}.{2}".format(file_stem,
+                                                _INSERTION_FIXED,
+                                                _REQUIREMENTS_EXTENSION)
+        _pin_requirements(source_file, requirements_file)
+    else:
+        requirements_file = "{0}.{1}".format(file_stem, _REQUIREMENTS_EXTENSION)
+
+    with _LogWrapper("Running pip on {0}".format(requirements_file)):
+        commands = ["pip", "install", "-r", requirements_file]
+        print(commands, sep=" ")
+
+    if fix_requirements:
+        _logger.info("Removing temporary file %s", requirements_file)
+        os.remove(requirements_file)
+
+
+def main(argv):
+    parser = _build_argument_parser()
+    args = parser.parse_args(argv)
+
+    if args.loglevel:
+        logging.basicConfig(level=getattr(logging, args.loglevel))
+
+    _logger.info("Fixed set to: %s", args.fixed)
+
+    for rs in _REQUIREMENTS_STEMS:
+        _install_requirements_file(rs, args.fixed)
+    _logger.info("All requirements files installed")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
