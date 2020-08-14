c4c3762f9a12a4e303662ee9387196f85331c0ed
Richard Edgar <riedgar@microsoft.com>
2020-08-14 16:51:53 -0400

    Use our metrics to calculate the plot
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- examples/plot_adult_dataset.py
+++ examples/plot_adult_dataset.py
@@ -3,10 +3,10 @@
 Plotting the UCI Adult Dataset
 ==============================
 """
-print(__doc__)
-
-from fairlearn.datasets import fetch_adult
 import matplotlib.pyplot as plt
+from fairlearn.metrics import selection_rate_group_summary
+from fairlearn.datasets import fetch_adult
+print(__doc__)
 
 
 data = fetch_adult(as_frame=True)
@@ -14,12 +14,9 @@ X = data.data
 y_true = (data.target == '>50K') * 1
 sex = X['sex']
 
+selection_rates = selection_rate_group_summary(y_true, y_true, sensitive_features=sex)
 
-def percentage_with_label_1(sex_value):
-    return y_true[sex == sex_value].sum() / (sex == sex_value).sum()
-
-
-plt.bar([0, 1], [percentage_with_label_1("Female"), percentage_with_label_1("Male")], color='g')
+plt.bar([0, 1], [selection_rates.by_group["Female"], selection_rates.by_group["Male"]], color='g')
 plt.xticks([0, 1], ["Female", "Male"])
 plt.ylabel("percentage earning over $50,000")
 plt.xlabel("sex")
