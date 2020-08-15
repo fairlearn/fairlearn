b3520375f959188e5e50260e6e964cf2da780548
Richard Edgar <riedgar@microsoft.com>
2020-08-15 19:45:18 -0400

    Need to trigger CircleCI again
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- fairlearn/__init__.py
+++ fairlearn/__init__.py
@@ -12,7 +12,6 @@ __name__ = "fairlearn"
 __version__ = "0.4.7.dev0"
 _base_version = __version__
 
-
 # Setup logging infrastructure
 # Only log to disk if environment variable FAIRLEARN_LOGS specified
 fairlearn_logs = os.environ.get('FAIRLEARN_LOGS')
