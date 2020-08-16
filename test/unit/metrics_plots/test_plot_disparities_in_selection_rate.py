a8b248ec71151f671202b1a4a5ab3b427fe17b1c
kevinrobinson <kevin.robinson.0@gmail.com>
2020-08-16 09:29:41 -0400

    Fix flake bits
    Signed-off-by: kevinrobinson <kevin.robinson.0@gmail.com>

--- test/unit/metrics_plots/test_plot_disparities_in_selection_rate.py
+++ test/unit/metrics_plots/test_plot_disparities_in_selection_rate.py
@@ -5,8 +5,8 @@ from fairlearn.metrics_plots import plot_disparities_in_selection_rate
 from .conftest import quickstart_setup
 
 
-# These are just smoke tests.
-# For ways to improve testing, see https://github.com/fairlearn/fairlearn/pull/289#issuecomment-587170978
+# These are just smoke tests.  For ways to improve testing,
+# see https://github.com/fairlearn/fairlearn/pull/289#issuecomment-587170978
 def test_plot_disparities_in_selection_rate():
     (y_true, y_pred, sensitive_features) = quickstart_setup()
     plot_disparities_in_selection_rate(y_true, y_pred, sensitive_features)
