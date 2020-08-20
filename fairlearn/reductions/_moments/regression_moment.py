669d480c67ca828c07efef7d5dd04375129f4e08
Acorn <wanglj17@mails.tsinghua.edu.cn>
2020-08-20 14:14:13 +0800

    add the copyright notice for these two files
    Signed-off-by: Acorn <wanglj17@mails.tsinghua.edu.cn>

--- fairlearn/reductions/_moments/regression_moment.py
+++ fairlearn/reductions/_moments/regression_moment.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation and contributors.
+# Licensed under the MIT License.
+
 from .utility_parity import UtilityParity
 from .moment import Moment,_LABEL,_GROUP_ID
 from .weighted_error_rate import WeightedErrorRate
--- fairlearn/reductions/_moments/weighted_error_rate.py
+++ fairlearn/reductions/_moments/weighted_error_rate.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation and contributors.
+# Licensed under the MIT License.
+
 import pandas as pd
 import numpy as np
 from itertools import repeat
