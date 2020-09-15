03083c8dc2324c1eea6d97a89e1825bd348d85c1
Roman Lutz <rolutz@microsoft.com>
2020-09-15 15:54:32 -0700

    fix selection_rate for y_pred of length 0 or 1
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- fairlearn/metrics/_selection_rate.py
+++ fairlearn/metrics/_selection_rate.py
@@ -9,7 +9,18 @@ def selection_rate(y_true, y_pred, *, pos_label=1, sample_weight=None):
 
     The argument `pos_label` specifies the 'good' outcome.
     """
-    selected = (np.squeeze(np.asarray(y_pred)) == pos_label)
+    y_pred_arr = np.asarray(y_pred)
+
+    if y_pred_arr.shape == (0,):
+        raise ValueError("Empty y_pred passed to selection_rate function.")
+
+    y_pred_arr = np.squeeze(y_pred_arr)
+    if y_pred_arr.shape == ():
+        # not actually an array, but rather a single entry that was compressed
+        # into a scalar by the squeeze call
+        return (y_pred_arr == pos_label) * 1.0
+
+    selected = (y_pred_arr == pos_label)
     s_w = np.ones(len(selected))
     if sample_weight is not None:
         s_w = np.squeeze(np.asarray(sample_weight))
