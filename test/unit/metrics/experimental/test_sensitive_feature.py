9841a2851c45c8abbd25afff76b6b895322fceb0
Richard Edgar <riedgar@microsoft.com>
2020-09-15 11:12:13 -0400

    Finish off basic SensitiveFeature tests
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- test/unit/metrics/experimental/test_sensitive_feature.py
+++ test/unit/metrics/experimental/test_sensitive_feature.py
@@ -11,15 +11,47 @@ raw_feature = ['a', 'b', 'c', 'a']
 expected_classes = ['a', 'b', 'c']
 
 
+def common_validations(sf):
+    assert np.array_equal(expected_classes, sf.classes)
+
+    mask = sf.get_mask_for_class('a')
+    assert np.array_equal(mask,  [True, False, False, True])
+    mask_b = sf.get_mask_for_class('b')
+    assert np.array_equal(mask_b, [False, True, False, False])
+    mask_c = sf.get_mask_for_class('c')
+    assert np.array_equal(mask_c, [False, False, True, False])
+
+
 def test_list():
-    target = metrics._sensitive_feature.SensitiveFeature(raw_feature, 0)
+    target = metrics.SensitiveFeature(raw_feature, 0)
 
     assert target.name == "SF 0"
-    assert np.array_equal(expected_classes, target.classes)
+    common_validations(target)
 
-    mask = target.get_mask_for_class('a')
-    assert np.array_equal(mask,  [True, False, False, True])
-    mask_b = target.get_mask_for_class('b')
-    assert np.array_equal(mask_b, [False, True, False, False])
-    mask_c = target.get_mask_for_class('c')
-    assert np.array_equal(mask_c, [False, False, True, False])
+
+def test_ndarray():
+    rf = np.asarray(raw_feature)
+
+    target = metrics.SensitiveFeature(rf, 1)
+
+    assert target.name == "SF 1"
+    common_validations(target)
+
+
+def test_unnamed_Series():
+    rf = pd.Series(data=raw_feature)
+
+    target = metrics.SensitiveFeature(rf, 2)
+
+    assert target.name == "SF 2"
+    common_validations(target)
+
+
+def test_named_Series():
+    name = "My Feature"
+    rf = pd.Series(data=raw_feature, name=name)
+
+    target = metrics.SensitiveFeature(rf, 2)
+
+    assert target.name == name
+    common_validations(target)
