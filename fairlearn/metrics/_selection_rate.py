20f24a99d44e29e109130e9a94b4d1e05ba1ba77
Roman Lutz <rolutz@microsoft.com>
2020-09-16 15:05:15 -0700

    redo previous change without UX files, and add simple tests
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- fairlearn/metrics/_selection_rate.py
+++ fairlearn/metrics/_selection_rate.py
@@ -3,24 +3,21 @@
 
 import numpy as np
 
+from fairlearn.metrics._input_manipulations import _convert_to_ndarray_and_squeeze
+
+
+_EMPTY_INPUT_PREDICTIONS_ERROR_MESSAGE = "Empty y_pred passed to selection_rate function."
+
 
 def selection_rate(y_true, y_pred, *, pos_label=1, sample_weight=None):
     """Calculate the fraction of predicted labels matching the 'good' outcome.
 
     The argument `pos_label` specifies the 'good' outcome.
     """
-    y_pred_arr = np.asarray(y_pred)
-
-    if y_pred_arr.shape == (0,):
-        raise ValueError("Empty y_pred passed to selection_rate function.")
-
-    y_pred_arr = np.squeeze(y_pred_arr)
-    if y_pred_arr.shape == ():
-        # not actually an array, but rather a single entry that was compressed
-        # into a scalar by the squeeze call
-        return (y_pred_arr == pos_label) * 1.0
-
-    selected = (y_pred_arr == pos_label)
+    if len(y_pred) == 0:
+        raise ValueError(_EMPTY_INPUT_PREDICTIONS_ERROR_MESSAGE)
+    
+    selected = (_convert_to_ndarray_and_squeeze(y_pred) == pos_label)
     s_w = np.ones(len(selected))
     if sample_weight is not None:
         s_w = np.squeeze(np.asarray(sample_weight))
