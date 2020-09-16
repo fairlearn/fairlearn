37d05903af9ea88725816efcbbb65a73541301f8
Richard Edgar <riedgar@microsoft.com>
2020-09-16 09:10:21 -0400

    Add flake8 check for copyright header (#575)
    Add the `flake8-copyright` package, and enable checking for the presence of a copyright header.
    
    As a result of this:
    - Pick 'Fairlearn' contributors' as the standard (rather than just 'contributors')
    - Add missing header to some files
    
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- fairlearn/postprocessing/_interpolated_thresholder.py
+++ fairlearn/postprocessing/_interpolated_thresholder.py
@@ -1,4 +1,4 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
+# Copyright (c) Microsoft Corporation and Fairlearn contributors.
 # Licensed under the MIT License.
 
 import numpy as np
