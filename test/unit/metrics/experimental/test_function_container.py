a045470d6e6954051a1487ac48a0c3282a272883
Richard Edgar <riedgar@microsoft.com>
2020-09-16 13:35:14 -0400

    Another little test
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- test/unit/metrics/experimental/test_function_container.py
+++ test/unit/metrics/experimental/test_function_container.py
@@ -18,3 +18,19 @@ def test_constructor_unnamed():
     assert fc.params['pos_label'] == 0
     assert isinstance(fc.params['sample_weight'], np.ndarray)
     assert np.array_equal(fc.params['sample_weight'], [1, 2, 3])
+
+
+def test_constructor_named():
+    sample_param_names = ['sample_weight']
+    params = {'pos_label': 0, 'sample_weight': [1, 2, 3]}
+
+    fc = FunctionContainer(recall_score, "OverrideName", sample_param_names, params)
+    assert fc.name == "OverrideName"
+    assert len(fc.sample_param_names) == 1
+    assert fc.sample_param_names[0] == 'sample_weight'
+    assert len(fc.params) == 2
+    assert fc.params['pos_label'] == 0
+    assert isinstance(fc.params['sample_weight'], np.ndarray)
+    assert np.array_equal(fc.params['sample_weight'], [1, 2, 3])
+
+
