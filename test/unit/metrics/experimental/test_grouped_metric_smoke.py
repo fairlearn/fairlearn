7e61018ef881665c9278bf77a336865474cfbda8
Richard Edgar <riedgar@microsoft.com>
2020-09-16 16:05:22 -0400

    Test for condition feature and two metrics
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- test/unit/metrics/experimental/test_grouped_metric_smoke.py
+++ test/unit/metrics/experimental/test_grouped_metric_smoke.py
@@ -10,13 +10,13 @@ import fairlearn.metrics.experimental as metrics
 # ============================================================
 
 
-y_t_A = [0, 1, 1, 1, 0, 0, 1, 1, 0]
-y_p_A = [1, 1, 1, 0, 0, 0, 0, 1, 0]
-wgt_A = [1, 2, 1, 2, 1, 2, 1, 3, 4]
+y_t_A = [0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1]
+y_p_A = [1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0]
+wgt_A = [1, 2, 1, 2, 1, 2, 1, 3, 4, 1, 2, 1, 2, 1, 2, 1, 2, 5]
 As = np.full(len(y_t_A), 'A')
-y_t_B = [0, 1, 1, 0, 0, 0, 0, 1]
-y_p_B = [0, 1, 0, 1, 0, 1, 0, 1]
-wgt_B = [2, 2, 1, 1, 3, 3, 4, 1]
+y_t_B = [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0]
+y_p_B = [0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1]
+wgt_B = [2, 2, 1, 1, 3, 3, 4, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 1]
 Bs = np.full(len(y_t_B), 'B')
 
 y_t = np.concatenate((y_t_A, y_t_B))
@@ -196,3 +196,112 @@ def test_two_metrics():
     assert target.by_group[('precision_score',)][('A',)] == precision_A
     precision_B = skm.precision_score(y_t_B, y_p_B, sample_weight=wgt_B)
     assert target.by_group[('precision_score',)][('B',)] == precision_B
+
+
+def test_two_metrics_two_sensitive_features():
+    sf_2 = ['x' if (x % 2) == 0 else 'y' for x in range(len(y_t))]
+
+    target = metrics.GroupedMetric([skm.recall_score, skm.precision_score],
+                                   y_t, y_p,
+                                   sensitive_features=[gid, sf_2])
+
+    recall_overall = skm.recall_score(y_t, y_p)
+    assert target.overall['recall_score'][0] == recall_overall
+    precision_overall = skm.precision_score(y_t, y_p)
+    assert target.overall['precision_score'][0] == precision_overall
+
+    A_x_mask = [x % 2 == 0 for x in range(len(y_t_A))]
+    A_y_mask = [x % 2 == 1 for x in range(len(y_t_A))]
+    y_t_A_x = np.asarray(y_t_A)[A_x_mask]
+    y_p_A_x = np.asarray(y_p_A)[A_x_mask]
+    expected_A_x = skm.recall_score(y_t_A_x, y_p_A_x)
+    assert target.by_group[('recall_score',)][('A', 'x')] == expected_A_x
+    expected_A_x = skm.precision_score(y_t_A_x, y_p_A_x)
+    assert target.by_group[('precision_score',)][('A', 'x')] == expected_A_x
+    y_t_A_y = np.asarray(y_t_A)[A_y_mask]
+    y_p_A_y = np.asarray(y_p_A)[A_y_mask]
+    expected_A_y = skm.recall_score(y_t_A_y, y_p_A_y)
+    assert target.by_group[('recall_score',)][('A', 'y')] == expected_A_y
+    expected_A_y = skm.precision_score(y_t_A_y, y_p_A_y)
+    assert target.by_group[('precision_score',)][('A', 'y')] == expected_A_y
+
+    # Rely on B's being after the A's, but don't assume even numbers
+    B_x_mask = [x % 2 == 0 for x in range(len(y_t_A), len(y_t))]
+    B_y_mask = [x % 2 == 1 for x in range(len(y_t_A), len(y_t))]
+    y_t_B_x = np.asarray(y_t_B)[B_x_mask]
+    y_p_B_x = np.asarray(y_p_B)[B_x_mask]
+    expected_B_x = skm.recall_score(y_t_B_x, y_p_B_x)
+    assert target.by_group[('recall_score',)][('B', 'x')] == expected_B_x
+    expected_B_x = skm.precision_score(y_t_B_x, y_p_B_x)
+    assert target.by_group[('precision_score',)][('B', 'x')] == expected_B_x
+    y_t_B_y = np.asarray(y_t_B)[B_y_mask]
+    y_p_B_y = np.asarray(y_p_B)[B_y_mask]
+    expected_B_y = skm.recall_score(y_t_B_y, y_p_B_y)
+    assert target.by_group[('recall_score',)][('B', 'y')] == expected_B_y
+    expected_B_y = skm.precision_score(y_t_B_y, y_p_B_y)
+    assert target.by_group[('precision_score',)][('B', 'y')] == expected_B_y
+
+
+def test_two_metrics_single_conditional_feature():
+    cf = ['x' if (x % 2) == 0 else 'y' for x in range(len(y_t))]
+    cf_x_mask = [(x % 2) == 0 for x in range(len(y_t))]
+    cf_y_mask = [(x % 2) == 1 for x in range(len(y_t))]
+
+    fns = [skm.recall_score, skm.precision_score]
+    sample_param_names = [['sample_weight'], ['sample_weight']]
+    params = [{'sample_weight': wgt},
+              {'sample_weight': wgt}]
+    target = metrics.GroupedMetric(fns,
+                                   y_t, y_p,
+                                   conditional_features=[cf],
+                                   sensitive_features=[gid],
+                                   sample_param_names=sample_param_names,
+                                   params=params)
+
+    overall_x = skm.recall_score(y_t[cf_x_mask], y_p[cf_x_mask], sample_weight=wgt[cf_x_mask])
+    assert target.overall['recall_score'][('x',)] == overall_x
+    overall_y = skm.recall_score(y_t[cf_y_mask], y_p[cf_y_mask], sample_weight=wgt[cf_y_mask])
+    assert target.overall['recall_score'][('y',)] == overall_y
+    
+    overall_x = skm.precision_score(y_t[cf_x_mask], y_p[cf_x_mask], sample_weight=wgt[cf_x_mask])
+    assert target.overall['precision_score'][('x',)] == overall_x
+    overall_y = skm.precision_score(y_t[cf_y_mask], y_p[cf_y_mask], sample_weight=wgt[cf_y_mask])
+    assert target.overall['precision_score'][('y',)] == overall_y
+
+    # This will look somewhat familiar from above.... mathematically
+    # there's no difference between conditional and sensitive features
+    # The difference is how they are inserted into the table
+    A_x_mask = [x % 2 == 0 for x in range(len(y_t_A))]
+    A_y_mask = [x % 2 == 1 for x in range(len(y_t_A))]
+    y_t_A_x = np.asarray(y_t_A)[A_x_mask]
+    y_p_A_x = np.asarray(y_p_A)[A_x_mask]
+    wgt_A_x = np.asarray(wgt_A)[A_x_mask]
+    expected_A_x = skm.recall_score(y_t_A_x, y_p_A_x, sample_weight=wgt_A_x)
+    assert target.by_group[('recall_score', 'x')][('A',)] == expected_A_x
+    expected_A_x = skm.precision_score(y_t_A_x, y_p_A_x, sample_weight=wgt_A_x)
+    assert target.by_group[('precision_score', 'x')][('A',)] == expected_A_x
+    y_t_A_y = np.asarray(y_t_A)[A_y_mask]
+    y_p_A_y = np.asarray(y_p_A)[A_y_mask]
+    wgt_A_y = np.asarray(wgt_A)[A_y_mask]
+    expected_A_y = skm.recall_score(y_t_A_y, y_p_A_y, sample_weight=wgt_A_y)
+    assert target.by_group[('recall_score', 'y')][('A',)] == expected_A_y
+    expected_A_y = skm.precision_score(y_t_A_y, y_p_A_y, sample_weight=wgt_A_y)
+    assert target.by_group[('precision_score', 'y')][('A',)] == expected_A_y
+
+    # Rely on B's being after the A's, but don't assume even numbers
+    B_x_mask = [x % 2 == 0 for x in range(len(y_t_A), len(y_t))]
+    B_y_mask = [x % 2 == 1 for x in range(len(y_t_A), len(y_t))]
+    y_t_B_x = np.asarray(y_t_B)[B_x_mask]
+    y_p_B_x = np.asarray(y_p_B)[B_x_mask]
+    wgt_B_x = np.asarray(wgt_B)[B_x_mask]
+    expected_B_x = skm.recall_score(y_t_B_x, y_p_B_x, sample_weight=wgt_B_x)
+    assert target.by_group[('recall_score', 'x')][('B',)] == expected_B_x
+    expected_B_x = skm.precision_score(y_t_B_x, y_p_B_x, sample_weight=wgt_B_x)
+    assert target.by_group[('precision_score', 'x')][('B',)] == expected_B_x
+    y_t_B_y = np.asarray(y_t_B)[B_y_mask]
+    y_p_B_y = np.asarray(y_p_B)[B_y_mask]
+    wgt_B_y = np.asarray(wgt_B)[B_y_mask]
+    expected_B_y = skm.recall_score(y_t_B_y, y_p_B_y, sample_weight=wgt_B_y)
+    assert target.by_group[('recall_score', 'y')][('B',)] == expected_B_y
+    expected_B_y = skm.precision_score(y_t_B_y, y_p_B_y, sample_weight=wgt_B_y)
+    assert target.by_group[('precision_score', 'y')][('B',)] == expected_B_y
