ef7bdd349d9b48627b49e8b57038b58a37f35a90
Roman Lutz <rolutz@microsoft.com>
2020-09-16 20:00:48 -0700

    fix selection_rate for y_pred of length 0 or 1 (#576)
    * fix selection_rate for y_pred of length 0 or 1
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * redo previous change without UX files, and add simple tests
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * flake8
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- fairlearn/metrics/_selection_rate.py
+++ fairlearn/metrics/_selection_rate.py
@@ -3,13 +3,21 @@
 
 import numpy as np
 
+from fairlearn.metrics._input_manipulations import _convert_to_ndarray_and_squeeze
+
+
+_EMPTY_INPUT_PREDICTIONS_ERROR_MESSAGE = "Empty y_pred passed to selection_rate function."
+
 
 def selection_rate(y_true, y_pred, *, pos_label=1, sample_weight=None):
     """Calculate the fraction of predicted labels matching the 'good' outcome.
 
     The argument `pos_label` specifies the 'good' outcome.
     """
-    selected = (np.squeeze(np.asarray(y_pred)) == pos_label)
+    if len(y_pred) == 0:
+        raise ValueError(_EMPTY_INPUT_PREDICTIONS_ERROR_MESSAGE)
+
+    selected = (_convert_to_ndarray_and_squeeze(y_pred) == pos_label)
     s_w = np.ones(len(selected))
     if sample_weight is not None:
         s_w = np.squeeze(np.asarray(sample_weight))
