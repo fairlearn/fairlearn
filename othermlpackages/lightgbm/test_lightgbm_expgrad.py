224eb8806afc5ad5eb29115f3620f24c4c122b82
Richard Edgar <riedgar@microsoft.com>
2020-09-18 09:22:14 -0400

    First pass at getting test running
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- othermlpackages/lightgbm/test_lightgbm_expgrad.py
+++ othermlpackages/lightgbm/test_lightgbm_expgrad.py
@@ -4,19 +4,33 @@
 from copy import deepcopy
 import numpy as np
 import pandas as pd
-import pytest
 
-from fairlearn.datasets import fetch_adult
+from sklearn.datasets import fetch_openml
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
 from fairlearn.reductions import ExponentiatedGradient
 from fairlearn.reductions import DemographicParity
 
 import lightgbm as lgb
 
 def test_smoke():
-    data = fetch_adult(as_frame=True)
-    X = data.data
-    y_true = (data.target == '>50K') * 1
-    sex = X['sex']
+
+    data = fetch_openml(data_id=1590, as_frame=True)
+    X = data.data.drop(labels=['sex'], axis=1)
+    X = pd.get_dummies(X)
+    Y = (data.target == '>50K') * 1
+    A = data.data['sex']
+    A = pd.get_dummies(A)
+    
+    le = LabelEncoder()
+    Y = le.fit_transform(Y)
+
+    le = LabelEncoder()
+    
+
+    sc = StandardScaler()
+    X_scaled = sc.fit_transform(X)
+    X_scaled = pd.DataFrame(X_scaled, columns=X.columns)
 
     lgb_params = {
         'objective' : 'binary',
@@ -34,6 +48,6 @@ def test_smoke():
             constraints=deepcopy(disparity_moment),
             eps=eps)
 
-    expgrad.fit(X, y_true, sensitive_features=sex)
+    expgrad.fit(X_scaled, Y, sensitive_features=A)
     assert expgrad.n_oracle_calls_ > 1
     assert len(expgrad.predictors_) > 1
