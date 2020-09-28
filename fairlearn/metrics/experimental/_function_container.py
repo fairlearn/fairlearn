caf00ce05a39b485d733bb6902ad3b5b7cf63bd6
Richard Edgar <riedgar@microsoft.com>
2020-09-28 14:09:46 -0400

    Remove params= from the FunctionContainer
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- fairlearn/metrics/experimental/_function_container.py
+++ fairlearn/metrics/experimental/_function_container.py
@@ -8,7 +8,7 @@ import numpy as np
 class FunctionContainer:
     """Read a placeholder comment."""
 
-    def __init__(self, func, name, sample_params, params):
+    def __init__(self, func, name, sample_params):
         """Read a placeholder comment."""
         assert func is not None
         self._func = func
@@ -22,14 +22,6 @@ class FunctionContainer:
         if sample_params is not None:
             assert isinstance(sample_params, dict)
             self._sample_params = sample_params
-        self._params = dict()
-        if params is not None:
-            assert isinstance(params, dict)
-            self._params = params
-
-        all_param_names = list(self._sample_params.keys()) + list(self._params.keys())
-        unique_param_names = np.unique(all_param_names)
-        assert len(all_param_names) == len(unique_param_names)
 
         # Coerce any sample_params to being ndarrays for easy masking
         for k, v in self._sample_params.items():
@@ -50,17 +42,12 @@ class FunctionContainer:
         """Read a placeholder comment."""
         return self._sample_params
 
-    @property
-    def params_(self):
+    def generate_sample_params_for_mask(self, mask):
         """Read a placeholder comment."""
-        return self._params
-
-    def generate_params_for_mask(self, mask):
-        """Read a placeholder comment."""
-        curr_params = copy.deepcopy(self.params_)
+        curr_sample_params = dict()
         for name, value in self.sample_params_.items():
-            curr_params[name] = value[mask]
-        return curr_params
+            curr_sample_params[name] = value[mask]
+        return curr_sample_params
 
     def evaluate(self, y_true, y_pred, mask):
         """Read a placeholder comment."""
@@ -68,11 +55,10 @@ class FunctionContainer:
         assert isinstance(y_pred, np.ndarray)
         assert len(y_true) == len(y_pred)
         assert len(y_true) == len(mask)
-        params = self.generate_params_for_mask(mask)
+        params = self.generate_sample_params_for_mask(mask)
 
         return self.func_(y_true[mask], y_pred[mask], **params)
 
     def evaluate_all(self, y_true, y_pred):
         """Read a placeholder comment."""
-        all_params = {**self.params_, **self.sample_params_}
-        return self.func_(y_true, y_pred, **all_params)
+        return self.func_(y_true, y_pred, **self.sample_params_)
--- test/unit/metrics/experimental/test_function_container.py
+++ test/unit/metrics/experimental/test_function_container.py
@@ -1,4 +1,4 @@
-# Copyright (c) Microsoft Corporation and Fairlearn contributors.
+m# Copyright (c) Microsoft Corporation and Fairlearn contributors.
 # Licensed under the MIT License.
 
 import numpy as np
@@ -7,50 +7,41 @@ from fairlearn.metrics.experimental._function_container import FunctionContainer
 
 
 def test_constructor_unnamed():
-    sample_params = {'sample_weight': [1, 2, 3]}
-    params = {'pos_label': 0, }
+o    sample_params = {'sample_weight': [1, 2, 3]}
 
-    fc = FunctionContainer(recall_score, None, sample_params, params)
+    fc = FunctionContainer(recall_score, None, sample_params)
     assert fc.name_ == recall_score.__name__
     assert len(fc.sample_params_) == 1
     assert isinstance(fc.sample_params_['sample_weight'], np.ndarray)
     assert np.array_equal(fc.sample_params_['sample_weight'], [1, 2, 3])
-    assert len(fc.params_) == 1
-    assert fc.params_['pos_label'] == 0
 
 
 def test_constructor_named():
     sample_params = {'sample_weight': [1, 2, 3]}
-    params = {'pos_label': 0}
 
-    fc = FunctionContainer(recall_score, "OverrideName", sample_params, params)
+    fc = FunctionContainer(recall_score, "OverrideName", sample_params)
     assert fc.name_ == "OverrideName"
     assert len(fc.sample_params_) == 1
     assert isinstance(fc.sample_params_['sample_weight'], np.ndarray)
     assert np.array_equal(fc.sample_params_['sample_weight'], [1, 2, 3])
-    assert len(fc.params_) == 1
-    assert fc.params_['pos_label'] == 0
 
 
-def test_generate_params_for_mask():
+def test_generate_sample_params_for_mask():
     sample_params = {'sample_weight': [1, 2, 3]}
-    params = {'pos_label': 0}
 
-    fc = FunctionContainer(recall_score, None, sample_params, params)
+    fc = FunctionContainer(recall_score, None, sample_params)
 
     mask = [True, False, True]
-    result = fc.generate_params_for_mask(mask)
-    assert len(result) == 2
-    assert result['pos_label'] == 0
+    result = fc.generate_sample_params_for_mask(mask)
+    assert len(result) == 1
     assert isinstance(result['sample_weight'], np.ndarray)
     assert np.array_equal(result['sample_weight'], [1, 3])
 
 
 def test_function_evaluation():
     sample_params = {'sample_weight': [1, 2, 3, 1, 1, 2, 2, 1]}
-    params = {'pos_label': 0}
 
-    fc = FunctionContainer(recall_score, None, sample_params, params)
+    fc = FunctionContainer(recall_score, None, sample_params)
 
     mask = [True, False, True, False, False, True, True, False]
     y_t = np.asarray([0, 1, 0, 1, 0, 1, 0, 1])
@@ -61,13 +52,11 @@ def test_function_evaluation():
     wgt_masked = [1, 3, 2, 2]
 
     expected = recall_score(y_t_masked, y_p_masked,
-                            pos_label=0,
                             sample_weight=wgt_masked)
     actual = fc.evaluate(y_t, y_p, mask)
     assert expected == actual
 
     expected_all = recall_score(y_t, y_p,
-                                pos_label=0,
                                 sample_weight=sample_params['sample_weight'])
     actual_all = fc.evaluate_all(y_t, y_p)
     assert expected_all == actual_all
