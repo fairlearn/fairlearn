45bc9d784a6960fbf54302368e198ac2c5e0ad62
Richard Edgar <riedgar@microsoft.com>
2020-09-30 08:34:06 -0400

    Remove private exports
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- fairlearn/metrics/experimental/__init__.py
+++ fairlearn/metrics/experimental/__init__.py
@@ -5,12 +5,8 @@
 
 from ._derived_metrics import make_derived_metric
 from ._grouped_metric import GroupedMetric
-from . import _function_container
-from . import _group_feature
 
 __all__ = [
     'GroupedMetric',
     'make_derived_metric',
-    '_function_container',
-    '_group_feature'
 ]
