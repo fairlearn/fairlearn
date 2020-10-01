bd23182a5c34f30c6ae046a928ce7cc171e77806
Richard Edgar <riedgar@microsoft.com>
2020-10-01 15:35:17 -0400

    Working through missing conditional features
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- test/unit/metrics/experimental/test_grouped_metric_missing.py
+++ test/unit/metrics/experimental/test_grouped_metric_missing.py
@@ -8,7 +8,7 @@ import sklearn.metrics as skm
 
 import fairlearn.metrics.experimental as metrics
 
-from .data_for_test import y_t, y_p, group_gen
+from .data_for_test import y_t, y_p, group_gen, g_1
 
 metric_functions = [skm.recall_score,
                     skm.precision_score,
@@ -16,11 +16,13 @@ metric_functions = [skm.recall_score,
                     skm.balanced_accuracy_score]
 
 
+n = len(y_t)
+g_A = np.asarray([group_gen(x, int(n/2), ['aa', 'bb']) for x in range(n)])
+g_B = np.asarray([group_gen(x, 1+int(n/3), ['x', 'y', 'z']) for x in range(n)])
+
+
 @pytest.mark.parametrize("metric_fn", metric_functions)
 def test_missing_sensitive_feature_combinations(metric_fn):
-    n = len(y_t)
-    g_A = np.asarray([group_gen(x, int(n/2), ['aa', 'bb']) for x in range(n)])
-    g_B = np.asarray([group_gen(x, 1+int(n/3), ['x', 'y', 'z']) for x in range(n)])
 
     target = metrics.GroupedMetric(metric_fn,
                                    y_t, y_p,
@@ -51,3 +53,65 @@ def test_missing_sensitive_feature_combinations(metric_fn):
     assert target.ratio()[metric_fn.__name__] == min(direct_eval) / max(direct_eval)
     assert target.ratio(method='to_overall')[metric_fn.__name__] == \
         min([x/overall for x in direct_eval] + [overall/x for x in direct_eval])
+
+
+def test_missing_conditional_feature_combinations():
+    metric_fn = skm.accuracy_score
+
+    target = metrics.GroupedMetric(metric_fn,
+                                   y_t, y_p,
+                                   conditional_features=[g_A, g_B],
+                                   sensitive_features=g_1)
+
+    # Build all the expected values
+    overall = dict()
+    by_groups = dict()
+    for i_A in np.unique(g_A):
+        mask_A = g_A == i_A
+
+        overall_B = dict()
+        by_groups_B = dict()
+        for i_B in np.unique(g_B):
+            mask_B = g_B == i_B
+
+            mask_A_B = np.logical_and(mask_A, mask_B)
+            if i_A == 'bb' and i_B == 'x':
+                assert sum(mask_A_B) == 0
+            else:
+                assert sum(mask_A_B) != 0
+                overall_B[i_B] = metric_fn(y_t[mask_A_B], y_p[mask_A_B])
+                b_g = dict()
+                for i_1 in np.unique(g_1):
+                    mask = np.logical_and(mask_A_B, g_1 == i_1)
+                    if sum(mask) != 0:
+                        b_g[i_1] = metric_fn(y_t[mask], y_p[mask])
+                by_groups_B[i_B] = b_g
+        overall[i_A] = overall_B
+        by_groups[i_A] = by_groups_B
+
+    # Check values are as expected
+    for i_A in np.unique(g_A):
+        for i_B in np.unique(g_B):
+            if i_B in overall[i_A]:
+                assert overall[i_A][i_B] == \
+                    target.overall[metric_fn.__name__][(i_A, i_B)]
+                for i_1 in np.unique(g_1):
+                    if i_1 in by_groups[i_A][i_B]:
+                        assert by_groups[i_A][i_B][i_1] == \
+                            target.by_group[metric_fn.__name__][(i_A, i_B, i_1)]
+                    else:
+                        assert target.by_group[metric_fn.__name__][(i_A, i_B, i_1)] is None
+            else:
+                assert target.overall[metric_fn.__name__][(i_A, i_B)] is None
+
+    # Check differences and ratios
+    for i_A in np.unique(g_A):
+        for i_B in np.unique(g_B):
+            if i_B in overall[i_A]:
+                expected_max = max(by_groups[i_A][i_B].values())
+                expected_min = min(by_groups[i_A][i_B].values())
+                assert expected_max == target.group_max()[metric_fn.__name__][(i_A, i_B)]
+                assert expected_min == target.group_min()[metric_fn.__name__][(i_A, i_B)]
+            else:
+                assert target.group_max()[metric_fn.__name__][(i_A, i_B)] is None
+                assert target.group_min()[metric_fn.__name__][(i_A, i_B)] is None
