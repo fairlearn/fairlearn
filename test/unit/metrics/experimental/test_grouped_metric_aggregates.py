562163246a0d20822f2312b7d5c6acf67f3a4c8b
Richard Edgar <riedgar@microsoft.com>
2020-10-02 09:01:44 -0400

    Extra test for aggregates
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- test/unit/metrics/experimental/test_grouped_metric_aggregates.py
+++ test/unit/metrics/experimental/test_grouped_metric_aggregates.py
@@ -417,3 +417,116 @@ def test_2m_1sf_1cf():
     assert ratios_overall['prec']['kk'] == min(precision_k_overall)
     assert ratios_overall['prec']['m'] == pytest.approx(min(precision_m_overall),
                                                         rel=1e-10, abs=1e-16)
+
+
+def test_2m_1sf_2cf():
+    func_dict = {'recall': skm.recall_score, 'prec': skm.precision_score}
+    target = metrics.GroupedMetric(func_dict,
+                                   y_t, y_p,
+                                   sensitive_features=g_2,
+                                   conditional_features=[g_3, g_1])
+
+    # Check we have correct return types
+    assert isinstance(target.overall, pd.DataFrame)
+    assert isinstance(target.by_group, pd.DataFrame)
+
+    mask_a = (g_1 == 'aa')
+    mask_b = (g_1 == 'ba')
+    mask_f = (g_2 == 'f')
+    mask_g = (g_2 == 'g')
+    mask_k = (g_3 == 'kk')
+    mask_m = (g_3 == 'm')
+
+    mask_k_a = np.logical_and(mask_k, mask_a)
+    mask_k_b = np.logical_and(mask_k, mask_b)
+    mask_m_a = np.logical_and(mask_m, mask_a)
+    mask_m_b = np.logical_and(mask_m, mask_b)
+    mask_k_a_f = np.logical_and(mask_k_a, mask_f)
+    mask_k_a_g = np.logical_and(mask_k_a, mask_g)
+    mask_k_b_f = np.logical_and(mask_k_b, mask_f)
+    mask_k_b_g = np.logical_and(mask_k_b, mask_g)
+    mask_m_a_f = np.logical_and(mask_m_a, mask_f)
+    mask_m_a_g = np.logical_and(mask_m_a, mask_g)
+    mask_m_b_f = np.logical_and(mask_m_b, mask_f)
+    mask_m_b_g = np.logical_and(mask_m_b, mask_g)
+
+    for mfn, metric_fn in func_dict.items():
+        metric_k_a = metric_fn(y_t[mask_k_a], y_p[mask_k_a])
+        metric_k_b = metric_fn(y_t[mask_k_b], y_p[mask_k_b])
+        metric_m_a = metric_fn(y_t[mask_m_a], y_p[mask_m_a])
+        metric_m_b = metric_fn(y_t[mask_m_b], y_p[mask_m_b])
+        metric_k_a_f = metric_fn(y_t[mask_k_a_f], y_p[mask_k_a_f])
+        metric_k_a_g = metric_fn(y_t[mask_k_a_g], y_p[mask_k_a_g])
+        metric_k_b_f = metric_fn(y_t[mask_k_b_f], y_p[mask_k_b_f])
+        metric_k_b_g = metric_fn(y_t[mask_k_b_g], y_p[mask_k_b_g])
+        metric_m_a_f = metric_fn(y_t[mask_m_a_f], y_p[mask_m_a_f])
+        metric_m_a_g = metric_fn(y_t[mask_m_a_g], y_p[mask_m_a_g])
+        metric_m_b_f = metric_fn(y_t[mask_m_b_f], y_p[mask_m_b_f])
+        metric_m_b_g = metric_fn(y_t[mask_m_b_g], y_p[mask_m_b_g])
+
+        metric_k_a_arr = [metric_k_a_f, metric_k_a_g]
+        metric_k_b_arr = [metric_k_b_f, metric_k_b_g]
+        metric_m_a_arr = [metric_m_a_f, metric_m_a_g]
+        metric_m_b_arr = [metric_m_b_f, metric_m_b_g]
+
+        target_mins = target.group_min()
+        assert isinstance(target_mins, pd.DataFrame)
+        assert target_mins.shape == (4, 2)
+        assert target_mins[mfn][('kk', 'aa')] == min(metric_k_a_arr)
+        assert target_mins[mfn][('kk', 'ba')] == min(metric_k_b_arr)
+        assert target_mins[mfn][('m', 'aa')] == min(metric_m_a_arr)
+        assert target_mins[mfn][('m', 'ba')] == min(metric_m_b_arr)
+
+        target_maxs = target.group_max()
+        assert isinstance(target_mins, pd.DataFrame)
+        assert target_maxs.shape == (4, 2)
+        assert target_maxs[mfn][('kk', 'aa')] == max(metric_k_a_arr)
+        assert target_maxs[mfn][('kk', 'ba')] == max(metric_k_b_arr)
+        assert target_maxs[mfn][('m', 'aa')] == max(metric_m_a_arr)
+        assert target_maxs[mfn][('m', 'ba')] == max(metric_m_b_arr)
+
+        diffs = target.difference()
+        assert isinstance(diffs, pd.DataFrame)
+        assert diffs.shape == (4, 2)
+        assert diffs[mfn][('kk', 'aa')] == max(metric_k_a_arr) - min(metric_k_a_arr)
+        assert diffs[mfn][('kk', 'ba')] == max(metric_k_b_arr) - min(metric_k_b_arr)
+        assert diffs[mfn][('m', 'aa')] == max(metric_m_a_arr) - min(metric_m_a_arr)
+        assert diffs[mfn][('m', 'ba')] == max(metric_m_b_arr) - min(metric_m_b_arr)
+
+        diffs_overall = target.difference(method='to_overall')
+        assert isinstance(diffs_overall, pd.DataFrame)
+        assert diffs_overall.shape == (4, 2)
+        diff_overall_k_a = max([abs(x-metric_k_a) for x in metric_k_a_arr])
+        diff_overall_k_b = max([abs(x-metric_k_b) for x in metric_k_b_arr])
+        diff_overall_m_a = max([abs(x-metric_m_a) for x in metric_m_a_arr])
+        diff_overall_m_b = max([abs(x-metric_m_b) for x in metric_m_b_arr])
+
+        assert diffs_overall[mfn][('kk', 'aa')] == diff_overall_k_a
+        assert diffs_overall[mfn][('kk', 'ba')] == diff_overall_k_b
+        assert diffs_overall[mfn][('m', 'aa')] == diff_overall_m_a
+        assert diffs_overall[mfn][('m', 'ba')] == diff_overall_m_b
+
+        ratios = target.ratio()
+        assert isinstance(ratios, pd.DataFrame)
+        assert ratios.shape == (4, 2)
+        assert ratios[mfn][('kk', 'aa')] == min(metric_k_a_arr) / max(metric_k_a_arr)
+        assert ratios[mfn][('kk', 'ba')] == min(metric_k_b_arr) / max(metric_k_b_arr)
+        assert ratios[mfn][('m', 'aa')] == min(metric_m_a_arr) / max(metric_m_a_arr)
+        assert ratios[mfn][('m', 'ba')] == min(metric_m_b_arr) / max(metric_m_b_arr)
+
+        ratios_overall = target.ratio(method='to_overall')
+        assert isinstance(ratios_overall, pd.DataFrame)
+        assert ratios_overall.shape == (4, 2)
+        ratio_overall_k_a = [x/metric_k_a for x in metric_k_a_arr] + \
+            [metric_k_a/x for x in metric_k_a_arr]
+        ratio_overall_k_b = [x/metric_k_b for x in metric_k_b_arr] + \
+            [metric_k_b/x for x in metric_k_b_arr]
+        ratio_overall_m_a = [x/metric_m_a for x in metric_m_a_arr] + \
+            [metric_m_a/x for x in metric_m_a_arr]
+        ratio_overall_m_b = [x/metric_m_b for x in metric_m_b_arr] + \
+            [metric_m_b/x for x in metric_m_b_arr]
+        assert ratios_overall[mfn][('kk', 'aa')] == min(ratio_overall_k_a)
+        assert ratios_overall[mfn][('kk', 'ba')] == min(ratio_overall_k_b)
+        assert ratios_overall[mfn][('m', 'aa')] == min(ratio_overall_m_a)
+        assert ratios_overall[mfn][('m', 'ba')] == pytest.approx(min(ratio_overall_m_b),
+                                                                 rel=1e-10, abs=1e-16)
