5cd16d12e5aded87569081e769e46d5c8d0875b6
Richard Edgar <riedgar@microsoft.com>
2020-10-02 08:32:08 -0400

    Test a few more metric functions in overall
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- test/unit/metrics/experimental/test_grouped_metric_overall.py
+++ test/unit/metrics/experimental/test_grouped_metric_overall.py
@@ -3,34 +3,43 @@
 
 import numpy as np
 import pandas as pd
+import pytest
 import sklearn.metrics as skm
 
 from .data_for_test import y_t, y_p, s_w, g_1, g_2
 from .utils import _get_raw_GroupedMetric
 
 
-def test_1m_0cf():
+metric_functions = [skm.recall_score,
+                    skm.precision_score,
+                    skm.accuracy_score,
+                    skm.balanced_accuracy_score]
+
+
+@pytest.mark.parametrize("metric_fn", metric_functions)
+def test_1m_0cf(metric_fn):
     target = _get_raw_GroupedMetric()
-    func_dict = target._process_functions(skm.recall_score, None)
+    func_dict = target._process_functions(metric_fn, None)
     result = target._compute_overall(func_dict, y_t, y_p, None)
 
     assert isinstance(result, pd.Series)
     assert len(result) == 1
-    expected = skm.recall_score(y_t, y_p)
-    assert result['recall_score'] == expected
+    expected = metric_fn(y_t, y_p)
+    assert result[metric_fn.__name__] == expected
 
 
-def test_1m_0cf_wgt():
+@pytest.mark.parametrize("metric_fn", metric_functions)
+def test_1m_0cf_wgt(metric_fn):
     target = _get_raw_GroupedMetric()
-    func_dict = target._process_functions(skm.recall_score,
+    func_dict = target._process_functions(metric_fn,
                                           {'sample_weight': s_w})
 
     result = target._compute_overall(func_dict, y_t, y_p, None)
 
     assert isinstance(result, pd.Series)
     assert len(result) == 1
-    expected = skm.recall_score(y_t, y_p, sample_weight=s_w)
-    assert result['recall_score'] == expected
+    expected = metric_fn(y_t, y_p, sample_weight=s_w)
+    assert result[metric_fn.__name__] == expected
 
 
 def test_2m_0cf():
@@ -47,9 +56,10 @@ def test_2m_0cf():
     assert result['prec'] == exp_prec
 
 
-def test_1m_1cf():
+@pytest.mark.parametrize("metric_fn", metric_functions)
+def test_1m_1cf(metric_fn):
     target = _get_raw_GroupedMetric()
-    func_dict = target._process_functions(skm.recall_score, None)
+    func_dict = target._process_functions(metric_fn, None)
     cf_list = target._process_features("CF", g_1, y_t)
 
     result = target._compute_overall(func_dict, y_t, y_p, cf_list)
@@ -59,10 +69,10 @@ def test_1m_1cf():
     assert np.array_equal(result.index.names, ['CF 0'])
     mask_a = (g_1 == 'aa')
     mask_b = (g_1 == 'ba')
-    exp_recall_a = skm.recall_score(y_t[mask_a], y_p[mask_a])
-    exp_recall_b = skm.recall_score(y_t[mask_b], y_p[mask_b])
-    assert result['recall_score']['aa'] == exp_recall_a
-    assert result['recall_score']['ba'] == exp_recall_b
+    exp_metric_a = metric_fn(y_t[mask_a], y_p[mask_a])
+    exp_metric_b = metric_fn(y_t[mask_b], y_p[mask_b])
+    assert result[metric_fn.__name__]['aa'] == exp_metric_a
+    assert result[metric_fn.__name__]['ba'] == exp_metric_b
 
 
 def test_1m_1cf_wgt():
