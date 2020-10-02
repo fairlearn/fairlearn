5c9d69c2c49076261c5e16d8e9fadea140def678
Richard Edgar <riedgar@microsoft.com>
2020-10-02 08:36:37 -0400

    Test a few different functions for by_group
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- test/unit/metrics/experimental/test_grouped_metric_by_group.py
+++ test/unit/metrics/experimental/test_grouped_metric_by_group.py
@@ -3,15 +3,23 @@
 
 import numpy as np
 import pandas as pd
+import pytest
 import sklearn.metrics as skm
 
 from .data_for_test import y_t, y_p, s_w, g_1, g_2, g_3, g_4
 from .utils import _get_raw_GroupedMetric
 
 
-def test_1m_1sf_0cf():
+metric_functions = [skm.recall_score,
+                    skm.precision_score,
+                    skm.accuracy_score,
+                    skm.balanced_accuracy_score]
+
+
+@pytest.mark.parametrize("metric_fn", metric_functions)
+def test_1m_1sf_0cf(metric_fn):
     target = _get_raw_GroupedMetric()
-    func_dict = target._process_functions(skm.recall_score, None)
+    func_dict = target._process_functions(metric_fn, None)
     sf_list = target._process_features("SF", g_1, y_t)
 
     result = target._compute_by_group(func_dict, y_t, y_p, sf_list, None)
@@ -21,15 +29,16 @@ def test_1m_1sf_0cf():
     assert np.array_equal(result.index.names, ['SF 0'])
     mask_a = (g_1 == 'aa')
     mask_b = (g_1 == 'ba')
-    recall_a = skm.recall_score(y_t[mask_a], y_p[mask_a])
-    recall_b = skm.recall_score(y_t[mask_b], y_p[mask_b])
-    assert result['recall_score']['aa'] == recall_a
-    assert result['recall_score']['ba'] == recall_b
+    metric_a = metric_fn(y_t[mask_a], y_p[mask_a])
+    metric_b = metric_fn(y_t[mask_b], y_p[mask_b])
+    assert result[metric_fn.__name__]['aa'] == metric_a
+    assert result[metric_fn.__name__]['ba'] == metric_b
 
 
-def test_1m_1sf_1cf():
+@pytest.mark.parametrize("metric_fn", metric_functions)
+def test_1m_1sf_1cf(metric_fn):
     target = _get_raw_GroupedMetric()
-    func_dict = target._process_functions(skm.recall_score, None)
+    func_dict = target._process_functions(metric_fn, None)
     sf_list = target._process_features("SF", g_1, y_t)
     cf_list = target._process_features("CF", g_2, y_t)
 
@@ -44,14 +53,14 @@ def test_1m_1sf_1cf():
     mask_b_f = np.logical_and((g_1 == 'ba'), (g_2 == 'f'))
     mask_b_g = np.logical_and((g_1 == 'ba'), (g_2 == 'g'))
 
-    exp_a_f = skm.recall_score(y_t[mask_a_f], y_p[mask_a_f])
-    exp_a_g = skm.recall_score(y_t[mask_a_g], y_p[mask_a_g])
-    exp_b_f = skm.recall_score(y_t[mask_b_f], y_p[mask_b_f])
-    exp_b_g = skm.recall_score(y_t[mask_b_g], y_p[mask_b_g])
-    assert result['recall_score'][('f', 'aa')] == exp_a_f
-    assert result['recall_score'][('f', 'ba')] == exp_b_f
-    assert result['recall_score'][('g', 'aa')] == exp_a_g
-    assert result['recall_score'][('g', 'ba')] == exp_b_g
+    exp_a_f = metric_fn(y_t[mask_a_f], y_p[mask_a_f])
+    exp_a_g = metric_fn(y_t[mask_a_g], y_p[mask_a_g])
+    exp_b_f = metric_fn(y_t[mask_b_f], y_p[mask_b_f])
+    exp_b_g = metric_fn(y_t[mask_b_g], y_p[mask_b_g])
+    assert result[metric_fn.__name__][('f', 'aa')] == exp_a_f
+    assert result[metric_fn.__name__][('f', 'ba')] == exp_b_f
+    assert result[metric_fn.__name__][('g', 'aa')] == exp_a_g
+    assert result[metric_fn.__name__][('g', 'ba')] == exp_b_g
 
 
 def test_2m_2sf_2cf():
