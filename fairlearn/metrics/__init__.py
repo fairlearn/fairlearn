4147f037db9094381529d5cd4d88c8d2d7dfb848
Richard Edgar <riedgar@microsoft.com>
2020-10-08 08:43:44 -0400

    Start putting in some derived metrics
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- fairlearn/metrics/__init__.py
+++ fairlearn/metrics/__init__.py
@@ -18,6 +18,8 @@ The metric is evaluated for the entire set of data, and also
 for each subgroup identified in ``group_membership``.
 """
 
+import sklearn.metrics as skm
+
 from ._derived_metrics import make_derived_metric  # noqa: F401
 from ._disparities import (  # noqa: F401
     demographic_parity_difference,
@@ -39,33 +41,18 @@ from ._extra_metrics import (  # noqa: F401
     )
 
 
-_core = [
-    "GroupedMetric",
-    "make_derived_metric",
-]
+# Derived metrics from SciKit-Learn
+_standard_spn = ['sample_weight']
 
-_extra_metrics = [
-    "true_positive_rate",
-    "true_negative_rate",
-    "false_positive_rate",
-    "false_negative_rate",
-    "mean_prediction",
-    "selection_rate",
-    "_balanced_root_mean_squared_error",
-    "_mean_overprediction",
-    "_mean_underprediction",
-]
-
-
-_disparities = [
-    "demographic_parity_difference",
-    "demographic_parity_ratio",
-    "equalized_odds_difference",
-    "equalized_odds_ratio"
-]
-
-
-__all__ = (
-    _core +
-    _extra_metrics +
-    _disparities)
+accuracy_score_group_min = make_derived_metric('group_min',
+                                               skm.accuracy_score,
+                                               sample_param_names=_standard_spn)
+accuracy_score_group_max = make_derived_metric('group_max',
+                                               skm.accuracy_score,
+                                               sample_param_names=_standard_spn)
+accuracy_score_difference = make_derived_metric('difference',
+                                                skm.accuracy_score,
+                                                sample_param_names=_standard_spn)
+accuracy_score_ratio = make_derived_metric('ratio',
+                                           skm.accuracy_score,
+                                           sample_param_names=_standard_spn)
