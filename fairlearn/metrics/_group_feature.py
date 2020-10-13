5e607079e743551ae182adc00e96810ce600e95e
Richard Edgar <riedgar@microsoft.com>
2020-10-13 10:06:45 -0400

    Add some documentation to GroupFeature
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- fairlearn/metrics/_group_feature.py
+++ fairlearn/metrics/_group_feature.py
@@ -1,6 +1,7 @@
 # Copyright (c) Microsoft Corporation and Fairlearn contributors.
 # Licensed under the MIT License.
 
+from typing import Any, List, Optional
 import numpy as np
 import pandas as pd
 
@@ -8,10 +9,44 @@ from sklearn.preprocessing import LabelEncoder
 
 
 class GroupFeature:
-    """Read a placeholder comment."""
+    """A helper class for Fairlearn metrics.
 
-    def __init__(self, base_name, feature_vector, index, name):
-        """Read a placeholder comment."""
+    Each instance of this class holds a single 'grouping' feature
+    on behalf of a metric object. A grouping feature is either
+    a sensitive or conditional feature which is used to divide
+    up the sample population.
+
+    This class takes care of encoding the grouping feature, and producing
+    masks (used to select samples from the population) corresponding
+    to the subgroups it identifies.
+
+    It also holds the feature name, which can be supplied by the caller,
+    or generated from a base and and index.
+    """
+
+    def __init__(self,
+                 base_name: str,
+                 feature_vector,
+                 index: int,
+                 name: Optional[str]):
+        """Help with the metrics.
+
+        Parameters
+        ----------
+        base_name : str
+            The base string to use as a feature name if `name` is not specified.
+            The value of `index` is appended
+
+        feature_vector : array_like
+            Some sort of array encoding the feature. It is fed into
+            :class:`sklrearn.preprocessing.LabelEncoder` for easy masking
+
+        index : int
+            Used together with `base_name` when automatically generating a name
+
+        name : str
+            Optional name for the feature
+        """
         self._encoder = LabelEncoder()
         self._encoded = np.asarray(self._encoder.fit_transform(feature_vector))
 
@@ -22,21 +57,23 @@ class GroupFeature:
             if feature_vector.name is not None:
                 self._name = feature_vector.name
 
-    def get_mask_for_class(self, target_class):
-        """Read a placeholder comment."""
+    def get_mask_for_class(self,
+                           target_class: Any) -> np.ndarray:
+        """Fetch a mask array for the given class."""
         idx = self.classes.index(target_class)
         return self.get_mask_for_class_index(idx)
 
-    def get_mask_for_class_index(self, target_class_index):
-        """Read a placeholder comment."""
+    def get_mask_for_class_index(self,
+                                 target_class_index: int) -> np.ndarray:
+        """Fetch a mask array for the given class index."""
         return (self._encoded == target_class_index)
 
     @property
-    def name(self):
-        """Read a placeholder comment."""
+    def name(self) -> str:
+        """Return the name of the feature."""
         return self._name
 
     @property
-    def classes(self):
-        """Read a placeholder comment."""
+    def classes(self) -> List:
+        """Return list of unique classes."""
         return list(self._encoder.classes_)
