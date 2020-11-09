bab99613dfa1c88b431af88be0bd219984021cfe
Roman Lutz <rolutz@microsoft.com>
2020-11-09 10:26:28 -0800

    Revert dashboard to ipywidgets (#625)
    * add ipywidgets based dashboard back, make it compatible with metrics changes
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * fix bug in passing data to MetricFrame and convert by_group output to dict
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * flake8
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- fairlearn/widget/__init__.py
+++ fairlearn/widget/__init__.py
@@ -1,11 +1,18 @@
 # Copyright (c) Microsoft Corporation and Fairlearn contributors.
 # Licensed under the MIT License.
 
-"""Package for the fairness dashboard widget."""
+"""Package for the Fairlearn Dashboard widget."""
 
-from .fairness_dashboard import FairlearnDashboard
+from ._fairlearn_dashboard import FairlearnDashboard
 
-
-__version__ = "0.1.0-dev"
 
 __all__ = ['FairlearnDashboard']
+
+
+def _jupyter_nbextension_paths():
+    return [{
+        'section': 'notebook',
+        'src': 'static',
+        'dest': 'fairlearn-widget',
+        'require': 'fairlearn-widget/extension'
+    }]
