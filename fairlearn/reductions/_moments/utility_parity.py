11d0dfadf2eb234156fedae80776cc34fa1449a5
Richard Edgar <riedgar@microsoft.com>
2020-11-20 11:14:10 -0500

    Control features for Equalised Odds too
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- fairlearn/reductions/_moments/utility_parity.py
+++ fairlearn/reductions/_moments/utility_parity.py
@@ -378,9 +378,14 @@ class EqualizedOdds(UtilityParity):
 
     def load_data(self, X, y, **kwargs):
         """Load the specified data into the object."""
-        super().load_data(X, y,
-                          event=pd.Series(y).apply(lambda y: _LABEL + "=" + str(y)),
-                          **kwargs)
+        # The `where` clause is used to put `pd.nan` on all values where `Y!=0`.
+        base_event = pd.Series(y).apply(lambda y: _LABEL + "=" + str(y))
+        control_features = kwargs.get(_KW_CONTROL_FEATURES)
+        if control_features is not None:
+            event = base_event.combine(pd.Series(control_features), _combine_event_and_control)
+        else:
+            event = base_event
+        super().load_data(X, y, event=event, **kwargs)
 
 
 class ErrorRateParity(UtilityParity):
