29b9f1b3a6c59f31ccfa42820cfe6b87a95c57f7
koaning <vincentwarmerdam@gmail.com>
2020-11-25 17:04:47 +0100

    added microsofts prefferred copyright message
    Signed-off-by: koaning <vincentwarmerdam@gmail.com>

--- fairlearn/preprocessing/__init__.py
+++ fairlearn/preprocessing/__init__.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation and Fairlearn contributors.
+# Licensed under the MIT License.
+
 """Preprocessing tools to help deal with sensitive attributes."""
 
 from ._linear_dep_remover import LinearDependenceRemover
--- test/unit/preprocessing/linear_dep_remover/test_sklearn_compat.py
+++ test/unit/preprocessing/linear_dep_remover/test_sklearn_compat.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation and Fairlearn contributors.
+# Licensed under the MIT License.
+
 import pytest
 import numpy as np
 import pandas as pd
