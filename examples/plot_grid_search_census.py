0e24d3ddedc274f6e505427c03b7174ce3c3d0a9
Richard Edgar <riedgar@microsoft.com>
2021-01-29 10:16:03 -0500

    Update FairlearnDashboard deprecation
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- examples/plot_grid_search_census.py
+++ examples/plot_grid_search_census.py
@@ -34,7 +34,7 @@ GridSearch with Census Data
 # .. note::
 #
 #     The :code:`FairlearnDashboard` will move from Fairlearn to the
-#     :code:`raiwidgets` package after the v0.5.0 release. Instead, Fairlearn
+#     :code:`raiwidgets` package in the near future. Instead, Fairlearn
 #     will provide some of the existing functionality through
 #     :code:`matplotlib`-based visualizations.
 
