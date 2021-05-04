4cc92c1e98d06a1ee1097622d1bdafac605f2f8c
Roman Lutz <rolutz@microsoft.com>
2021-05-03 21:01:31 -0700

    replace bokeh in plot_* script
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- examples/plot_quickstart_selection_rate.py
+++ examples/plot_quickstart_selection_rate.py
@@ -7,7 +7,6 @@ Selection rates in census dataset
 =================================
 """
 # %%
-from bokeh.plotting import figure, show
 from fairlearn.metrics import MetricFrame, selection_rate
 from fairlearn.datasets import fetch_adult
 
@@ -20,20 +19,4 @@ selection_rates = MetricFrame(selection_rate,
                               y_true, y_true,
                               sensitive_features=sex)
 
-xs = list(selection_rates.by_group.index)
-ys = [selection_rates.by_group[s] for s in xs]
-
-p = figure(x_range=xs,
-           plot_height=480,
-           plot_width=640,
-           title="Fraction earning over $50,0000",
-           toolbar_location=None,
-           tools="")
-
-p.vbar(x=xs, top=ys, width=0.9)
-
-p.y_range.start = 0
-p.xgrid.grid_line_color = None
-p.axis.minor_tick_line_color = None
-
-show(p)
+selection_rates.by_group.plot(legend=False, title='Fraction earning over $50,000')
