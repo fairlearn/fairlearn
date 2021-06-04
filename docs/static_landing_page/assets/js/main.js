4c0d76c65f4ba572826393f25318f522ba0fb700
Malo Ware <devware090@gmail.com>
2021-06-05 04:53:29 +0700

    Update grid/layout#01

--- docs/static_landing_page/assets/js/main.js
+++ docs/static_landing_page/assets/js/main.js
@@ -82,42 +82,6 @@
         $(".mobile-nav, .mobile-nav-toggle").hide();
     }
 
-    // Navigation active state on scroll
-    var nav_sections = $('section');
-    var main_nav = $('.nav-menu, #mobile-nav');
-
-    $(window).on('scroll', function() {
-        var cur_pos = $(this).scrollTop() + 200;
-
-        nav_sections.each(function() {
-            var top = $(this).offset().top,
-                bottom = top + $(this).outerHeight();
-
-            if (cur_pos >= top && cur_pos <= bottom) {
-                if (cur_pos <= bottom) {
-                    main_nav.find('li').removeClass('active');
-                }
-                main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
-            }
-            if (cur_pos < 300) {
-                $(".nav-menu ul:first li:first").addClass('active');
-            }
-        });
-    });
-
-    // Toggle .header-scrolled class to #header when page is scrolled
-    // $(window).scroll(function() {
-    //     if ($(this).scrollTop() > 100) {
-    //         $('#header').addClass('header-scrolled');
-    //     } else {
-    //         $('#header').removeClass('header-scrolled');
-    //     }
-    // });
-
-    // if ($(window).scrollTop() > 100) {
-    //     $('#header').addClass('header-scrolled');
-    // }
-
     // Back to top button
     $(window).scroll(function() {
         if ($(this).scrollTop() > 100) {
