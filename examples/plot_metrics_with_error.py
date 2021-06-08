67241f2f0cf81ddcf7cc7d1983741979d5e9add5
Alex Quach <alexhq1000@gmail.com>
2021-06-08 10:05:57 -0400

    code linting

--- examples/plot_metrics_with_error.py
+++ examples/plot_metrics_with_error.py
@@ -13,19 +13,15 @@ Plotting Metrics with Error Bars
 # We download the data set using `fetch_adult` function in `fairlearn.datasets`.
 # We start by importing the various modules we're going to use:
 #
-import os
-import sys
-nb_dir = os.path.split(os.getcwd())[0]
-if nb_dir not in sys.path:
-    sys.path.append(nb_dir)
 
-from fairlearn.metrics import MetricFrame, ErrorPlotter
-from sklearn.metrics import confusion_matrix
-from sklearn.datasets import fetch_openml
-from sklearn.metrics import recall_score
-from sklearn.tree import DecisionTreeClassifier
-import pandas as pd
 import numpy as np
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import recall_score
+from sklearn.datasets import fetch_openml
+from sklearn.metrics import confusion_matrix
+from fairlearn.metrics import MetricFrame, ErrorPlotter
+
 
 data = fetch_openml(data_id=1590, as_frame=True)
 X = pd.get_dummies(data.data)
@@ -33,26 +29,28 @@ y_true = (data.target == '>50K') * 1
 sex = data.data['sex']
 
 # %%
-# Error Metrics 
+# Error Metrics
 # -------------
-# We have many different choices for error metrics. In this notebook we'll just be using a 
-# Normal approximation interval (symmetric) and a Wilson score interval (asymmetric). 
+# We have many different choices for error metrics. In this notebook we'll just be using a
+# Normal approximation interval (symmetric) and a Wilson score interval (asymmetric).
 #
 # https://en.wikipedia.org/wiki/Binomial_proportion_confidence_interval#Wilson_score_interval
 # https://en.wikipedia.org/wiki/Binomial_proportion_confidence_interval#Normal_approximation_interval
 #
-# 
+#
 
 z_score = 1.959964
 digits = 4
 
+
 def recall_normal_err(y_t, y_p):
-    assert len(y_t)==len(y_p)
-    tn, fp, fn, tp  = confusion_matrix(y_t, y_p).ravel()
-    error = compute_error_metric(tp/(tp+fn), tp + fn, z_score= z_score)
+    assert len(y_t) == len(y_p)
+    tn, fp, fn, tp = confusion_matrix(y_t, y_p).ravel()
+    error = compute_error_metric(tp/(tp+fn), tp + fn, z_score=z_score)
     return error
 
-def wilson(p, n, digits= digits, z = z_score):
+
+def wilson(p, n, digits=digits, z=z_score):
     """ Returns lower and upper bound """
     denominator = 1 + z**2/n
     centre_adjusted_probability = p + z*z / (2*n)
@@ -61,18 +59,21 @@ def wilson(p, n, digits= digits, z = z_score):
     upper_bound = (centre_adjusted_probability + z*adjusted_standard_deviation) / denominator
     return (round(lower_bound, digits), round(upper_bound, digits))
 
+
 def compute_error_metric(metric_value, sample_size, z_score):
     return z_score*np.sqrt(metric_value*(1.0-metric_value))/np.sqrt(sample_size)
 
+
 def recall_wilson_lower_bound(y_t, y_p):
-    assert len(y_t)==len(y_p)
-    tn, fp, fn, tp  = confusion_matrix(y_t, y_p).ravel()
+    assert len(y_t) == len(y_p)
+    tn, fp, fn, tp = confusion_matrix(y_t, y_p).ravel()
     bounds = wilson(tp/(tp+fn), tp + fn, digits, z_score)
     return bounds[0]
 
+
 def recall_wilson_upper_bound(y_t, y_p):
-    assert len(y_t)==len(y_p)
-    tn, fp, fn, tp  = confusion_matrix(y_t, y_p).ravel()
+    assert len(y_t) == len(y_p)
+    tn, fp, fn, tp = confusion_matrix(y_t, y_p).ravel()
     bounds = wilson(tp/(tp+fn), tp + fn, digits, z_score)
     return bounds[1]
 
@@ -96,10 +97,10 @@ y_pred = classifier.predict(X)
 # Analyze metrics using MetricFrame
 metrics = {
     'Recall': recall_score,
-    'Recall lower bound': recall_wilson_lower_bound, 
-    'Recall upper bound': recall_wilson_upper_bound, 
+    'Recall lower bound': recall_wilson_lower_bound,
+    'Recall upper bound': recall_wilson_upper_bound,
     'Recall Error': recall_normal_err
-    }
+}
 metric_frame = MetricFrame(metrics, y_true, y_pred, sensitive_features=sex)
 
 # %%
@@ -115,7 +116,8 @@ error_mapping = {
 }
 
 ep = ErrorPlotter(metric_frame, error_mapping)
-ep.plot_with_error("bar", metric="Recall", title="(Symmetric) Normal Error Intervals", capsize=10, colormap="Pastel1")
+ep.plot_with_error("bar", metric="Recall",
+                   title="(Symmetric) Normal Error Intervals", capsize=10, colormap="Pastel1")
 
 # %%
 # 2. We plot wiuth the asymmetric Wilson Bounds
@@ -127,29 +129,33 @@ error_mapping = {
 }
 
 ep = ErrorPlotter(metric_frame, error_mapping)
-ep.plot_with_error("bar", metric="Recall", title="(Asymmetric) Wilson Bounds", capsize=10, colormap="Pastel2")
+ep.plot_with_error("bar", metric="Recall", title="(Asymmetric) Wilson Bounds",
+                   capsize=10, colormap="Pastel2")
 
 # %%
 # Creating Custom Error Metrics
 # -----------------------------
-# In this tutorial we used Normal intervals and Wilson bounds to demonstrate how to plot symmetric and asymmetric metrics, respectively. 
+# In this tutorial we used Normal intervals and Wilson bounds to demonstrate how to plot symmetric and asymmetric metrics, respectively.
 #
 # However, in order to support many different metrics, we reuse the MetricFrame functionality by writing a function
 # that takes in y_true and y_pred, and returns the error metric:
 
+
 def error_metric_function(y_true, y_pred):
-    assert len(y_true)==len(y_pred)
+    assert len(y_true) == len(y_pred)
     # compute error metric
-    tn, fp, fn, tp  = confusion_matrix(y_true, y_pred).ravel()  # compute custom metric function here
+    # compute custom metric function here
+    tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
     bounds = wilson(tp/(tp+fn), tp + fn, digits, z_score)   # compute custom metric function here
     # returns the lower bound
     return bounds[0]
 
+
 # Then when creating the ErrorPlotter we pass in the mapping of the form:
 error_mapping = {
-    "Recall": { # `Recall` is the Metric Name
-        "upper_bound": "Recall upper bound", # `upper_bound` is a predefined type of error metric
-                                             # `Recall upper bound` is the column name of the metric defined in the MetricFrame
+    "Recall": {  # `Recall` is the Metric Name
+        "upper_bound": "Recall upper bound",  # `upper_bound` is a predefined type of error metric
+        # `Recall upper bound` is the column name of the metric defined in the MetricFrame
         "lower_bound": "Recall lower bound"  # `lower_bound` is another predefined type of error metric
                                              # `Recall lower bound` is the column name of the metric defined in the MetricFrame
     }
--- fairlearn/metrics/_error_plotter.py
+++ fairlearn/metrics/_error_plotter.py
@@ -3,7 +3,7 @@
 
 """Utility class for plotting metrics with error ranges."""
 
-from typing import Any, Callable, Dict, List, Optional, Union
+from typing import Any, Dict
 
 from ._metric_frame import MetricFrame
 
@@ -16,9 +16,10 @@ class ErrorPlotter:
                  error_mapping: Dict[str, Dict[str, Any]]):
         """
         First level keys in `error_mapping`: Metrics (str corresponding to pandas column)
-        Second level keys in `error_mapping`: "upper_bound" | "lower_bound" | "upper_error" | "lower_error" | "symmetric_error"
+        Second level keys in `error_mapping`: "upper_bound" | "lower_bound" | "upper_error" |
+                                              "lower_error" | "symmetric_error"
         Second level values in `error_mapping`: Error Metric (str corresponding to pandas column)
-        
+
         error_mapping example:
             {
                 "Recall": {
@@ -37,14 +38,17 @@ class ErrorPlotter:
         self.metric_frame = metric_frame
         self.error_mapping = error_mapping
 
-    def plot_with_error(self, plot_type: str, metric: str, ax = None, plot_error_labels=True, text_precision_digits=4, text_fontsize=8, text_color="black", text_ha="center", **kwargs):
+    def plot_with_error(self, plot_type: str, metric: str, ax=None, plot_error_labels=True,
+                        text_precision_digits=4, text_fontsize=8, text_color="black",
+                        text_ha="center", **kwargs):
         """
         Currently plots only 1 metric + its error
 
         """
         # assert metric is string
         assert metric in self.error_mapping.keys()
-        # assert exactly one of (upper_bound, lower_bound), (upper_error, lower_error), or (symmetric_error)
+        # assert exactly one of (upper_bound, lower_bound),
+        #                       (upper_error, lower_error), or (symmetric_error)
 
         df = self.metric_frame.by_group
 
@@ -53,32 +57,34 @@ class ErrorPlotter:
 
         # assert upper_error, lower_error, symmetric_error > 0
 
-        if "upper_bound" in self.error_mapping[metric].keys() and "lower_bound" in self.error_mapping[metric].keys():
+        if "upper_bound" in self.error_mapping[metric].keys() and \
+           "lower_bound" in self.error_mapping[metric].keys():
             lower_bound = self.error_mapping[metric]["lower_bound"]
             upper_bound = self.error_mapping[metric]["upper_bound"]
-            
+
             df_lower_error = df[metric] - df[lower_bound]
             df_upper_error = df[upper_bound] - df[metric]
             df_error = [df_lower_error, df_upper_error]
-        
+
             if plot_error_labels:
                 df_lower_bound = df[lower_bound]
                 df_upper_bound = df[upper_bound]
-        elif "upper_error" in self.error_mapping[metric].keys() and "lower_error" in self.error_mapping[metric].keys():
+        elif "upper_error" in self.error_mapping[metric].keys() and \
+             "lower_error" in self.error_mapping[metric].keys():
             lower_error = self.error_mapping[metric]["lower_error"]
             upper_error = self.error_mapping[metric]["upper_error"]
 
             df_lower_error = df[lower_error]
             df_upper_error = df[upper_error]
             df_error = [df_lower_error, df_upper_error]
-        
+
             if plot_error_labels:
                 df_lower_bound = df[metric] - df_lower_error
                 df_upper_bound = df[metric] + df_upper_error
         elif "symmetric_error" in self.error_mapping[metric].keys():
             symmetric_error = self.error_mapping[metric]["symmetric_error"]
             df_error = df[symmetric_error]
-        
+
             if plot_error_labels:
                 df_lower_bound = df[metric] - df_error
                 df_upper_bound = df[metric] + df_error
@@ -95,8 +101,12 @@ class ErrorPlotter:
         if plot_error_labels:
             for i, item in enumerate(ax.patches):
                 # get_width pulls left or right; get_y pushes up or down
-                ax.text(i, df_lower_bound[i] - 0.05 * y_range, round(df_lower_bound[i], text_precision_digits), fontsize=text_fontsize, color=text_color, ha=text_ha)
-                ax.text(i, df_upper_bound[i] + 0.01 * y_range, round(df_upper_bound[i], text_precision_digits), fontsize=text_fontsize, color=text_color, ha=text_ha)
+                ax.text(i, df_lower_bound[i] - 0.05 * y_range, round(df_lower_bound[i],
+                        text_precision_digits), fontsize=text_fontsize, color=text_color,
+                        ha=text_ha)
+                ax.text(i, df_upper_bound[i] + 0.01 * y_range, round(df_upper_bound[i],
+                        text_precision_digits), fontsize=text_fontsize, color=text_color,
+                        ha=text_ha)
 
         return ax
 
