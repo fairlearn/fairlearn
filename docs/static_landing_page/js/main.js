efe19bda1e64663bf3312430a668c4f8b247b54d
Malo Ware <devware090@gmail.com>
2021-06-14 11:58:23 -0400

    Integration updates into sphinx#05

--- docs/static_landing_page/js/main.js
+++ docs/static_landing_page/js/main.js
@@ -81,5 +81,40 @@
     } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
         $(".mobile-nav, .mobile-nav-toggle").hide();
     }
+    // Navigation active state on scroll
+    var nav_sections = $('section');
+    var main_nav = $('.nav-menu, #mobile-nav');
+
+    $(window).on('scroll', function() {
+        var cur_pos = $(this).scrollTop() + 200;
+
+        nav_sections.each(function() {
+            var top = $(this).offset().top,
+                bottom = top + $(this).outerHeight();
+
+            if (cur_pos >= top && cur_pos <= bottom) {
+                if (cur_pos <= bottom) {
+                    main_nav.find('li').removeClass('active');
+                }
+                main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+            }
+            if (cur_pos < 300) {
+                $(".nav-menu ul:first li:first").addClass('active');
+            }
+        });
+    });
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('#header').addClass('header-scrolled');
+        } else {
+            $('#header').removeClass('header-scrolled');
+        }
+    });
+
+    if ($(window).scrollTop() > 100) {
+        $('#header').addClass('header-scrolled');
+    }
+
 
 })(jQuery);
