9f4fbafac8286c13fa552a3e564f87878a99b98e
kurianbenoy-aot <kurian.benoy@aot-technologies.com>
2021-06-19 23:03:30 +0530

    format conf.py with black
    Signed-off-by: kurianbenoy-aot <kurian.benoy@aot-technologies.com>

--- docs/conf.py
+++ docs/conf.py
@@ -16,21 +16,25 @@
 import os
 import sys
 import inspect
-rootdir = os.path.join(os.getenv("SPHINX_MULTIVERSION_SOURCEDIR", default=os.getcwd()), "..")
+
+rootdir = os.path.join(
+    os.getenv("SPHINX_MULTIVERSION_SOURCEDIR", default=os.getcwd()), ".."
+)
 sys.path.insert(0, rootdir)
 print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
 [print(p) for p in sys.path]
 print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
 import fairlearn  # noqa: E402
+
 print(fairlearn.__version__)
 print("================================")
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Fairlearn'
-copyright = '2021, Fairlearn contributors'
-author = 'Fairlearn contributors'
+project = "Fairlearn"
+copyright = "2021, Fairlearn contributors"
+author = "Fairlearn contributors"
 
 # The full version, including alpha/beta/rc tags
 release = fairlearn.__version__
@@ -60,59 +64,60 @@ def check_if_v046():
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'bokeh.sphinxext.bokeh_plot',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.extlinks',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.linkcode',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.napoleon',
-    'sphinx_gallery.gen_gallery',
-    'sphinx_multiversion',
-    'sphinx_autodoc_typehints',  # needs to be AFTER napoleon
+    "bokeh.sphinxext.bokeh_plot",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.linkcode",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx_gallery.gen_gallery",
+    "sphinx_multiversion",
+    "sphinx_autodoc_typehints",  # needs to be AFTER napoleon
 ]
 
-source_suffix = ['.rst']
+source_suffix = [".rst"]
 
-intersphinx_mapping = {'python3': ('https://docs.python.org/3', None),
-                       'numpy': ('https://numpy.org/doc/stable/', None),
-                       'pandas': ('https://pandas.pydata.org/pandas-docs/stable/', None),
-                       'sklearn': ('https://scikit-learn.org/stable/', None),
-                       'matplotlib': ('http://matplotlib.sourceforge.net/', None)}
+intersphinx_mapping = {
+    "python3": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
+    "sklearn": ("https://scikit-learn.org/stable/", None),
+    "matplotlib": ("http://matplotlib.sourceforge.net/", None),
+}
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.md']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "README.md"]
 
-master_doc = 'index'
+master_doc = "index"
 
 # Multiversion settings
 
-smv_tag_whitelist = r'^v0\.4\.6|^v0\.5\.\d|^v0\.6\.\d+$'
-smv_branch_whitelist = r'^main$'
+smv_tag_whitelist = r"^v0\.4\.6|^v0\.5\.\d|^v0\.6\.\d+$"
+smv_branch_whitelist = r"^main$"
 
 if check_if_v046():
     print("Current version is v0.4.6, will apply overrides")
-    master_doc = 'index'
+    master_doc = "index"
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'pydata_sphinx_theme'
+html_theme = "pydata_sphinx_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
     "logo_link": "https://fairlearn.org",
-
     "icon_links": [
         {
             "name": "GitHub",
@@ -135,7 +140,7 @@ html_theme_options = {
             "icon": "fab fa-gitter",
         },
     ],
-    "show_prev_next": False
+    "show_prev_next": False,
 }
 
 # The name of an image file (relative to this directory) to place at the top
@@ -144,8 +149,7 @@ html_logo = "_static/images/fairlearn_full_color.png"
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
-html_additional_pages = {
-}
+html_additional_pages = {}
 
 # If false, no index is generated.
 html_use_index = False
@@ -153,31 +157,31 @@ html_use_index = False
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
-html_css_files = ['css/custom.css']
+html_css_files = ["css/custom.css"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # Use filename_pattern so that plot_adult_dataset is not
 # included in the gallery, but its plot is available for
 # the quickstart
 sphinx_gallery_conf = {
-    'examples_dirs': '../examples',
-    'gallery_dirs': 'auto_examples',
+    "examples_dirs": "../examples",
+    "gallery_dirs": "auto_examples",
     # pypandoc enables rst to md conversion in downloadable notebooks
-    'pypandoc': True,
+    "pypandoc": True,
     # binder configuration is used to ru
-    'binder': {
-        'org': 'fairlearn',
-        'repo': 'fairlearn', 
-        'binderhub_url': 'https://mybinder.org', 
-        'branch': 'main', 
-        'dependencies': ['../requirements.txt'],
-        'notebooks_dir': 'examples',
-        'use_jupyter_lab': False  
-    }
+    "binder": {
+        "org": "fairlearn",
+        "repo": "fairlearn",
+        "binderhub_url": "https://mybinder.org",
+        "branch": "main",
+        "dependencies": ["../requirements.txt"],
+        "notebooks_dir": "examples",
+        "use_jupyter_lab": False,
+    },
 }
 
 html_sidebars = {
@@ -188,7 +192,7 @@ html_sidebars = {
 # ----------------
 
 # Change the ordering of the member documentation
-autodoc_member_order = 'groupwise'
+autodoc_member_order = "groupwise"
 
 
 # Linking Code
@@ -233,7 +237,9 @@ def linkcode_resolve(domain, info):
         linespec = ""
 
     tag_or_branch = os.getenv("SPHINX_MULTIVERSION_NAME", default="main")
-    fn = os.path.relpath(fn, start=os.path.dirname(fairlearn.__file__)).replace(os.sep, '/')
+    fn = os.path.relpath(
+        fn, start=os.path.dirname(fairlearn.__file__)
+    ).replace(os.sep, "/")
     return f"http://github.com/fairlearn/fairlearn/blob/{tag_or_branch}/fairlearn/{fn}{linespec}"
 
 
@@ -242,9 +248,9 @@ def linkcode_resolve(domain, info):
 mathjax3_config = {
     "tex": {
         "macros": {
-            "E": '{\\mathbb{E}}',
-            "P": '{\\mathbb{P}}',
-            "given": '\\mathbin{\\vert}'
+            "E": "{\\mathbb{E}}",
+            "P": "{\\mathbb{P}}",
+            "given": "\\mathbin{\\vert}",
         }
     }
 }
