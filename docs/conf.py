51d00745e6194fec62003bf841ed3d8b05f6e96f
Pankaj Patil <pankaj.patil2099@hotmail.com>
2021-07-10 15:21:43 +0530

    MNT Convert README.md to README.rst (#885)
    * Convert REAMDE.md to README.rst
    
    * Removed current README.md
    
    * Discord changes
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * fix setup.py reference to README.md
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * adjust all other mentions of README.md
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    Co-authored-by: Roman Lutz <rolutz@microsoft.com>

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.md']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.rst']
 
 master_doc = 'index'
 
