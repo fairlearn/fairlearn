0c7c34bbc2594547bc3409534dbdecc5db0f00ca
Richard Edgar <riedgar@microsoft.com>
2021-07-18 10:28:47 -0400

    Add a simple smoke test
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

---
+++ test/unit/utils/test_utils.py
@@ -0,0 +1,29 @@
+# Copyright (c) Fairlearn contributors.
+# Licensed under the MIT License.
+
+import numpy as np
+import pandas as pd
+
+import fairlearn.utils._input_validation as iv
+
+
+class TestValidateAndReformatInput:
+    def test_smoke(self):
+        X = pd.DataFrame.from_dict(
+            {
+                'alpha': ['a', 'a', 'b'],
+                'beta': [1, 2, 1]
+            }
+        )
+        sf = np.asarray(['C', 'D', 'C'])
+        y = np.asarray([0, 0, 1])
+
+        X_update, y_update, sf_update, cf_update = iv._validate_and_reformat_input(
+            X=X,
+            y=y,
+            sensitive_features=sf
+        )
+        assert isinstance(X_update, pd.DataFrame)
+        assert isinstance(y_update, pd.Series)
+        assert isinstance(y_update, pd.Series)
+        assert cf_update is None
