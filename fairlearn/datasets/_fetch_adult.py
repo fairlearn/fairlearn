bf563f51181126298fbd6bd5312b52532b1fa4c9
Roman Lutz <romanlutz13@gmail.com>
2021-09-30 10:23:15 +0200

    Update fairlearn/datasets/_fetch_adult.py

--- fairlearn/datasets/_fetch_adult.py
+++ fairlearn/datasets/_fetch_adult.py
@@ -25,7 +25,7 @@ def fetch_adult(*, cache=True, data_home=None,
     Prediction task is to determine whether a person makes over $50,000 a
     year.
 
-    ..versionadded: 0.5.0
+    .. versionadded:: 0.5.0
 
     Parameters
     ----------
