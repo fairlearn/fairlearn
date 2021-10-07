0c0b45197c6b13533e1414287cd369977e5df18b
Richard Edgar <riedgar@microsoft.com>
2021-10-07 08:25:52 -0400

    Starting with a function wrapper
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

---
+++ fairlearn/metrics/_mfv2.py
@@ -0,0 +1,29 @@
+# Copyright (c) Microsoft Corporation and Fairlearn contributors.
+# Licensed under the MIT License.
+
+from typing import List
+
+import pandas as pd
+
+
+class MetricFunctionRequest:
+    def __init__(self, func, arguments: List[str] = ['y_true, y_pred']):
+        self._func = func
+        self._args = arguments
+
+    @property
+    def func(self):
+        return self._func
+
+    @property
+    def arguments(self) -> List[str]:
+        return self._args
+
+    def invoke(self, df: pd.DataFrame):
+        kwargs = dict()
+        for arg_name in self.arguments:
+            kwargs[arg_name] = df[arg_name]
+
+        result = self.func(**kwargs)
+
+        return result
