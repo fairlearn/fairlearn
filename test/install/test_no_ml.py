ca71d4733ffb2c531d1388cbd46bc654eeaaf90f
SeanMcCarren <mccarrensean@gmail.com>
2021-11-05 18:11:11 +0100

    flake8

--- test/install/test_no_ml.py
+++ test/install/test_no_ml.py
@@ -6,13 +6,14 @@ import pytest
 from fairlearn.adversarial import AdversarialMitigationBase
 from fairlearn.adversarial._constants import _IMPORT_ERROR_MESSAGE
 
+
 @pytest.mark.parametrize("library", [None, 'torch', 'tensorflow'])
 def test_no_library(library):
     expected_library = "torch or tensorflow" if library is None else library
     with pytest.raises(RuntimeError) as exc:
         AdversarialMitigationBase(
             library=library,
-            predictor_model=[5,5],
+            predictor_model=[5, 5],
             adversary_model=[2]
         )
 
