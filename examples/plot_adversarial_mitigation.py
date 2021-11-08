0701b7d922510fd5e2415ef352980c7843adde66
SeanMcCarren <mccarrensean@gmail.com>
2021-11-08 17:59:18 +0100

    flakey8

--- examples/plot_adversarial_mitigation.py
+++ examples/plot_adversarial_mitigation.py
@@ -30,17 +30,22 @@ Mitigating Fairness using Adversarial Mitigation
 # Example 1: Simple use case with UCI Adult Dataset
 # =================================================
 # Firstly, we cover a most basic application of adversarial mitigation. However,
-# this starts by loading and preprocessing the dataset. 
-# 
+# this starts by loading and preprocessing the dataset.
+#
 # For this example we choose the sex as the sensitive feature.
 
 
 # Get dataset.
-
+from fairlearn.metrics import MetricFrame, selection_rate
+from sklearn.metrics import accuracy_score
+from fairlearn.adversarial import AdversarialClassifier
+from pandas import Series
+from numpy import number
+from sklearn.compose import make_column_transformer, make_column_selector
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.datasets import fetch_openml
+
 X, y = fetch_openml(data_id=1590, as_frame=True, return_X_y=True)
-# from fairlearn.datasets import fetch_adult
-# X, y = fetch_adult(as_frame=True, return_X_y=True)
 
 # Remove rows with NaNs. In general dropping NaNs is not statistically sound,
 # but for this example we ignore that.
@@ -52,17 +57,14 @@ y = y[non_NaN_rows]
 # Choose sensitive feature
 sensitive_feature = X['sex']
 
-# %% 
+# %%
 # Clearly, the UCI adult dataset can not be fed into a neural network (yet),
 # as we have many columns that are not numerical in nature. To resolve this
 # issues, we could for instance use one-hot-encodings to preprocess categorical
 # columns. Additionally, let's preprocess the columns of number to a
 # standardized range. For these tasks, we can use functionality from
 # `sklearn.preprocessor`.
-from sklearn.preprocessing import MinMaxScaler, OneHotEncoder, StandardScaler
-from sklearn.compose import make_column_transformer, make_column_selector
-from numpy import number
-from pandas import Series
+
 
 def transform(X):
     if isinstance(X, Series):  # make_column_transformer works with DataFrames
@@ -86,12 +88,11 @@ sensitive_feature = transform(sensitive_feature)
 # networks with sigmoid activations. We initialize neural network constructors
 # by passing a list :math:`h_1, h_2, \dots` that indicate the number of nodes
 # :math:`h_i` per hidden layer :math:`i`.
-# 
+#
 # The specific fairness
 # objective that we choose for this example is demographic parity, so we also
 # set :code:`objective = "demographic_parity"`.
 
-from fairlearn.adversarial import AdversarialClassifier
 
 mitigator = AdversarialClassifier(
     predictor_model=[50, 20],
@@ -100,7 +101,7 @@ mitigator = AdversarialClassifier(
     learning_rate=0.0001
 )
 
-# %% 
+# %%
 # Then, we can fit the data to our model. We generally follow sklearn API,
 # but in this case we require some extra kwargs. In particular, we should
 # specify the number of epochs, batch size, whether to shuffle the rows of data
@@ -117,16 +118,13 @@ mitigator.fit(
     progress_updates=5
 )
 
-# %% 
+# %%
 # Predict and evaluate. In particular, we trained the predictor for demographic
 # parity, so we are not only interested in the accuracy, but also in the selection
 # rate.
 
 y_pred = mitigator.predict(X)
 
-from sklearn.metrics import accuracy_score
-from fairlearn.metrics import equalized_odds_difference, MetricFrame, \
-    true_positive_rate, false_positive_rate, selection_rate
 mf = MetricFrame(
     metrics={
         'accuracy': accuracy_score,
@@ -139,7 +137,7 @@ print(mf.by_group)
 
 # %%
 # We see that the results are not great. The accuracy is not optimal, and there
-# remains demographic disparity. 
+# remains demographic disparity.
 # Of course, the success of this method of mitigating fairness
 # depends strongly on neural network models and the specific hyperparameters, so
 # when applying this method one should pay close attention to their parameter
