9007779c40fd2542061fa423b195187f2a3d2ea7
SeanMcCarren <mccarrensean@gmail.com>
2021-11-09 21:38:25 +0100

    more extensive example :D

--- examples/plot_adversarial_mitigation.py
+++ examples/plot_adversarial_mitigation.py
@@ -36,17 +36,21 @@ Mitigating Fairness using Adversarial Mitigation
 
 
 # Get dataset.
-from fairlearn.metrics import MetricFrame, selection_rate
+from math import ceil, sqrt
+from fairlearn.metrics import MetricFrame, selection_rate, \
+    demographic_parity_difference
 from sklearn.metrics import accuracy_score
 from fairlearn.adversarial import AdversarialFairnessClassifier, \
     AdversarialFairness
 from pandas import Series
-from numpy import number
+from numpy import number, random, mean, where, concatenate
 from sklearn.compose import make_column_transformer, make_column_selector
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
-from torch.nn import BCEWithLogitsLoss
+import torch
+random.seed(123)
+torch.manual_seed(123)
 
 X, y = fetch_openml(data_id=1590, as_frame=True, return_X_y=True)
 
@@ -84,7 +88,7 @@ X = transform(X)
 y = transform(y)
 sensitive_feature = transform(sensitive_feature)
 
-X_train, X_test, Y_train, Y_test, A_train, A_test = \
+X_train, X_test, Y_train, Y_test, Z_train, Z_test = \
         train_test_split(X, y, sensitive_feature,
                          test_size=0.2,
                          random_state=12345,
@@ -117,8 +121,8 @@ mitigator = AdversarialFairness(
     library="torch",
     predictor_model=[50, 20],
     adversary_model=[6, 6],
-    predictor_loss=BCEWithLogitsLoss(reduction='mean'),
-    adversary_loss=BCEWithLogitsLoss(reduction='mean'),
+    predictor_loss=torch.nn.BCEWithLogitsLoss(reduction='mean'),
+    adversary_loss=torch.nn.BCEWithLogitsLoss(reduction='mean'),
     predictor_function=lambda pred: (pred >= 0.).astype(float),
     constraints="demographic_parity",
     optimizer="Adam",
@@ -137,7 +141,7 @@ mitigator = AdversarialFairness(
 mitigator.fit(
     X_train,
     Y_train,
-    sensitive_features=A_train,
+    sensitive_features=Z_train,
     epochs=100,
     batch_size=2**9,
     shuffle=True,
@@ -157,14 +161,188 @@ mf = MetricFrame(
         'selection_rate': selection_rate},
     y_true=Y_test,
     y_pred=predictions,
-    sensitive_features=A_test)
+    sensitive_features=Z_test)
 
 print(mf.by_group)
 
 # %%
 # We see that the results are not great. The accuracy is not optimal, and there
-# remains demographic disparity.
-# Of course, the success of this method of mitigating fairness
-# depends strongly on neural network models and the specific hyperparameters, so
-# when applying this method one should pay close attention to their parameter
-# settings.
+# remains demographic disparity. That is because there is no known out-of-the-box
+# solution that is able to train an adversarial network succesfully. There are many
+# variables at play, and we will improve our model in the next section.
+
+# %%
+# Example 2: More advanced models
+# -------------------------------
+# Below we experiment with our models in order to achieve better results than above.
+# Adversarial Learning is inherently difficult because models can diverge quickly.
+# Intuitively, you should imagine that there are "very easy" local minima that the
+# models may converge to. For instance, if the predictor always outputs class=0,
+# then the adversary's objective is much easier, namely it only has to output the
+# most correlated sensitive feature to class=0. Such minima are very easily reached
+# unfortunately. #TODO this description is very bad at the moment, working on
+# understanding it:).
+#
+# So, to finetune our model and the training thereof, we start by defining our
+# neural networks in the way we'd like them and in the way we can easily tweak them.
+# I'll be using PyTorch, but the same can be achieved using Tensorflow!
+random.seed(123)
+torch.manual_seed(123)
+
+X = X_train
+Y = Y_train
+Z = Z_train
+
+class PredictorModel(torch.nn.Module):
+    def __init__(self):
+        super(PredictorModel, self).__init__()
+        self.layers = torch.nn.Sequential(
+            torch.nn.Linear(X.shape[1], 50),
+            torch.nn.LeakyReLU(),
+            torch.nn.Linear(50, 50),
+            torch.nn.LeakyReLU(),
+            torch.nn.Linear(50, 5),
+            torch.nn.Sigmoid(),
+            torch.nn.Linear(5, 1)
+        ) # NO final activation function!
+
+    def forward(self, x):
+        return self.layers(x)
+
+class AdversaryModel(torch.nn.Module):
+    def __init__(self):
+        super(AdversaryModel, self).__init__()
+        self.layers = torch.nn.Sequential(
+            torch.nn.Linear(1, 6),
+            torch.nn.Sigmoid(),
+            torch.nn.Linear(6, 6),
+            torch.nn.Sigmoid(),
+            torch.nn.Linear(6, 1),
+        ) # NO final activation function!
+
+    def forward(self, x):
+        return self.layers(x)
+
+predictor_model=PredictorModel()
+adversary_model=AdversaryModel()
+# %%
+# Xavier initialization is more popular than PyTorch's default initialization, so
+# let's put that to the test. Note that I also initialize the biases, but this is
+# less common in practice. Intuitively, it seems wise to initialize small weights,
+# so we set the gain low.
+def weights_init(m):
+    if isinstance(m, torch.nn.Conv2d):
+        torch.nn.init.xavier_normal_(m.weight.data, gain=0.1)
+        torch.nn.init.xavier_normal_(m.bias.data, gain=0.1)
+
+predictor_model.apply(weights_init)
+adversary_model.apply(weights_init)
+
+# %%
+# We define some funky optimizers.
+predictor_optimizer = torch.optim.SGD(predictor_model.parameters(), lr=0.5, momentum=0.9, nesterov=True)
+adversary_optimizer = torch.optim.Adam(adversary_model.parameters(), lr=0.1)
+scheduler1 = torch.optim.lr_scheduler.LambdaLR(predictor_optimizer, lr_lambda=lambda epoch: 1/(3*epoch+2))
+
+# %%
+# Then, the instance itself.
+# We take care in defining the loss function with weights because
+
+mitigator = AdversarialFairnessClassifier(
+    predictor_model=predictor_model,
+    adversary_model=adversary_model,
+    predictor_loss=torch.nn.BCEWithLogitsLoss(
+        pos_weight=torch.ones([1])*mean(Y == 1.)),
+    adversary_loss=torch.nn.BCEWithLogitsLoss(
+        pos_weight=torch.ones([1])*mean(Z == 1.)),
+    predictor_function="binary",
+    optimizer=None,
+    predictor_optimizer=predictor_optimizer,
+    adversary_optimizer=adversary_optimizer,
+    alpha=0.1,
+    constraints='demographic_parity'
+)
+
+# Carefully tweaked ;)
+epochs = 13
+batch_size = 2**8
+
+# %%
+# Instead of only looking at training loss, we also take a look at some validation
+# metrics. For this, we chose the demographic parity difference to check to what
+# extent the constraint (demographic parity in this case) is satisfied.
+def validate():
+    predictions = mitigator.predict(X_test)
+
+    dp_diff = demographic_parity_difference(Y_test,
+                                            predictions,
+                                            sensitive_features=Z_test)
+    accuracy = mean(predictions == Y_test)
+    selection_rate = mean(predictions == 1.)
+    return (dp_diff, accuracy, selection_rate)
+
+# %%
+# We make use of a callback function to incorporate the validation into the training
+# loop. Additionally, we handle the prediction_optimizer updates here.
+epoch_count = 1
+def callback_fn():
+    global epoch_count
+    mitigator.alpha = 0.1 * sqrt(epoch_count)
+    epoch_count+=1
+    scheduler1.step()
+
+    print(validate())
+
+# %%
+# Finally, we fit the model
+mitigator.fit(
+    X,
+    Y,
+    sensitive_features=Z,
+    epochs=epochs,
+    batch_size=batch_size,
+    shuffle=True,
+    progress_updates=1,
+    callback_fn=callback_fn)
+
+
+# %%
+# Then, we more carefully improve this by changing the model. That is, we do not
+# change the predictor/adversary model parameters, but we do change the learning
+# parameters. In particular, we set very small learning rates and alpha a bit higher.
+# As mentioned earlier, training adversarially is tricky, and ideally
+# you'd train this in a controlled way.
+
+predictor_optimizer = torch.optim.Adam(predictor_model.parameters(), lr=0.0001)
+adversary_optimizer = torch.optim.Adam(adversary_model.parameters(), lr=0.0001)
+mitigator.alpha = 5
+
+def callback_fn():
+    print(validate())
+
+mitigator.fit(
+    X,
+    Y,
+    sensitive_features=Z,
+    epochs=epochs,
+    batch_size=batch_size,
+    shuffle=True,
+    progress_updates=1,
+    callback_fn=callback_fn)
+# %%
+# We take a look at the results. Notice we achieve a much lower demographic parity
+# difference than in Exercise 1! This does come at the cost of some accuracy, but
+# such a tradeof is to be expected.
+
+predictions = mitigator.predict(X_test)
+
+mf = MetricFrame(
+    metrics={
+        'accuracy': accuracy_score,
+        'selection_rate': selection_rate},
+    y_true=Y_test,
+    y_pred=predictions,
+    sensitive_features=Z_test)
+
+print(mf.by_group)
+
