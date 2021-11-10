64c0ecd8f1beb400bcaaa83017e2a3a6f1ce8214
SeanMcCarren <mccarrensean@gmail.com>
2021-11-10 09:13:55 +0100

    Better docstring and API

--- fairlearn/adversarial/_adversarial_mitigation.py
+++ fairlearn/adversarial/_adversarial_mitigation.py
@@ -24,20 +24,21 @@ TYPES = [AUTO, CLASSIFICATION, BINARY, CATEGORY, CONTINUOUS]
 
 
 class AdversarialFairness():
-    r"""Inprocessing algorithm to mitigate biases using PyTorch or Tensorflow.
+    r"""Train complex predictors while mitigating biases in PyTorch or Tensorflow.
 
-    This algorithm is our implementation of work in `"Mitigating Unwanted Biases with
-    Adversarial Learning" <https://dl.acm.org/doi/pdf/10.1145/3278721.3278779>`_.
-    This algorithm takes as input two
+    This algorithm is our implementation of the supervised learning method proposed in
+    `"Mitigating Unwanted Biases with Adversarial Learning"
+    <https://dl.acm.org/doi/pdf/10.1145/3278721.3278779>`_ [1]_.
+    This algorithm takes as input two neural network
     models, a predictor and an adversarial, defined either as a `PyTorch module
     <https://pytorch.org/docs/stable/generated/torch.nn.Module.html>`_ or
     `Tensorflow2 model
     <https://www.tensorflow.org/api_docs/python/tf/keras/Model>`_. You train this
     predictor using an API that is similar to estimators in `sklearn`.
 
-    To summarize the paper, assume we have data :math:`X, Y, Z`, where we want to
+    To summarize [1]_, assume we have data :math:`X, Y, Z`, where we want to
     predict :math:`Y` from :math:`X` while being fair with respect to :math:`Z`
-    and to some fairness measure. We firstly create predictor and adversary neural
+    w.r.t. some fairness measure. We firstly create predictor and adversary neural
     networks with learnable weights :math:`W` and :math:`U` respectively. Without
     considering fairness yet, this typical supervised-learning problem aims to
     minimize the predictor loss :math:`L_P`. Now, to improve fairness, we not
@@ -50,17 +51,58 @@ class AdversarialFairness():
         \nabla_W L_P - \text{proj}_{\nabla_W L_A} \nabla_W L_P - \alpha \nabla_W L_A
 
     In this implementation, we accept :math:`X, Y, Z` as 1d or 2d array-like. We
-    make the important design choice to allow for arbitrary distribution types,
-    such as binary classes or continuous values, and allow for arbitrary loss
-    functions. However, the user should take care when choosing loss functions
-    #TODO reference below a section on loss functions?
+    provide both a full-fledged training loop by using :func:`AdversarialFairness.fit`,
+    though one may also implement their own training loop by continuously calling
+    :func:`AdversarialFairness.fit` or :func:`AdversarialFairness.partial_fit`.
 
+    As per [1]_, the neural network models :code:`predictor_model` and
+    :code:`adversary_model` may not have an activation function at the end of the
+    model. Instead, even if we are dealing with discrete predictions, we must output
+    logits. # TODO say something about easy-to-use List[int] models.
+
+    There are three important key-words to identify the assumption about the
+    underlying distribution of the data, which are:
+    :code:`"binary"`, :code:`"category"`, or :code:`"continuous"`.
+    Additionally, passing
+    :code:`"classification"` will infer whether to use :code:`"binary"` or
+    :code:`"category"`, and passing :code:`"auto"` will infer whether to use
+    :code:`"binary"`, :code:`"category"`, or :code:`"continuous"`.
+
+    We make the important design choice to allow for arbitrary loss functions
+    :code:`predictor_loss` and :code:`adversary_loss` while
+    also providing a simple interface for simple use cases. So, for simple use
+    cases, the user may for example pass :code:`predictor_loss="binary"` to indicate
+    that we assume that we are predicting binary data, and that we should use an
+    instance of :mod:`torch.nn.BCEWithLogitsLoss` (or the
+    tensorflow equivalent of binary cross entropy if the models are tensorflow
+    models). Or, the user could pass an instance of
+    :mod:`torch.nn.BCEWithLogitsLoss` directly to accomplish the same.
+    Besides :code:`"binary"`, we provide loss functions for :code:`"category"`
+    (categorical cross entropy)
+    and :code:`"continuous"` (mean squared error) as well.
+    We handle :code:`adversary_loss` in the same way.
+
+    Additionally, one may pass one of these key-words as a :code:`predictor_function`.
+    For instance, setting :code:`predictor_function="category"` maps the model's
+    continuous output vector (assumped to be the logits or probabilities per
+    category) to a discrete category prediction. However, we could also pass such
+    a mapping function as :code:`predictor_function` directly, which may be
+    helpful in complex use cases where we are predicting multiple discrete
+    values from one model.
+
+    When passing arbitrary functions as :code:`predictor_loss`, :code:`adversary_loss`,
+    or :code:`predictor_function`, one has to take care that the underlying library
+    supports these functions. That is to say, one should not use
+    TensorFlow loss functions with PyTorch models for instance, foremostly
+    because a TensorFlow loss function may not work with PyTorch's automatic
+    differentation, and TensorFlow loss functions expect their parameters differently
+    than PyTorch loss functions expect their parameters.
 
     Parameters
     ----------
     library : str, default = 'auto'
-        The library to use. Must be one of :code:`['torch','tensorflow',
-        'auto']` which indicates PyTorch, TensorFlow, or to automatically infer
+        The library to use. Must be one of :code:`'torch'`, :code:`'tensorflow'`,
+        or :code:`'auto'` which indicates PyTorch, TensorFlow, or to automatically infer
         the library from the :code:`predictor_model` and which are installed.
 
     predictor_model : list, torch.nn.Module, tensorflow.keras.Model
@@ -93,7 +135,8 @@ class AdversarialFairness():
         Either a string that indicates the type of :code:`y`,
         or :code:`'auto'` to infer the type of :code:`y`, or a callable
         prediction function maps the continuous output of the predictor model to
-        a discrete prediction.
+        a discrete prediction. For example, passing 'binary' maps the predictor's
+        output logits :math:`y` to 1 iff :math:`y \geq 0`.
 
     constraints : str, default = 'demographic_parity'
         The fairness measure to optimize for. Must be either 'demographic_parity'
@@ -103,7 +146,7 @@ class AdversarialFairness():
         The optimizer class to use. If a string is passed instead, this must be
         either "SGD" or "Adam".
 
-    learning_rate : float, default = 0.01
+    learning_rate : float, default = 0.001
         A small number greater than zero to set as initial learning rate
 
     alpha : float, default = 1.0
@@ -115,12 +158,84 @@ class AdversarialFairness():
 
     Notes
     -----
+    Hey
     # TODO
 
     References
     ----------
-    # TODO
+    .. [1] Zhang, Lemoine, Mitchell `"Mitigating Unwanted Biases with
+       Adversarial Learning" <https://dl.acm.org/doi/pdf/10.1145/3278721.3278779>`_,
+       AIES, 2018.
 
+    Examples
+    --------
+    >>> from fairlearn.adversarial import AdversarialFairness
+    >>> from fairlearn.metrics import MetricFrame, selection_rate
+    >>> from numpy import number
+    >>> from sklearn.metrics import accuracy_score
+    >>> from sklearn.compose import make_column_transformer, make_column_selector
+    >>> from sklearn.preprocessing import OneHotEncoder, StandardScaler
+    >>> from sklearn.datasets import fetch_openml
+    >>> from pandas import Series
+    >>>
+    >>> mitigator = AdversarialFairness(
+    ...     predictor_model=[50, 20],
+    ...     adversary_model=[6, 6],
+    ...     learning_rate=0.0001
+    ... )
+    >>>
+    >>> def transform(X):
+    ...     if isinstance(X, Series):
+    ...         X = X.to_frame()
+    ...     ct = make_column_transformer(
+    ...         (StandardScaler(),
+    ...          make_column_selector(dtype_include=number)),
+    ...         (OneHotEncoder(drop='if_binary', sparse=False),
+    ...          make_column_selector(dtype_include="category")))
+    ...     return ct.fit_transform(X)
+    ...
+    >>> X, y = fetch_openml(data_id=1590, as_frame=True, return_X_y=True)
+    >>> non_NaN_rows = ~X.isna().any(axis=1)
+    >>> X, y = X[non_NaN_rows], y[non_NaN_rows]
+    >>> sensitive_feature = X['sex']
+    >>>
+    >>> X = transform(X)
+    >>> y = transform(y)
+    >>> sensitive_feature = transform(sensitive_feature)
+    >>>
+    >>> mitigator.fit(
+    ...     X,
+    ...     y,
+    ...     sensitive_features=sensitive_feature,
+    ...     epochs=100,
+    ...     batch_size=2**9,
+    ...     shuffle=True,
+    ...     progress_updates=5
+    ... )
+    |==>                  | Epoch: 12/100, Batch: 50/89, ETA: 38.30 sec. Loss (pred/adv): 0.55/0.63
+    |=====>               | Epoch: 24/100, Batch: 35/89, ETA: 32.78 sec. Loss (pred/adv): 0.57/0.62
+    |=======>             | Epoch: 36/100, Batch: 54/89, ETA: 27.15 sec. Loss (pred/adv): 0.56/0.61
+    |==========>          | Epoch: 48/100, Batch: 68/89, ETA: 21.89 sec. Loss (pred/adv): 0.37/0.63
+    |============>        | Epoch: 60/100, Batch: 81/89, ETA: 16.74 sec. Loss (pred/adv): 0.34/0.58
+    |==============>      | Epoch: 73/100, Batch: 12/89, ETA: 11.60 sec. Loss (pred/adv): 0.38/0.58
+    |=================>   | Epoch: 85/100, Batch: 13/89, ETA: 6.60 sec. Loss (pred/adv): 0.36/0.65
+    |===================> | Epoch: 97/100, Batch: 2/89, ETA: 1.66 sec. Loss (pred/adv): 0.35/0.64
+    >>>
+    >>> y_pred = mitigator.predict(X)
+    >>>
+    >>> mf = MetricFrame(
+    ...     metrics={
+    ...         'accuracy': accuracy_score,
+    ...         'selection_rate': selection_rate},
+    ...     y_true=y,
+    ...     y_pred=y_pred,
+    ...     sensitive_features=sensitive_feature)
+    >>>
+    >>> print(mf.by_group)
+                        accuracy selection_rate
+    sensitive_feature_0
+    0.0                  0.906295       0.147397
+    1.0                  0.809447       0.228322
     """
 
     def __init__(self, *,
@@ -132,7 +247,9 @@ class AdversarialFairness():
                  predictor_function='auto',
                  constraints='demographic_parity',
                  optimizer='Adam',
-                 learning_rate=0.01,
+                 predictor_optimizer=None,
+                 adversary_optimizer=None,
+                 learning_rate=0.001,
                  alpha=1.0,
                  cuda=False
                  ):
@@ -148,6 +265,8 @@ class AdversarialFairness():
         self._init_losses(predictor_loss, adversary_loss, predictor_function)
         self._init_constraints(constraints)
         self.optimizer = optimizer
+        self.predictor_optimizer = predictor_optimizer
+        self.adversary_optimizer = adversary_optimizer
         self.learning_rate = learning_rate
         self.alpha = alpha
         self._init_cuda(cuda)
@@ -161,7 +280,9 @@ class AdversarialFairness():
             epochs=1,
             batch_size=-1,
             shuffle=False,
-            progress_updates=None):
+            progress_updates=None,
+            skip_validation=False,
+            callback_fn=None):
         """
         Fit the model based on the given training data and sensitive features.
 
@@ -189,6 +310,15 @@ class AdversarialFairness():
         progress_updates : number, optional
             If a number :math:`t` is provided, we regularly print an update
             about the training loop after at least every :math:`t` seconds.
+
+        skip_validation : bool
+            Skip the validation of the data. Useful because validate_input is
+            a costly operation, and we may instead pass all data to validate_input
+            at an earlier stage.
+
+        callback_fn : callable
+            Callback function, called every epoch. For instance useable when
+            wanting to validate. Should take zero arguments.
         """
         X, Y, Z = self._validate_input(X, y, sensitive_features)
         # TODO decreasing learning rate: not really necessary with adam
@@ -225,13 +355,15 @@ class AdversarialFairness():
                 adversary_losses.append(LA)
             if shuffle and epoch != epochs - 1:
                 X, Y, Z = self._shuffle(X, Y, Z)
+            if callable(callback_fn):
+                callback_fn()
 
     def _shuffle(self, X, Y, Z):
         """Shuffle the rows of X, Y, Z."""
         X, Y, Z = shuffle(X, Y, Z)
         return X, Y, Z
 
-    def partial_fit(self, X, y, *, sensitive_features):
+    def partial_fit(self, X, y, *, sensitive_features, skip_validation=False):
         """
         Perform one epoch on given samples and update model.
 
@@ -246,8 +378,15 @@ class AdversarialFairness():
         sensitive_features : numpy.ndarray
             Two-dimensional numpy array containing the sensitive feature of the
             training data.
+
+        skip_validation : bool
+            Skip the validation of the data. Useful because validate_input is
+            a costly operation, and we may instead pass all data to validate_input
+            at an earlier stage.
         """
-        X, Y, Z = self._validate_input(X, y, sensitive_features)
+        X, Y, Z = X, y, sensitive_features
+        if not skip_validation:
+            X, Y, Z = self._validate_input(X, Y, Z)
         self._train_step(X, Y, Z)
 
     def predict(self, X):
@@ -546,7 +685,7 @@ def _check_array(X):
     """
     return check_array(
         X, accept_sparse=False, accept_large_sparse=False,
-        dtype="numeric", order=None, copy=False, force_all_finite=True,
+        dtype=float, order=None, copy=False, force_all_finite=True,
         ensure_2d=True, allow_nd=False, ensure_min_samples=1,
         ensure_min_features=1, estimator=None
     )
@@ -582,7 +721,22 @@ class AdversarialMixin():
         Setup self.predictor_optimizer and self.adversary_optimizer using the
         parameter optimizer given by the user.
         """
-        pass
+        if not isinstance(optimizer, str) and \
+                (not self.predictor_optimizer or not self.adversary_optimizer):
+            raise ValueError(_KWARG_ERROR_MESSAGE.format(
+                "optimizer",
+                "'Adam' or 'SGD' for standard optimizers."
+                + " Or, predictor_optimizer and adversary_optimizer are optimizers."
+            ))
+        if (self.predictor_optimizer or self.adversary_optimizer) \
+                and isinstance(optimizer, str):
+            raise ValueError("Either use kwarg optimizer or use kwargs "
+                             + "predictor_optimizer and adversary_optimizer")
+        if (self.predictor_optimizer and not self.adversary_optimizer) or \
+                (self.adversary_optimizer and not self.predictor_optimizer):
+            raise ValueError("Either define both predictor_optimizer and" +
+                             " adversary_optimizer explicitely, or define" +
+                             " none and use kwarg optimizer")
 
 
 class AdversarialPytorchMixin(AdversarialMixin):
@@ -670,6 +824,7 @@ class AdversarialPytorchMixin(AdversarialMixin):
         Setup self.predictor_optimizer and self.adversary_optimizer using the
         parameter optimizer given by the user.
         """
+        super(AdversarialPytorchMixin, self)._setup_optimizer(optimizer)
         if isinstance(optimizer, str):
             # keyword cases.
             if optimizer.lower() == "adam":
@@ -685,10 +840,6 @@ class AdversarialPytorchMixin(AdversarialMixin):
             else:
                 raise ValueError(_KWARG_ERROR_MESSAGE.format(
                     'optimizer', '"Adam" or "SGD" or an optimizer'))
-        else:
-            # TODO this setup for optimizers doesnt work in pytorch
-            self.predictor_optimizer = optimizer
-            self.adversary_optimizer = optimizer
 
     def _validate_input(self, X, Y, Z):
         """Extend the base `_validate_input` to send data to GPU when required."""
@@ -772,6 +923,7 @@ class AdversarialTensorflowMixin(AdversarialMixin):
         Setup self.predictor_optimizer and self.adversary_optimizer using the
         parameter optimizer given by the user.
         """
+        super(AdversarialTensorflowMixin, self)._setup_optimizer(optimizer)
         if isinstance(optimizer, str):
             # keyword cases.
             if optimizer.lower() == "adam":
@@ -787,9 +939,6 @@ class AdversarialTensorflowMixin(AdversarialMixin):
             else:
                 raise ValueError(_KWARG_ERROR_MESSAGE.format(
                     'optimizer', '"Adam" or "SGD" or an optimizer'))
-        else:
-            self.predictor_optimizer = optimizer
-            self.adversary_optimizer = optimizer
 
 
 class AdversarialFairnessClassifier(AdversarialFairness):
