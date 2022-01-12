1d169f366476ba7848781b784e90a4c3895027a7
Kevin Spiekermann <kspieker@mit.edu>
2022-01-12 11:03:14 -0500

    f! Add function to fetch ACSIncome from OpenML

--- fairlearn/datasets/_fetch_acsincome.py
+++ fairlearn/datasets/_fetch_acsincome.py
@@ -3,19 +3,23 @@
 
 import pathlib
 
+import numpy as np
+import pandas as pd
 from sklearn.datasets import fetch_openml
 from ._constants import _DOWNLOAD_DIRECTORY_NAME
 
 
 def fetch_acsincome(*, cache=True, data_home=None,
-                    as_frame=False, return_X_y=False):
+                    as_frame=False, return_X_y=False,
+                    states=None,
+                    ):
     """Load the ACS Income dataset.
 
     Download it if necessary.
 
     ==============   ==============
     Samples total           1664500
-    Dimensionality               12
+    Dimensionality               10
     Features                   real
     Target                     real
     ==============   ==============
@@ -47,21 +51,24 @@ def fetch_acsincome(*, cache=True, data_home=None,
         If True, returns ``(data.data, data.target)`` instead of a Bunch
         object.
 
+    states: list, default=None
+        List containing state abbreviations. If None, data from all 50 US
+        states, including Puerto Rico, will be returned.
+
     Returns
     -------
     dataset : :obj:`~sklearn.utils.Bunch`
         Dictionary-like object, with the following attributes.
 
-        data : ndarray, shape (1664500, 12)
-            Each row corresponding to the 10 feature values in order as well as
-            the state code (ST) and the target variable (PINCP).
+        data : ndarray, shape (1664500, 10)
+            Each row corresponding to the 10 feature values in order.
             If ``as_frame`` is True, ``data`` is a pandas object.
         target : numpy array of shape (1664500,)
             Integer denoting each person's income.
             A threshold can be applied as a postprocessing step to frame this
             as a binary classification problem.
             If ``as_frame`` is True, ``target`` is a pandas object.
-        feature_names : list of length 12
+        feature_names : list of length 10
             Array of ordered feature names used in the dataset.
         DESCR : string
             Description of the ACSIncome dataset.
@@ -79,13 +86,55 @@ def fetch_acsincome(*, cache=True, data_home=None,
        Advances in Neural Information Processing Systems, 34.
 
     """
+    # State Code based on 2010 Census definitions
+    _STATE_CODES = {'AL': '01', 'AK': '02', 'AZ': '04', 'AR': '05', 'CA': '06',
+                    'CO': '08', 'CT': '09', 'DE': '10', 'FL': '12', 'GA': '13',
+                    'HI': '15', 'ID': '16', 'IL': '17', 'IN': '18', 'IA': '19',
+                    'KS': '20', 'KY': '21', 'LA': '22', 'ME': '23', 'MD': '24',
+                    'MA': '25', 'MI': '26', 'MN': '27', 'MS': '28', 'MO': '29',
+                    'MT': '30', 'NE': '31', 'NV': '32', 'NH': '33', 'NJ': '34',
+                    'NM': '35', 'NY': '36', 'NC': '37', 'ND': '38', 'OH': '39',
+                    'OK': '40', 'OR': '41', 'PA': '42', 'RI': '44', 'SC': '45',
+                    'SD': '46', 'TN': '47', 'TX': '48', 'UT': '49', 'VT': '50',
+                    'VA': '51', 'WA': '53', 'WV': '54', 'WI': '55', 'WY': '56',
+                    'PR': '72'}
+    if states is None:
+        states = _STATE_CODES.keys()
     if not data_home:
         data_home = pathlib.Path().home() / _DOWNLOAD_DIRECTORY_NAME
 
-    return fetch_openml(
-        data_id=43137,
+    # fetch data for all 50 US states and Puerto Rico
+    data_dict = fetch_openml(
+        data_id=43141,
         data_home=data_home,
         cache=cache,
-        as_frame=as_frame,
-        return_X_y=return_X_y,
+        as_frame=True,
+        return_X_y=False,
     )
+
+    # filter by state
+    df_all = data_dict['data'].copy(deep=True)
+    df_all['PINCP'] = data_dict['target']
+    cols = df_all.columns
+    df = pd.DataFrame(np.zeros((0, len(cols))), columns=cols)
+    for state in states:
+        dfs = [df, df_all.query(f"ST == {int(_STATE_CODES[state])}")]
+        df = pd.concat(dfs)
+    # drop the state column since it is not a feature in the published ACSIncome dataset
+    df.drop('ST', axis=1, inplace=True)
+
+    if as_frame:
+        data_dict['data'] = df.iloc[:, :10]
+        data_dict['frame'] = df
+        data_dict['target'] = df.iloc[:, 10:]
+    else:
+        data_dict['data'] = df.iloc[:, :10].values
+        data_dict['frame'] = None
+        data_dict['target'] = df.iloc[:, 10:].values.flatten()
+
+    output = data_dict
+
+    if return_X_y:
+        output = (data_dict['data'], data_dict['target'])
+
+    return output
