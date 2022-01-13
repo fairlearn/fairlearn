4c33abc80ceeebdd0764b46799d2dc98f5489aaa
Britney <britneyting123@gmail.com>
2022-01-13 10:21:49 -0500

    Added function to fetch ACSPublicCoverage from OpenML

--- fairlearn/datasets/__init__.py
+++ fairlearn/datasets/__init__.py
@@ -6,12 +6,14 @@
 
 
 from ._fetch_acsincome import fetch_acsincome
+from ._fetch_acspubliccoverage import fetch_acspubliccoverage
 from ._fetch_adult import fetch_adult
 from ._fetch_boston import fetch_boston
 from ._fetch_bank_marketing import fetch_bank_marketing
 
 __all__ = [
     "fetch_acsincome",
+    "fetch_acspubliccoverage",
     "fetch_adult",
     "fetch_boston",
     "fetch_bank_marketing",
