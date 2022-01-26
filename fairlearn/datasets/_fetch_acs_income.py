ff0d0fe39d34230a68a311ac2bbc21de5fb42d0c
Britney <britneyting123@gmail.com>
2022-01-26 16:14:14 -0500

    Refactor checking for valid states to a utils file

--- fairlearn/datasets/_fetch_acs_income.py
+++ fairlearn/datasets/_fetch_acs_income.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 from sklearn.datasets import fetch_openml
 from ._constants import _DOWNLOAD_DIRECTORY_NAME
+from ._utils import _STATE_CODES, check_states_valid
 
 
 def fetch_acs_income(*, cache=True, data_home=None,
@@ -93,33 +94,14 @@ def fetch_acs_income(*, cache=True, data_home=None,
     .. [2] "2018 ACS PUMS Data Dictionary". United States Census Bureau.
 
     """
-    # State Code based on 2010 Census definitions
-    _STATE_CODES = {'AL': '01', 'AK': '02', 'AZ': '04', 'AR': '05', 'CA': '06',
-                    'CO': '08', 'CT': '09', 'DE': '10', 'FL': '12', 'GA': '13',
-                    'HI': '15', 'ID': '16', 'IL': '17', 'IN': '18', 'IA': '19',
-                    'KS': '20', 'KY': '21', 'LA': '22', 'ME': '23', 'MD': '24',
-                    'MA': '25', 'MI': '26', 'MN': '27', 'MS': '28', 'MO': '29',
-                    'MT': '30', 'NE': '31', 'NV': '32', 'NH': '33', 'NJ': '34',
-                    'NM': '35', 'NY': '36', 'NC': '37', 'ND': '38', 'OH': '39',
-                    'OK': '40', 'OR': '41', 'PA': '42', 'RI': '44', 'SC': '45',
-                    'SD': '46', 'TN': '47', 'TX': '48', 'UT': '49', 'VT': '50',
-                    'VA': '51', 'WA': '53', 'WV': '54', 'WI': '55', 'WY': '56',
-                    'PR': '72'}
+
     # number of features
     _NUM_FEATS = 10
 
     # check that user-provided state abbreviations are valid
     if states is not None:
         states = [state.upper() for state in states]
-        for state in states:
-            try:
-                _STATE_CODES[state]
-            except KeyError:
-                raise KeyError(f'Error with state code: {state}\n'
-                               f'State code must be a two letter abbreviation'
-                               f'from the list {list(_STATE_CODES.keys())}\n'
-                               f'Note that PR is the abbreviation for Puerto Rico.'
-                               )
+        check_states_valid(states)
     else:
         states = _STATE_CODES.keys()
 
