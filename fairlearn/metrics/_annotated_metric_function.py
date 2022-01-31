89386e949b1e54e2acd3f3544add80140d360bd9
Richard Edgar <riedgar@microsoft.com>
2022-01-31 09:25:52 -0500

    Improve some documentation
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- fairlearn/metrics/_annotated_metric_function.py
+++ fairlearn/metrics/_annotated_metric_function.py
@@ -21,7 +21,25 @@ class AnnotatedMetricFunction:
     The :class:`MetricFrame` makes extensive use of `pandas` DataFrames
     internally. In particular, combinations of sensitive (and control)
     features are selected using `DataFrame.groupby()` and acted on via
-    `DataFrame.apply()`.
+    `DataFrame.apply()`. The net result of this is that it is useful
+    to have a function wrapper which can be invoked with a DataFrame
+    argument, and extract its arguments from that DataFrame.
+
+    Parameters
+    ----------
+    func : callable
+        The metric function we wish to invoke
+    name: str
+        An optional string defining the name of the function
+    positional_argument_names: List[str]
+        The column names to be extracted and passed as positional arguments
+        when invoking the function
+    kw_argument_mapping: Dict[str, str]
+        The column names which are to be passed as keyword arguments
+        when invoking the function. Since the DataFrame column names may
+        not match the function's argument names, this is a dictionary
+        where the keys are the function argument names, and the values
+        are the column names
     """
 
     def __init__(
@@ -71,8 +89,8 @@ class AnnotatedMetricFunction:
 
         The second issue is coping with when users have passed in a 2D array as
         a named argument (especially, `y_true` or `y_pred`).
-        These are subject to some extra list-washing; columns requiring this
-        treatment are recorded in `multi_d_columns`.
+        For this reason, we perform some extra list-washing, to make sure the
+        expected types are passed to the underlying metric function.
         """
         args = []
         for arg_name in self.postional_argument_names:
@@ -87,3 +105,4 @@ class AnnotatedMetricFunction:
         result = self.func(*args, **kwargs)
 
         return result
+.
