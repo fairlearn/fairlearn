074280d8487574937e93bacf1ea51daa326a4683
SeanMcCarren <mccarrensean@gmail.com>
2022-02-15 16:28:34 +0100

    add API docstring, remove commented code and unnecessary imports

--- fairlearn/preprocessing/_reweighing.py
+++ fairlearn/preprocessing/_reweighing.py
@@ -1,18 +1,24 @@
 # Copyright (c) Microsoft Corporation and Fairlearn contributors.
 # Licensed under the MIT License.
 
-import numpy as np
-import pandas as pd
+from ..utils._input_validation import _KW_SENSITIVE_FEATURES
+from numpy import unique, zeros
+from numpy import sum as np_sum
 from sklearn.base import BaseEstimator, MetaEstimatorMixin
 from sklearn.utils import check_array, check_consistent_length
-from sklearn.utils.validation import check_is_fitted, column_or_1d
+from sklearn.utils.validation import column_or_1d
 
 
 """
 Implements the "reweighing" algorithm from
-Data preprocessing techniques for classification
-without discrimination by Kamiran and Calders
-https://link.springer.com/content/pdf/10.1007%2Fs10115-011-0463-8.pdf
+"data preprocessing techniques for classification
+without discrimination" paper by Kamiran and Calders.
+
+Reference to paper:
+    Arrieta, A. B., Díaz-Rodríguez, N., Del Ser, J., Bennetot, A.,
+    Tabik, S., Barbado, A., ... & Herrera, F. (2020). Explainable
+    Artificial Intelligence (XAI): Concepts, taxonomies, opportunities
+    and challenges toward responsible AI. Information fusion, 58, 82-115.
 """
 
 
@@ -32,89 +38,127 @@ def calculate_weights(Y, A):
     W : array
         One-dimensional array-like containing weights
     """
-    Y = column_or_1d(check_array(Y, ensure_2d=False))
-    A = column_or_1d(check_array(A, ensure_2d=False))
+    Y = column_or_1d(check_array(Y, ensure_2d=False, dtype=None))
+    A = column_or_1d(check_array(A, ensure_2d=False, dtype=None))
     check_consistent_length(Y, A)
-    Ys = np.unique(Y)
-    As = np.unique(A)
+    Ys = unique(Y)
+    As = unique(A)
     # Per y, store whether an item in Y is y.
     Y_is_y = [(Y == y).astype(int) for y in Ys]
     A_is_a = [(A == a).astype(int) for a in As]
     # Per y, store the sum, because we use it multiple times.
-    y_n = [np.sum(is_y) for is_y in Y_is_y]
-    a_n = [np.sum(is_a) for is_a in A_is_a]
-    W = np.zeros(len(Y), dtype=float)
+    y_n = [np_sum(is_y) for is_y in Y_is_y]
+    a_n = [np_sum(is_a) for is_a in A_is_a]
+    W = zeros(len(Y), dtype=float)
     for i in range(len(Ys)):
         for j in range(len(As)):
             # FIXME: laplace smoothing? Or throw meaningful error if denominator
             # is zero?
             is_both = Y_is_y[i] * A_is_a[j]
-            w = (y_n[i] * a_n[j] / (len(Y) * np.sum(is_both)))
+            w = y_n[i] * a_n[j] / (len(Y) * np_sum(is_both))
             W += w * is_both
     return W
 
 
-# add_weights_methods = ["fit", "fit_transform"]
-# estimator_methods = ["fit", "fit_transform", "predict_proba", "decision_function", "predict", "predict_log_proba"]
-
 class Reweighing(BaseEstimator, MetaEstimatorMixin):
-    # TODO meta estimator
-    # look at exponentiated gradient
-    # write to roman about discussion
+    """
+    Implementation of the reweighing technique from Kamiran and Calders.
+
+    The paper [1]_ by Kamiran and Calders outlines various preprocessing
+    techniques. The third one, reweighing, is implemented in this class.
+    More specifically, this class is a scikit-learn style meta-estimator
+    (wrapper) for Kamiran's algorithm.
+
+    Main functionality is as follows. We wrap this reweighing metaestimator
+    (let's call this :code:`MetaEst`) around a trainable scikit-learn style
+    estimator (let's call this :code:`Est`). You can then use
+    :code:`MetaEst.fit(X, y, sensitive_features)` which will internally compute
+    weights :code:`w` using :code:`y` and :code:`sensitive_features`, and pass this
+    along using :code:`Est.fit(X, y, sample_weights=w)`.
+
+    NOTE: only works with a single sensitive feature at the moment.
+
+    Parameters
+    ----------
+    estimator : estimator
+        The estimator to wrap.
+    passthrough_sensitive_features : bool, default = False
+        This boolean determines whether or not to pass the sensitive_features
+        through to the estimator.
+
+    References
+    ----------
+    .. [1] Arrieta, A. B., Díaz-Rodríguez, N., Del Ser, J., Bennetot, A.,
+       Tabik, S., Barbado, A., ... & Herrera, F. (2020). Explainable
+       Artificial Intelligence (XAI): Concepts, taxonomies, opportunities
+       and challenges toward responsible AI. Information fusion, 58, 82-115.
+    """
+
+    # TODO Possible enhancements:
+    # - casting categorical multi-column sensitive feature to single column.
+    # - laplace smoothing
     def __init__(self, estimator, *, passthrough_sensitive_features=False):
         self.estimator = estimator
         self.passthrough_sensitive_features = passthrough_sensitive_features
 
-        # TODO: mock all other functions of base-estimator?
-        # # These can be extended
-        # all_methods = []
-        # for method_name in dir(estimator):
-        #     try:
-        #         if callable(getattr(estimator, method_name)) and method_name in estimator_methods:
-        #             all_methods.append(method_name)
-        #     except AttributeError:
-        #         pass
-
-        # for method_name in all_methods:
-        #     def method(*args, **kwargs):
-        #         if len(args) > 1:
-        #             y = args[1]
-        #             sensitive_features = kwargs.get("sensitive_features")#_KW_SENSITIVE_FEATURES)
-        #             if sensitive_features is None:
-        #                 raise ValueError("oopsie")#_MESSAGE_SENSITIVE_FEATURES_NONE)
-        #             if method_name in add_weights_methods:
-        #                 W = calculate_weights(y, sensitive_features)
-        #                 kwargs['sample_weight'] = W
-        #         if passthrough_sensitive_features == False:
-        #             del kwargs['sensitive_features']
-        #         getattr(self.estimator, method_name)(*args, **kwargs)
-        #     setattr(self, method_name, method)
-
     def fit(self, X, y, **kwargs):
-        sensitive_features = kwargs.get("sensitive_features")#_KW_SENSITIVE_FEATURES)
+        """
+        Fit the model.
+
+        Calculate the weights from :code:`y` and :code:`sensitive_features`,
+        and pass the data and the computed weights to the estimator.
+
+        Parameters
+        ----------
+        X : array-like
+            This data is passed directly to the estimator.
+        y : 1d array-like
+            This data is used to compute weights and passed to the estimator
+        sensitive_features : 1d array-like
+            This data is used to compute weights and passed to the estimator if
+            passthrough_sensitive_features is set to true.
+
+        Returns
+        -------
+        Object
+            returns whatever the estimator returns, which is the estimator
+            itself it is following scikit-learn conventions.
+        """
+        sensitive_features = kwargs.get(_KW_SENSITIVE_FEATURES)
         W = calculate_weights(y, sensitive_features)
-        kwargs['sample_weight'] = W
-        if self.passthrough_sensitive_features == False:
-            del kwargs['sensitive_features']
-        self.estimator.fit(X, y, **kwargs)
+        kwargs["sample_weight"] = W
+        if self.passthrough_sensitive_features is False:
+            del kwargs["sensitive_features"]
+        # NOTE: Is returning self better up to sklearn-style?
+        return self.estimator.fit(X, y, **kwargs)
 
     def fit_transform(self, X, y, *, sensitive_features, **kwargs):
-        sensitive_features = kwargs.get("sensitive_features")#_KW_SENSITIVE_FEATURES)
-        W = calculate_weights(y, sensitive_features)
-        kwargs['sample_weight'] = W
-        if self.passthrough_sensitive_features == False:
-            del kwargs['sensitive_features']
-        self.estimator.fit_transform(X, y, **kwargs)
+        """
+        Fit and transform the model.
 
-from sklearn.naive_bayes import BernoulliNB
-from sklearn.datasets import fetch_openml
-import numpy as np
-data = fetch_openml(data_id=1590, as_frame=True)
-X = data.data.drop(labels=['sex'], axis=1)
-X = pd.get_dummies(X)
-Y = (data.target == '>50K') * 1
-A = (data.data['sex'] == 'Male') * 1
-est = BernoulliNB()
-metaest = Reweighing(est, passthrough_sensitive_features=False)
-metaest.fit(X, Y, sensitive_features=A)
-print(np.mean(est.predict(X) == Y))
+        Calculate the weights from :code:`y` and :code:`sensitive_features`,
+        and pass the data and the computed weights to the estimator.
+
+        Parameters
+        ----------
+        X : array-like
+            This data is passed directly to the estimator.
+        y : 1d array-like
+            This data is used to compute weights and passed to the estimator
+        sensitive_features : 1d array-like
+            This data is used to compute weights and passed to the estimator if
+            passthrough_sensitive_features is set to true.
+
+        Returns
+        -------
+        Object
+            returns whatever the estimator returns, which is the estimator
+            itself it is following scikit-learn conventions.
+        """
+        sensitive_features = kwargs.get(_KW_SENSITIVE_FEATURES)
+        W = calculate_weights(y, sensitive_features)
+        kwargs["sample_weight"] = W
+        if self.passthrough_sensitive_features is False:
+            del kwargs["sensitive_features"]
+        # NOTE: Is returning self better up to sklearn-style?
+        return self.estimator.fit_transform(X, y, **kwargs)
