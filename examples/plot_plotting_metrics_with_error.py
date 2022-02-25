47dad164085069d532bbc51436d5869fa977ed25
MiroDudik <mdudik@gmail.com>
2022-02-25 16:51:25 -0500

    Clean up comments, remove redundant example

--- examples/plot_plotting_metrics_with_error.py
+++ examples/plot_plotting_metrics_with_error.py
@@ -30,7 +30,7 @@ import numpy as np
 # We download the data set using :meth:`fetch_openml` function in :class:`sklearn.datasets`.
 # The original Adult data set can be found at https://archive.ics.uci.edu/ml/datasets/Adult
 # There are some caveats to using this dataset, but we will use it solely as an example
-# to demonstrate the plotting metrics with error bars functionality:
+# to demonstrate the functionality of plotting metrics with error bars.
 # We use a pipeline to preprocess the data then use a
 # :py:class:`sklearn.tree.DecisionTreeClassifier` to make predictions
 
@@ -77,19 +77,19 @@ y_test_pred = complete_pipeline.predict(X_test)
 test_set_sex = X_test["race"]
 
 # %%
-# Error metric calculations
-# =========================
-# We have many different choices for error metrics. In this notebook we'll just be using a
-# `Normal approximation interval <https://en.wikipedia.org/wiki/Binomial_proportion_confidence_interval#Normal_approximation_interval>`_ (symmetric)
-# and a `Wilson score interval <https://en.wikipedia.org/wiki/Binomial_proportion_confidence_interval#Wilson_score_interval>`_ (asymmetric).
+# Confidence interval calculations
+# ================================
+# We have many different choices for calculating confidence intervals. In this notebook we'll just be using a
+# `Wilson score interval <https://en.wikipedia.org/wiki/Binomial_proportion_confidence_interval#Wilson_score_interval>`_.
 
 # We aim to create a 95% confidence interval, so we use a :code:`z_score` of 1.959964
 z_score = 1.959964
 digits_of_precision = 4
+error_labels_legend="95% Confidence Interval"
 
 
 def general_wilson(p, n, digits=4, z=1.959964):
-    """Return lower and upper bound using Wilson Interval.
+    """Return lower and upper bound of a Wilson confidence interval.
 
     Parameters
     ----------
@@ -123,7 +123,7 @@ def general_wilson(p, n, digits=4, z=1.959964):
 
 
 def recall_wilson(y_true, y_pred):
-    """Return Wilson Interval bounds for recall metric.
+    """Return a Wilson confidence interval for the recall metric.
 
     Parameters
     ----------
@@ -146,7 +146,7 @@ def recall_wilson(y_true, y_pred):
 
 
 def accuracy_wilson(y_true, y_pred):
-    """Return Wilson Interval bounds for accuracy metric.
+    """Return a Wilson confidence interval for the accuracy metric.
 
     Parameters
     ----------
@@ -169,7 +169,7 @@ def accuracy_wilson(y_true, y_pred):
 # %%
 # MetricFrame
 # ===========
-# Now we create a :class:`fairlearn.metrics.MetricFrame` to generate the recall Wilson bounds and recall normal error
+# Now we create a :class:`fairlearn.metrics.MetricFrame` to generate the Wilson bounds for accuracy and recall
 #
 
 # Analyze metrics using :class:`fairlearn.metrics.MetricFrame`
@@ -189,8 +189,8 @@ metric_frame = MetricFrame(
 # %%
 # Plotting
 # ========
-# Plot metrics without error bars
-# -------------------------------
+# Plot metrics without confidence intervals
+# -----------------------------------------
 plot_metric_frame(metric_frame, kind="scatter", metrics=["Recall", "Accuracy"])
 
 
@@ -240,7 +240,7 @@ axs = plot_metric_frame(
     metrics=["Recall", "Accuracy"],
     conf_intervals=["Recall Bounds", "Accuracy Bounds"],
     subplots=True,
-    error_labels_legend="95% Confidence Interval",
+    error_labels_legend=error_labels_legend,
     # the following parameters are passed into `pandas.DataFrame.plot` as kwargs
     layout=[1, 2],
     rot=45,
@@ -258,48 +258,3 @@ axs[0][1].set_ylabel("Accuracy")
 # Set the y-scale for both metrics to [0, 1]
 axs[0][0].set_ylim((0, 1))
 axs[0][1].set_ylim((0, 1))
-
-# %%
-# Defining custom error metrics
-# =============================
-# In this tutorial we used Normal intervals and Wilson bounds to demonstrate how to plot symmetric and asymmetric metrics, respectively.
-#
-# However, in order to support many different metrics, we reuse the :class:`fairlearn.metrics.MetricFrame` functionality by writing a function
-# that takes in :code:`y_true` and :code:`y_pred`, and returns the error metric:
-
-
-def custom_error_metric_function(y_true, y_pred):
-    assert len(y_true) == len(y_pred)
-
-    # compute custom metric function here
-    # In this case, we will calculate a wilson bound for recall
-    tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
-    bounds = general_wilson(
-        tp / (tp + fn), tp + fn, digits_of_precision, z_score
-    )
-
-    # returns the bounds in the format (lower_bound, upper_bound)
-    return bounds
-
-
-# %%
-# Create :class:`fairlearn.metrics.MetricFrame` with custom metric function
-metrics_dict = {
-    "Recall": recall_score,
-    "Recall Bounds": custom_error_metric_function,
-}
-metric_frame = MetricFrame(
-    metrics=metrics_dict,
-    y_true=y_test_true,
-    y_pred=y_test_pred,
-    sensitive_features=test_set_sex,
-)
-
-# %%
-# Plotting
-# ========
-# Plot metrics without error bars
-# -------------------------------
-plot_metric_frame(
-    metric_frame, kind="bar", metrics="Recall", conf_intervals="Recall Bounds"
-)
