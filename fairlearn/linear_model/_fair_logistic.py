777bffd1cf4ab1169586b77362b43c6e91924e23
rensoostenbach <rensoostenbach@hotmail.com>
2022-03-14 12:44:52 +0100

    Remove unused solver code

--- fairlearn/linear_model/_fair_logistic.py
+++ fairlearn/linear_model/_fair_logistic.py
@@ -42,26 +42,6 @@ def _check_solver(solver, penalty, dual):
             % (all_penalties, penalty)
         )
 
-    if solver not in ["liblinear", "saga"] and penalty not in ("l2", "none"):
-        raise ValueError(
-            "Solver %s supports only 'l2' or 'none' penalties, got %s penalty."
-            % (solver, penalty)
-        )
-    if solver != "liblinear" and dual:
-        raise ValueError(
-            "Solver %s supports only dual=False, got dual=%s" % (solver, dual)
-        )
-
-    if penalty == "elasticnet" and solver != "saga":
-        raise ValueError(
-            "Only 'saga' solver supports elasticnet penalty, got solver={}.".format(
-                solver
-            )
-        )
-
-    if solver == "liblinear" and penalty == "none":
-        raise ValueError("penalty='none' is not supported for the liblinear solver")
-
     return solver
 
 
