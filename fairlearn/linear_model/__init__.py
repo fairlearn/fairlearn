4bf39472217923673f93f8bd48d5ed4f8a86dd8a
rensoostenbach <rensoostenbach@hotmail.com>
2022-03-14 13:53:41 +0100

    Docstring on one line

--- fairlearn/linear_model/__init__.py
+++ fairlearn/linear_model/__init__.py
@@ -1,8 +1,7 @@
 # Copyright (c) Microsoft Corporation and Fairlearn contributors.
 # Licensed under the MIT License.
 
-"""This module contains existing linear models that have been adapted to work with fairness constraints.
-"""
+"""This module contains existing linear models that have been adapted to work with fairness constraints."""
 
 from ._fair_logistic import FairLogisticRegression
 
