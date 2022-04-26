cb1558180e216f7316c24d20dd56fda261181cfa
rensoostenbach <rensoostenbach@hotmail.com>
2022-04-26 16:56:08 +0200

    Docstrings and rename attributes
    Renaming attributes for consistency

--- fairlearn/linear_model/_constrained_logistic.py
+++ fairlearn/linear_model/_constrained_logistic.py
@@ -17,7 +17,6 @@ from sklearn.preprocessing import LabelEncoder, LabelBinarizer, OneHotEncoder
 from sklearn.utils import (
     check_array,
     check_consistent_length,
-    check_random_state,
     compute_class_weight,
 )
 from sklearn.utils.fixes import delayed, _joblib_parallel_args
@@ -51,53 +50,84 @@ def _check_multi_class(multi_class):
     return multi_class
 
 
-def _sensitive_attr_constraint_cov(model, x_arr, x_control, thresh):
-    """Calculate the covariance threshold as in the paper.
-
-    https://github.com/mbilalzafar/fair-classification/blob/master/fair_classification/utils.py#L348-L388
-    The covariance is computed b/w the sensitive attr val and the distance from the boundary
-    If the model is None, we assume that the y_arr_dist_boundary contains the distance from the
-    decision boundary
-    If the model is not None, we just compute a dot product or model and x_arr
-    this function will return -1 if the constraint specified by thresh parameter is not satisfied
-    otherwise it will return +1
-    if the return value is >=0, then the constraint is satisfied
+def _sensitive_attr_constraint_cov(model, X_train, A_train, covariance_bound):
     """
-    assert x_arr.shape[0] == x_control.shape[0]
+    Calculate the covariance covariance_boundold as in
+    `Zafar et al. (2017) <https://proceedings.mlr.press/v54/zafar17a.html>`_.
+
+    We one-hot-encode the sensitive features.such that they can be splitted
+    from X and that the constraints can be correctly coded per feature value.
+
+    Parameters
+    ----------
+    model : numpy.ndarray
+        Model weights.
+    X_train : numpy.ndarray or pandas.DataFrame
+        Feature data.
+    A_train : numpy.ndarray or pandas.DataFrame
+        Sensitive features.
+    covariance_bound : float
+        The given covariance threshold that we optimize towards.
+
+    Returns
+    -------
+    ans : float
+        The difference between the given threshold (covariance_bound) and covariance.
+        This will be optimized towards covariance_bound.
+    """
+    assert X_train.shape[0] == A_train.shape[0]
     if (
-        len(x_control.shape) > 1
+        len(A_train.shape) > 1
     ):  # make sure we just have one column in the array
-        assert x_control.shape[1] == 1
+        assert A_train.shape[1] == 1
 
     intercept = 0.0
 
-    num_features = x_arr.shape[1]
+    num_features = X_train.shape[1]
     if (
         model.size == num_features + 1
     ):  # True if the intercept needs to be fitted
         intercept = model[-1]
         model = model[:-1]
 
-    arr = np.dot(model, x_arr.T) + intercept
+    arr = np.dot(model, X_train.T) + intercept
     # the product with the weight vector -- the sign of this is the output label
 
     arr = np.array(arr, dtype=np.float64)
-
-    cov = np.dot(x_control - np.mean(x_control), arr) / float(len(x_control))
-
-    ans = thresh - abs(cov)
-    print(f"Ans: {ans}")
+    cov = np.dot(A_train - np.mean(A_train), arr) / float(len(A_train))
+    ans = covariance_bound - abs(cov)
     return ans
 
 
 def _get_constraint_list_cov(
     X_train,
     A_train,
-    y_train,
     renamed_sensitive_feature_ids,
     categories,
     covariance_bound,
 ):
+    """
+    Collect all constraints in a list.
+
+    Parameters
+    ----------
+    X_train : numpy.ndarray or pandas.DataFrame
+        Feature data.
+    A_train : numpy.ndarray or pandas.DataFrame
+        Sensitive features.
+    renamed_sensitive_feature_ids : list
+        The renamed sensitive feature ids, either as strings or as numbers
+    categories : list
+        The categories from the encoder
+    covariance_bound : float
+        The given covariance threshold that we optimize towards.
+
+    Returns
+    -------
+    constraints : list
+        A list in which each element contains a correctly
+        formatted constraint per sensitive feature.
+    """
     # New covariance bound list such that it correctly handles the OHE sensitive features
     # Now we can have a separate covariance bound per sensitive feature
     cov_bound = []
@@ -147,7 +177,7 @@ def _logistic_regression_path(
     dual=False,
     penalty="l2",
     intercept_scaling=1.0,
-    multi_class="auto",
+    multi_class="ovr",
     random_state=None,
     check_input=True,
     max_squared_sum=None,
@@ -156,11 +186,15 @@ def _logistic_regression_path(
     constraints=None,
 ):
     """
-    TODO: add docstring.
+    Compute a Logistic Regression model for a list of regularization
+    parameters.
 
-    All this code is copied from the sklearn _logistic_regression_path function,
+    All code comes from the sklearn _logistic_regression_path function,
     except for the constraints argument and the changed solver
-    (all in the optimize.minimize function)
+    (all in the optimize.minimize function). Some parts of the original
+    code in sklearn might be removed, since they are not relevant here.
+    For example, we have only "ovr" implemented as multi_class
+    option, meaning that we don't need code for the multinomial case.
     """
     if isinstance(Cs, numbers.Integral):
         Cs = np.logspace(-4, 4, Cs)
@@ -180,7 +214,6 @@ def _logistic_regression_path(
     _, n_features = X.shape
 
     classes = np.unique(y)
-    random_state = check_random_state(random_state)
 
     multi_class = _check_multi_class(multi_class)
     if pos_class is None:
@@ -207,70 +240,27 @@ def _logistic_regression_path(
         sample_weight *= class_weight_[le.fit_transform(y)]
 
     # For doing a ovr, we need to mask the labels first.
-    if multi_class == "ovr":
-        w0 = np.zeros(n_features + int(fit_intercept), dtype=X.dtype)
-        mask_classes = np.array([-1, 1])
-        mask = y == pos_class
-        y_bin = np.ones(y.shape, dtype=X.dtype)
-        y_bin[~mask] = -1.0
-        # for compute_class_weight
+    w0 = np.zeros(n_features + int(fit_intercept), dtype=X.dtype)
+    mask_classes = np.array([-1, 1])
+    mask = y == pos_class
+    y_bin = np.ones(y.shape, dtype=X.dtype)
+    y_bin[~mask] = -1.0
+    # for compute_class_weight
 
-        if class_weight == "balanced":
-            class_weight_ = compute_class_weight(
-                class_weight, classes=mask_classes, y=y_bin
-            )
-            sample_weight *= class_weight_[le.fit_transform(y_bin)]
-
-    else:
-        lbin = LabelBinarizer()
-        Y_multi = lbin.fit_transform(y)
-        if Y_multi.shape[1] == 1:
-            Y_multi = np.hstack([1 - Y_multi, Y_multi])
-
-        w0 = np.zeros(
-            (classes.size, n_features + int(fit_intercept)),
-            order="F",
-            dtype=X.dtype,
+    if class_weight == "balanced":
+        class_weight_ = compute_class_weight(
+            class_weight, classes=mask_classes, y=y_bin
         )
+        sample_weight *= class_weight_[le.fit_transform(y_bin)]
 
     if coef is not None:
         # it must work both giving the bias term and not
-        if multi_class == "ovr":
-            if coef.size not in (n_features, w0.size):
-                raise ValueError(
-                    "Initialization coef is of shape %d, expected shape %d or %d"
-                    % (coef.size, n_features, w0.size)
-                )
-            w0[: coef.size] = coef
-        else:
-            # For binary problems coef.shape[0] should be 1, otherwise it
-            # should be classes.size.
-            n_classes = classes.size
-            if n_classes == 2:
-                n_classes = 1
-
-            if coef.shape[0] != n_classes or coef.shape[1] not in (
-                n_features,
-                n_features + 1,
-            ):
-                raise ValueError(
-                    "Initialization coef is of shape (%d, %d), expected "
-                    "shape (%d, %d) or (%d, %d)"
-                    % (
-                        coef.shape[0],
-                        coef.shape[1],
-                        classes.size,
-                        n_features,
-                        classes.size,
-                        n_features + 1,
-                    )
-                )
-
-            if n_classes == 1:
-                w0[0, : coef.shape[1]] = -coef
-                w0[1, : coef.shape[1]] = coef
-            else:
-                w0[:, : coef.shape[1]] = coef
+        if coef.size not in (n_features, w0.size):
+            raise ValueError(
+                "Initialization coef is of shape %d, expected shape %d or %d"
+                % (coef.size, n_features, w0.size)
+            )
+        w0[: coef.size] = coef
 
     target = y_bin
     func = _logistic_loss
@@ -358,9 +348,129 @@ def _ohe_sensitive_features(X, sensitive_feature_ids):
 
 
 class ConstrainedLogisticRegression(LogisticRegression):
-    """TODO: add docstring.
+    r"""Constrained logistic regression.
 
-    Check in BalancedRandomForestClassifier how they handle docstrings for inherited class
+    This implementation closely follows the implementation in
+    :class:`sklearn.linear_model.LogisticRegression`, and is constrained
+    via the same way as in
+    `Zafar et al. (2017) <https://proceedings.mlr.press/v54/zafar17a.html>`_ [1]_.
+
+    The constraint is defined as the covariance between the sensitive
+    features :math:`\left\{\mathbf{z}_{i}\right\}_{i=1}^{N}`, and the
+    signed distance from the users` feature vectors to the decision
+    boundary :math:`\left\{d_{\boldsymbol{\theta}}\left(\mathbf{x}_{i}\right)\right\}_{i=1}^{N}`.
+    This is mathemetically described as follows:
+
+    .. math::
+      \operatorname{Cov}\left(\mathbf{z}, d_{\boldsymbol{\theta}}(\mathbf{x})\right)
+      =\mathbb{E}\left[(\mathbf{z}-\overline{\mathbf{z}})
+        d_{\boldsymbol{\theta}}(\mathbf{x})\right]
+        -\mathbb{E}[(\mathbf{z}-\overline{\mathbf{z}})]
+        \bar{d}_{\boldsymbol{\theta}}(\mathbf{x})\\
+      \approx \frac{1}{N} \sum_{i=1}^{N}\left(\mathbf{z}_{i}-\overline{\mathbf{z}}\right)
+      d_{\boldsymbol{\theta}}\left(\mathbf{x}_{i}\right)
+
+    The reasoning behind this derivation is further explained in [1]_.
+
+    Parameters
+    ----------
+    constraints : {'demographic_parity', 'none'}, default='demographic_parity'
+        The constraints used in the logistic regression. Currently,
+        only the constraint as in [1]_ is implemented via the
+        `'demographic_parity'` parameter. `'none'` will fall back to
+        the LogisticRegression as in sklearn.
+    penalty : {'l2', 'none'}, default='l2'
+        Specify the norm of the penalty:
+        - `'none'`: no penalty is added;
+        - `'l2'`: add a L2 penalty term and it is the default choice;
+    covariance_bound : float, default=0
+        The covariance bound that the constraint optimizes towards.
+        It can either be one of the following two:
+        - float: A single float that will be used for all sensitive features
+        - list: A list of floats such that each sensitive feature can have
+                its own covariance bound.
+    dual : bool, default=False
+        Dual or primal formulation. This parameter is not used.
+        Only implemented for compatibility with sklearn.
+    tol : float, default=1e-4
+        Tolerance for stopping criteria.
+    C : float, default=1.0
+        Inverse of regularization strength; must be a positive float.
+        Like in support vector machines, smaller values specify stronger
+        regularization.
+    fit_intercept : bool, default=True
+        Specifies if a constant (a.k.a. bias or intercept) should be
+        added to the decision function.
+    intercept_scaling : float, default=1
+        This parameter is not used.
+        Only implemented for compatibility with sklearn.
+    class_weight : dict or 'balanced', default=None
+        Weights associated with classes in the form ``{class_label: weight}``.
+        If not given, all classes are supposed to have weight one.
+        The "balanced" mode uses the values of y to automatically adjust
+        weights inversely proportional to class frequencies in the input data
+        as ``n_samples / (n_classes * np.bincount(y))``.
+        Note that these weights will be multiplied with sample_weight (passed
+        through the fit method) if sample_weight is specified.
+    random_state : int, RandomState instance, default=None
+        This parameter is not used.
+        Only implemented for compatibility with sklearn.
+    solver : {'SLSQP'}, \
+            default='SLSQP'
+        Algorithm to use in the optimization problem.
+        SLSQP is used since it works with constraints. Other (default) solvers
+        in sklearn are not compatible with constraints.
+    max_iter : int, default=100
+        Maximum number of iterations taken for the solvers to converge.
+    multi_class : {'ovr'}, default='ovr'
+        If the option chosen is 'ovr', then a binary problem is fit for each
+        label. Sklearn also provides a 'multinomial' option, but this is not
+        compatible with the constraints.
+    verbose : int, default=0
+        Set verbose to any positive number for verbosity.
+    warm_start : bool, default=False
+        When set to True, reuse the solution of the previous call to fit as
+        initialization, otherwise, just erase the previous solution.
+        See `the glossary <https://scikit-learn.org/stable/glossary.html#term-warm_start>`.
+    n_jobs : int, default=None
+        Number of CPU cores used when parallelizing over classes if
+        multi_class='ovr'". ``None`` means 1 unless in a :obj:`joblib.parallel_backend`
+        context. ``-1`` means using all processors.
+        See `the glossary <https://scikit-learn.org/stable/glossary.html#term-n_jobs>`
+        for more details.
+    l1_ratio : float, default=None
+        This parameter is not used.
+        Only implemented for compatibility with sklearn.
+    Attributes
+    ----------
+    classes_ : ndarray of shape (n_classes, )
+        A list of class labels known to the classifier.
+    coef_ : ndarray of shape (1, n_features) or (n_classes, n_features)
+        Coefficient of the features in the decision function.
+        `coef_` is of shape (1, n_features) when the given problem is binary.
+        In particular, when `multi_class='multinomial'`, `coef_` corresponds
+        to outcome 1 (True) and `-coef_` corresponds to outcome 0 (False).
+    intercept_ : ndarray of shape (1,) or (n_classes,)
+        Intercept (a.k.a. bias) added to the decision function.
+        If `fit_intercept` is set to False, the intercept is set to zero.
+        `intercept_` is of shape (1,) when the given problem is binary.
+    n_features_in_ : int
+        Number of features seen during :term:`fit`.
+    feature_names_in_ : ndarray of shape (`n_features_in_`,)
+        Names of features seen during :term:`fit`. Defined only when `X`
+        has feature names that are all strings.
+    n_iter_ : ndarray of shape (n_classes,) or (1, )
+        Actual number of iterations for all classes. If binary or multinomial,
+        it returns only 1 element. For liblinear solver, only the maximum
+        number of iteration across all classes is given.
+
+    References
+    ----------
+    .. [1] Zafar, Muhammad Bilal, et al.
+       "Fairness constraints: Mechanisms for fair classification."
+       Artificial Intelligence and Statistics. PMLR, 2017.
+       [Online]. Available: https://proceedings.mlr.press/v54/zafar17a.html.
+
     """
 
     def __init__(
@@ -452,7 +562,30 @@ class ConstrainedLogisticRegression(LogisticRegression):
         sample_weight=None,
         sensitive_feature_ids=None,
     ):
-        """TODO: add docstring."""
+        """
+        Fit the model according to the given training data and sensitive features.
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape (n_samples, n_features)
+            Training vector, where `n_samples` is the number of samples and
+            `n_features` is the number of features.
+        y : array-like of shape (n_samples,)
+            Target vector relative to X.
+        sample_weight : array-like of shape (n_samples,) default=None
+            Array of weights that are assigned to individual samples.
+            If not provided, then each sample is given unit weight.
+        sensitive_feature_ids : list
+        sensitive features in the data, either as strings (DataFrame) or numbers (numpy)
+
+        Returns
+        -------
+        self
+            Fitted estimator.
+        Notes
+        -----
+        If `self.constraints` is None, the Logistic Regression from sklearn
+        will be used with the parameters that you supplied.
+        """
         if self.constraints is None:
             clf = LogisticRegression(
                 penalty=self.penalty,
@@ -504,17 +637,16 @@ class ConstrainedLogisticRegression(LogisticRegression):
         # Split the data similarly to how the CorrelationRemover does it
         self._create_lookup(X_ohe)
         (
-            X_nonsensitive,
-            X_sensitive,
+            X_train,
+            A_train,
             renamed_sensitive_feature_ids,
         ) = self._split_X(X_ohe, renamed_sensitive_feature_ids)
 
         # TODO: Think about whether the constraints should be
         #  implemented in `fit`, or in `_logistic_regression_path`
         constraints = _get_constraint_list_cov(
-            X_nonsensitive,
-            X_sensitive,
-            y,
+            X_train,
+            A_train,
             renamed_sensitive_feature_ids,
             categories,
             self.covariance_bound,
@@ -554,8 +686,8 @@ class ConstrainedLogisticRegression(LogisticRegression):
         else:
             _dtype = [np.float64, np.float32]
 
-        X_nonsensitive, y = self._validate_data(
-            X_nonsensitive,
+        X_train, y = self._validate_data(
+            X_train,
             y,
             accept_sparse="csr",
             dtype=_dtype,
@@ -603,7 +735,7 @@ class ConstrainedLogisticRegression(LogisticRegression):
             **_joblib_parallel_args(prefer=prefer),
         )(
             path_func(
-                X_nonsensitive,  # Only use the nonsensitive features
+                X_train,  # Only use the nonsensitive features
                 y,
                 pos_class=class_,
                 Cs=[C_],
@@ -629,7 +761,7 @@ class ConstrainedLogisticRegression(LogisticRegression):
         fold_coefs_, _, n_iter_ = zip(*fold_coefs_)
         self.n_iter_ = np.asarray(n_iter_, dtype=np.int32)[:, 0]
 
-        n_features = X_nonsensitive.shape[1]
+        n_features = X_train.shape[1]
 
         self.coef_ = np.asarray(fold_coefs_)
         self.coef_ = self.coef_.reshape(
