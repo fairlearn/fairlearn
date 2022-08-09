7e3b8516ddb649b108a97ae45792c70608173f34
SeanMcCarren <mccarrensean@gmail.com>
2022-08-09 17:37:51 +0200

    fix tests with pytest.raises

--- test/unit/metrics/test_model_comparison_plot.py
+++ test/unit/metrics/test_model_comparison_plot.py
@@ -5,6 +5,7 @@ from fairlearn.metrics import plot_model_comparison, make_derived_metric, select
 from sklearn.metrics import accuracy_score
 
 from .data_for_test import g_1, y_p, y_t
+import pytest
 
 
 def test_full():
@@ -25,20 +26,21 @@ def test_full():
 
 
 def test_dict():
-    ax = plot_model_comparison(
+    plot_model_comparison(
         x_axis_metric=accuracy_score,
         y_axis_metric=make_derived_metric(
             metric=selection_rate, transform="difference"
         ),
         y_true=y_t,
-        y_preds={'true':y_t, 'false':y_p},
+        y_preds={"true": y_t, "false": y_p},
         sensitive_features=g_1,
         point_labels=True,
         show_plot=False,
     )
 
+
 def test_reshape():
-    ax = plot_model_comparison(
+    plot_model_comparison(
         x_axis_metric=accuracy_score,
         y_axis_metric=make_derived_metric(
             metric=selection_rate, transform="difference"
@@ -46,137 +48,139 @@ def test_reshape():
         y_true=y_t,
         y_preds=y_p,
         sensitive_features=g_1,
-        point_labels=True,
-        show_plot=False,
     )
 
+
 def test_wrong_shape():
-    ax = plot_model_comparison(
-        x_axis_metric=accuracy_score,
-        y_axis_metric=make_derived_metric(
-            metric=selection_rate, transform="difference"
-        ),
-        y_true=y_t,
-        y_preds=y_p + [1],
-        sensitive_features=g_1,
-        point_labels=True,
-        show_plot=False,
-    )
+    with pytest.raises(Exception):
+        plot_model_comparison(
+            x_axis_metric=accuracy_score,
+            y_axis_metric=make_derived_metric(
+                metric=selection_rate, transform="difference"
+            ),
+            y_true=y_t,
+            y_preds=y_p + [1],
+            sensitive_features=g_1,
+            point_labels=True,
+            show_plot=False,
+        )
 
 
 def test_kw_error_labels1():
-    ax = plot_model_comparison(
-        x_axis_metric=accuracy_score,
-        y_axis_metric=make_derived_metric(
-            metric=selection_rate, transform="difference"
-        ),
-        y_true=y_t,
-        y_preds=y_p,
-        sensitive_features=g_1,
-        axis_labels="true"
-    )
+    with pytest.raises(Exception):
+        plot_model_comparison(
+            x_axis_metric=accuracy_score,
+            y_axis_metric=make_derived_metric(
+                metric=selection_rate, transform="difference"
+            ),
+            y_true=y_t,
+            y_preds=y_p,
+            sensitive_features=g_1,
+            axis_labels="true",
+        )
+
 
 def test_kw_error_labels2():
-    ax = plot_model_comparison(
-        x_axis_metric=accuracy_score,
-        y_axis_metric=make_derived_metric(
-            metric=selection_rate, transform="difference"
-        ),
-        y_true=y_t,
-        y_preds=y_p,
-        sensitive_features=g_1,
-        axis_labels=(1, 2, 3)
-    )
+    with pytest.raises(Exception):
+        plot_model_comparison(
+            x_axis_metric=accuracy_score,
+            y_axis_metric=make_derived_metric(
+                metric=selection_rate, transform="difference"
+            ),
+            y_true=y_t,
+            y_preds=y_p,
+            sensitive_features=g_1,
+            axis_labels=(1, 2, 3),
+        )
 
 
 def test_kw_error_bools():
-    ax = plot_model_comparison(
-        x_axis_metric=accuracy_score,
-        y_axis_metric=make_derived_metric(
-            metric=selection_rate, transform="difference"
-        ),
-        y_true=y_t,
-        y_preds=y_p,
-        sensitive_features=g_1,
-        show_plot="true",
-        legend="true"
-    )
+    with pytest.raises(Exception):
+        plot_model_comparison(
+            x_axis_metric=accuracy_score,
+            y_axis_metric=make_derived_metric(
+                metric=selection_rate, transform="difference"
+            ),
+            y_true=y_t,
+            y_preds=y_p,
+            sensitive_features=g_1,
+            show_plot="true",
+            legend="true",
+        )
 
 
 def test_kw_error_point_labels1():
-    ax = plot_model_comparison(
-        x_axis_metric=accuracy_score,
-        y_axis_metric=make_derived_metric(
-            metric=selection_rate, transform="difference"
-        ),
-        y_true=y_t,
-        y_preds=y_p,
-        sensitive_features=g_1,
-        point_labels=[1,2,3]
-    )
+    with pytest.raises(Exception):
+        plot_model_comparison(
+            x_axis_metric=accuracy_score,
+            y_axis_metric=make_derived_metric(
+                metric=selection_rate, transform="difference"
+            ),
+            y_true=y_t,
+            y_preds=y_p,
+            sensitive_features=g_1,
+            point_labels=[1, 2, 3],
+        )
 
 
 def test_kw_error_point_labels2():
-    ax = plot_model_comparison(
-        x_axis_metric=accuracy_score,
-        y_axis_metric=make_derived_metric(
-            metric=selection_rate, transform="difference"
-        ),
-        y_true=y_t,
-        y_preds=y_p,
-        sensitive_features=g_1,
-        point_labels="label"
-    )
+    with pytest.raises(Exception):
+        plot_model_comparison(
+            x_axis_metric=accuracy_score,
+            y_axis_metric=make_derived_metric(
+                metric=selection_rate, transform="difference"
+            ),
+            y_true=y_t,
+            y_preds=y_p,
+            sensitive_features=g_1,
+            point_labels="label",
+        )
 
 
 def test_kw_error_point_label_position():
-    ax = plot_model_comparison(
-        x_axis_metric=accuracy_score,
-        y_axis_metric=make_derived_metric(
-            metric=selection_rate, transform="difference"
-        ),
-        y_true=y_t,
-        y_preds=y_p,
-        sensitive_features=g_1,
-        point_labels_position="okay"
-    )
+    with pytest.raises(Exception):
+        plot_model_comparison(
+            x_axis_metric=accuracy_score,
+            y_axis_metric=make_derived_metric(
+                metric=selection_rate, transform="difference"
+            ),
+            y_true=y_t,
+            y_preds=y_p,
+            sensitive_features=g_1,
+            point_labels_position="okay",
+        )
 
 
 def test_kw_error_point_label_position2():
-    ax = plot_model_comparison(
-        x_axis_metric=accuracy_score,
-        y_axis_metric=make_derived_metric(
-            metric=selection_rate, transform="difference"
-        ),
-        y_true=y_t,
-        y_preds=y_p,
-        sensitive_features=g_1,
-        point_labels_position=(1, "okay")
-    )
+    with pytest.raises(Exception):
+        plot_model_comparison(
+            x_axis_metric=accuracy_score,
+            y_axis_metric=make_derived_metric(
+                metric=selection_rate, transform="difference"
+            ),
+            y_true=y_t,
+            y_preds=y_p,
+            sensitive_features=g_1,
+            point_labels_position=(1, "okay"),
+        )
 
 
 def test_kw_error_point_legend_kws():
-    ax = plot_model_comparison(
-        x_axis_metric=accuracy_score,
-        y_axis_metric=make_derived_metric(
-            metric=selection_rate, transform="difference"
-        ),
-        y_true=y_t,
-        y_preds=y_p,
-        sensitive_features=g_1,
-        legend_kwargs=1
-    )
-
-
-
-
-
-
-
+    with pytest.raises(Exception):
+        plot_model_comparison(
+            x_axis_metric=accuracy_score,
+            y_axis_metric=make_derived_metric(
+                metric=selection_rate, transform="difference"
+            ),
+            y_true=y_t,
+            y_preds=y_p,
+            sensitive_features=g_1,
+            legend_kwargs=1,
+        )
 
 
 def test_other_order():
-    ax = plot_model_comparison(
+    plot_model_comparison(
         y_axis_metric=accuracy_score,
         x_axis_metric=make_derived_metric(
             metric=selection_rate, transform="difference"
