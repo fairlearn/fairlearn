a5d57a75f9ac292e670e7c3680fba62d01ae9b3e
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2022-09-20 08:24:09 -0400

    Refactor to some fixtures

--- test/unit/metrics/test_metricframe_bootstrap.py
+++ test/unit/metrics/test_metricframe_bootstrap.py
@@ -10,61 +10,63 @@ from fairlearn.metrics import MetricFrame
 
 from .data_for_test import g_1, g_2, y_p, y_t
 
+# Have fixtures so that tests can be specific without
+# constantly recomputing the same bootstrapping
+
+@pytest.fixture(scope='session')
+def mf_1m_0cf():
+    n_samples = 100
+    target = MetricFrame(
+        metrics=skm.recall_score,
+        y_true=y_t,
+        y_pred=y_p,
+        sensitive_features=g_1,
+        n_bootstrap_samples=n_samples,
+        bootstrap_random_state=13489623,
+    )
+    return target
+
+@pytest.fixture(scope='session')
+def mf_1m_1_cf():
+    n_samples = 100
+    target = MetricFrame(
+        metrics=skm.recall_score,
+        y_true=y_t,
+        y_pred=y_p,
+        sensitive_features=g_1,
+        control_features=g_2,
+        n_bootstrap_samples=n_samples,
+        bootstrap_random_state=13489623,
+    )
+    return target
+
 
 class TestOverallQuantiles:
-    def test_1m_0cf(self):
-        n_samples = 100
+    def test_1m_0cf(self, mf_1m_0cf):
         quantiles = [0.2, 0.5, 0.9]
-        target = MetricFrame(
-            metrics=skm.recall_score,
-            y_true=y_t,
-            y_pred=y_p,
-            sensitive_features=g_1,
-            n_bootstrap_samples=n_samples,
-            bootstrap_random_state=13489623,
-        )
-        overall_quantiles = target.overall_quantiles(quantiles)
+        overall_quantiles = mf_1m_0cf.overall_quantiles(quantiles)
         assert isinstance(overall_quantiles, np.ndarray)
         assert overall_quantiles.shape == (len(quantiles),)
         # Overall value should be close to quantile 0.5
-        assert target.overall == pytest.approx(overall_quantiles[1], abs=0.05)
+        assert mf_1m_0cf.overall == pytest.approx(overall_quantiles[1], abs=0.05)
 
-    def test_1m_1_cf(self):
-        n_samples = 100
+    def test_1m_1_cf(self, mf_1m_1_cf):
         quantiles = [0.2, 0.5, 0.9]
-        target = MetricFrame(
-            metrics=skm.recall_score,
-            y_true=y_t,
-            y_pred=y_p,
-            sensitive_features=g_1,
-            control_features=g_2,
-            n_bootstrap_samples=n_samples,
-            bootstrap_random_state=13489623,
-        )
-        overall_quantiles = target.overall_quantiles(quantiles)
+        overall_quantiles = mf_1m_1_cf.overall_quantiles(quantiles)
         assert isinstance(overall_quantiles, pd.Series)
         assert overall_quantiles.name == "recall_score"
         assert overall_quantiles.shape == (2,)
         # Overall value should be close to quantile 0.5
-        assert target.overall["f"] == pytest.approx(overall_quantiles["f"][1], abs=0.05)
-        assert target.overall["g"] == pytest.approx(overall_quantiles["g"][1], abs=0.05)
+        assert mf_1m_1_cf.overall["f"] == pytest.approx(overall_quantiles["f"][1], abs=0.05)
+        assert mf_1m_1_cf.overall["g"] == pytest.approx(overall_quantiles["g"][1], abs=0.05)
 
 
 class TestGroupMax:
-    def test_1m_0cf(self):
-        n_samples = 100
+    def test_1m_0cf(self, mf_1m_0cf):
         quantiles = [0.2, 0.5, 0.9]
-        target = MetricFrame(
-            metrics=skm.recall_score,
-            y_true=y_t,
-            y_pred=y_p,
-            sensitive_features=g_1,
-            n_bootstrap_samples=n_samples,
-            bootstrap_random_state=689692,
-        )
 
-        baseline = target.group_max()
-        ci_vals = target.group_max(quantiles=quantiles)
+        baseline = mf_1m_0cf.group_max()
+        ci_vals = mf_1m_0cf.group_max(quantiles=quantiles)
         assert isinstance(ci_vals, np.ndarray)
         assert ci_vals.shape == (len(quantiles),)
         # Median should be close to baseline
@@ -72,21 +74,11 @@ class TestGroupMax:
         assert ci_vals[0] < ci_vals[1]
         assert ci_vals[1] < ci_vals[2]
 
-    def test_1m_1cf(self):
-        n_samples = 100
+    def test_1m_1cf(self, mf_1m_1_cf):
         quantiles = [0.2, 0.5, 0.9]
-        target = MetricFrame(
-            metrics=skm.accuracy_score,
-            y_true=y_t,
-            y_pred=y_p,
-            sensitive_features=g_1,
-            control_features=g_2,
-            n_bootstrap_samples=n_samples,
-            bootstrap_random_state=18992,
-        )
 
-        baseline = target.group_max()
-        ci_vals = target.group_max(quantiles=quantiles)
+        baseline = mf_1m_1_cf.group_max()
+        ci_vals = mf_1m_1_cf.group_max(quantiles=quantiles)
         assert isinstance(ci_vals, pd.Series)
         assert ci_vals.shape == (2,)
         # Median should be close to baseline in each case
