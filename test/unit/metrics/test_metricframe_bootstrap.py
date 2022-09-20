d59e308260ae238f6f5f978a662b6e60dad81232
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2022-09-20 08:40:07 -0400

    Start on case where a dictionary of metrics was supplied

--- test/unit/metrics/test_metricframe_bootstrap.py
+++ test/unit/metrics/test_metricframe_bootstrap.py
@@ -29,6 +29,20 @@ def mf_1m_0cf():
 
 
 @pytest.fixture(scope="session")
+def mf_1mdict_0cf():
+    n_samples = 100
+    target = MetricFrame(
+        metrics={"recall": skm.recall_score},
+        y_true=y_t,
+        y_pred=y_p,
+        sensitive_features=g_1,
+        n_bootstrap_samples=n_samples,
+        bootstrap_random_state=13489623,
+    )
+    return target
+
+
+@pytest.fixture(scope="session")
 def mf_1m_1cf():
     n_samples = 100
     target = MetricFrame(
@@ -51,6 +65,18 @@ class TestOverallQuantiles:
         assert overall_quantiles.shape == (len(quantiles),)
         # Overall value should be close to quantile 0.5
         assert mf_1m_0cf.overall == pytest.approx(overall_quantiles[1], abs=0.05)
+
+    def test_1mdict_0cf(self, mf_1mdict_0cf):
+        quantiles = [0.2, 0.5, 0.9]
+        overall_quantiles = mf_1mdict_0cf.overall_quantiles(quantiles)
+        assert isinstance(overall_quantiles, pd.Series)
+        assert len(overall_quantiles) == 1
+        assert isinstance(overall_quantiles["recall"], np.ndarray)
+        assert overall_quantiles["recall"].shape == (len(quantiles),)
+        # Overall value should be close to quantile 0.5
+        assert mf_1mdict_0cf.overall["recall"] == pytest.approx(
+            overall_quantiles["recall"][1], abs=0.05
+        )
 
     def test_1m_1_cf(self, mf_1m_1cf):
         quantiles = [0.2, 0.5, 0.9]
