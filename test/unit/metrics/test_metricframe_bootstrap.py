55639dd2f4ac569c8d619a43f3eb1d11162387e8
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2022-09-20 08:33:07 -0400

    by_group tests

--- test/unit/metrics/test_metricframe_bootstrap.py
+++ test/unit/metrics/test_metricframe_bootstrap.py
@@ -29,7 +29,7 @@ def mf_1m_0cf():
 
 
 @pytest.fixture(scope="session")
-def mf_1m_1_cf():
+def mf_1m_1cf():
     n_samples = 100
     target = MetricFrame(
         metrics=skm.recall_score,
@@ -52,17 +52,17 @@ class TestOverallQuantiles:
         # Overall value should be close to quantile 0.5
         assert mf_1m_0cf.overall == pytest.approx(overall_quantiles[1], abs=0.05)
 
-    def test_1m_1_cf(self, mf_1m_1_cf):
+    def test_1m_1_cf(self, mf_1m_1cf):
         quantiles = [0.2, 0.5, 0.9]
-        overall_quantiles = mf_1m_1_cf.overall_quantiles(quantiles)
+        overall_quantiles = mf_1m_1cf.overall_quantiles(quantiles)
         assert isinstance(overall_quantiles, pd.Series)
         assert overall_quantiles.name == "recall_score"
         assert overall_quantiles.shape == (2,)
         # Overall value should be close to quantile 0.5
-        assert mf_1m_1_cf.overall["f"] == pytest.approx(
+        assert mf_1m_1cf.overall["f"] == pytest.approx(
             overall_quantiles["f"][1], abs=0.05
         )
-        assert mf_1m_1_cf.overall["g"] == pytest.approx(
+        assert mf_1m_1cf.overall["g"] == pytest.approx(
             overall_quantiles["g"][1], abs=0.05
         )
 
@@ -79,6 +79,19 @@ class TestByGroupQuantiles:
             # Check median close to nominal
             assert curr[1] == pytest.approx(mf_1m_0cf.by_group[g], abs=0.05)
 
+    def test_1m_1cf(self, mf_1m_1cf):
+        quantiles = [0.2, 0.5, 0.9]
+        by_group_quantiles = mf_1m_1cf.by_group_quantiles(quantiles)
+        assert isinstance(by_group_quantiles, pd.Series)
+        for cf in np.unique(g_2):
+            for g in np.unique(g_1):
+                curr = by_group_quantiles[cf][g]
+                assert isinstance(curr, np.ndarray)
+                assert curr.shape == (3,)
+                # Check median close to nominal
+                assert curr[1] == pytest.approx(mf_1m_1cf.by_group[cf][g], abs=0.05)
+
+
 
 class TestGroupMax:
     def test_1m_0cf(self, mf_1m_0cf):
@@ -93,11 +106,11 @@ class TestGroupMax:
         assert ci_vals[0] < ci_vals[1]
         assert ci_vals[1] < ci_vals[2]
 
-    def test_1m_1cf(self, mf_1m_1_cf):
+    def test_1m_1cf(self, mf_1m_1cf):
         quantiles = [0.2, 0.5, 0.9]
 
-        baseline = mf_1m_1_cf.group_max()
-        ci_vals = mf_1m_1_cf.group_max(quantiles=quantiles)
+        baseline = mf_1m_1cf.group_max()
+        ci_vals = mf_1m_1cf.group_max(quantiles=quantiles)
         assert isinstance(ci_vals, pd.Series)
         assert ci_vals.shape == (2,)
         # Median should be close to baseline in each case
