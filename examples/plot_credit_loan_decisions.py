e9a05a71fb67cfa1e1ddc558b1ab0c66db2bee2f
Miro Dudik <mdudik@gmail.com>
2023-01-04 18:13:44 -0500

    Revert "test sphinx (binary search iter 3)"
    This reverts commit 0021c870212c805bf95eab98ba9f2cff6975c039.

--- examples/plot_credit_loan_decisions.py
+++ examples/plot_credit_loan_decisions.py
@@ -580,8 +580,7 @@ metricframe_unmitigated.by_group[metrics_to_report]
 metricframe_unmitigated.difference()[metrics_to_report]
 
 metricframe_unmitigated.overall[metrics_to_report]
-# exit() # bin search iteration 1: 10min
-
+#exit()
 
 # %%
 def plot_group_metrics_with_error_bars(metricframe, metric, error_name):
@@ -789,7 +788,6 @@ compare_metricframe_results(
 metricframe_postprocess.by_group[metrics_to_report].plot.bar(
     subplots=True, layout=[1, 3], figsize=[12, 4], legend=None, rot=0
 )
-exit() # bin search iteration 3
 
 
 # %%
@@ -1043,7 +1041,7 @@ def model_performance_sweep(models_dict, X_test, y_test, A_test):
 
 
 # %%
-exit() # bin search iteration 2: timeout
+exit()
 performance_df = model_performance_sweep(all_models, X_test, y_test, A_test)
 
 # %%
