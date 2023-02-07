f81f4ac81d13d83695cdfaae15c9ff98d42619d5
Roman Lutz <romanlutz13@gmail.com>
2023-02-06 23:42:22 -0500

    fairness -> fairness_metrics, remove mean over/underprediction

--- fairlearn/metrics/_base_metrics.py
+++ fairlearn/metrics/_base_metrics.py
@@ -287,67 +287,6 @@ def mean_prediction(y_true, y_pred, sample_weight=None) -> float:
     return np.dot(y_p, s_w) / s_w.sum()
 
 
-def _mean_overprediction(y_true, y_pred, sample_weight=None) -> float:
-    """Calculate the (weighted) mean overprediction.
-
-    This is the (weighted) mean of the error where any negative
-    errors (i.e. underpredictions) are set to zero
-
-    Parameters
-    ----------
-    y_true : array_like
-        The true values
-
-    y_pred : array_like
-        The predicted values
-
-    sample_weight : array_like
-        Optional array of sample weights
-    """
-    y_t = _convert_to_ndarray_and_squeeze(y_true)
-    y_p = _convert_to_ndarray_and_squeeze(y_pred)
-    s_w = np.ones(len(y_p))
-    if sample_weight is not None:
-        s_w = _convert_to_ndarray_and_squeeze(sample_weight)
-
-    err = y_p - y_t
-    err[err < 0] = 0
-
-    return np.dot(err, s_w) / s_w.sum()
-
-
-def _mean_underprediction(y_true, y_pred, sample_weight=None) -> float:
-    """Calculate the (weighted) mean underprediction.
-
-    This is the (weighted) mean of the error where any
-    positive errors (i.e. overpredictions) are set to zero.
-    The absolute value of the underpredictions is used, so the
-    returned value is always positive.
-
-    Parameters
-    ----------
-    y_true : array_like
-        The true values
-
-    y_pred : array_like
-        The predicted values
-
-    sample_weight : array_like
-        Optional array of sample weights
-    """
-    y_t = _convert_to_ndarray_and_squeeze(y_true)
-    y_p = _convert_to_ndarray_and_squeeze(y_pred)
-    s_w = np.ones(len(y_p))
-    if sample_weight is not None:
-        s_w = _convert_to_ndarray_and_squeeze(sample_weight)
-
-    err = y_p - y_t
-    err[err > 0] = 0
-
-    # Error metrics should decrease to 0 so have to flip sign
-    return -np.dot(err, s_w) / s_w.sum()
-
-
 def selection_rate(y_true, y_pred, *, pos_label: Any = 1, sample_weight=None) -> float:
     """Calculate the fraction of predicted labels matching the 'good' outcome.
 
