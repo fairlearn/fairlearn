5d00cf91a4d8220eb3a6464c321daa71475b81fb
Roman Lutz <romanlutz13@gmail.com>
2023-02-07 14:28:16 -0500

    [ENH] merge metric files into _base_metrics.py, rename _disparities.py to fairness_metrics.py, and move _input_manipulations.py to the utils module (#1202)
    * merge base metric files together
    
    * extra -> base
    
    * move _input_manipulations to utils, rename _disparities to _fairness
    
    * move _input_manipulations to utils, rename _disparities to _fairness
    
    * flake8
    
    * test fixes
    
    * fairness -> fairness_metrics, remove mean over/underprediction
    
    * remove unused files and rename test file
    
    * fix references

--- fairlearn/metrics/_base_metrics.py
+++ fairlearn/metrics/_base_metrics.py
@@ -1,18 +1,21 @@
 # Copyright (c) Microsoft Corporation and Fairlearn contributors.
 # Licensed under the MIT License.
 
-"""A variety of extra metrics useful for assessing fairness.
+"""A variety of base metrics useful for assessing fairness.
 
 These are metrics which are not part of `scikit-learn`.
 """
 
 import numpy as np
 import sklearn.metrics as skm
+from typing import Any
 
-from ._mean_predictions import mean_prediction  # noqa: F401
 from ._metric_frame import check_consistent_length
-from ._selection_rate import selection_rate  # noqa: F401,E501
+from fairlearn.utils._input_manipulations import _convert_to_ndarray_and_squeeze
 
+_EMPTY_INPUT_PREDICTIONS_ERROR_MESSAGE = (
+    "Empty y_pred passed to selection_rate function."
+)
 _TOO_MANY_UNIQUE_Y_VALS = "Must have no more than two unique y values"
 _RESTRICTED_VALS_IF_POS_LABEL_NONE = (
     "If pos_label is not specified, values must be from {0, 1} or {-1, 1}"  # noqa: E501
@@ -257,3 +260,61 @@ def count(y_true, y_pred) -> int:
     """
     check_consistent_length(y_true, y_pred)
     return len(y_true)
+
+
+def mean_prediction(y_true, y_pred, sample_weight=None) -> float:
+    r"""Calculate the (weighted) mean prediction.
+
+    The true values are ignored, but required as an argument in order
+    to maintain a consistent interface
+
+    Parameters
+    ----------
+    y_true : array_like
+        The true labels (ignored)
+
+    y_pred : array_like
+        The predicted labels
+
+    sample_weight : array_like
+        Optional array of sample weights
+    """
+    y_p = _convert_to_ndarray_and_squeeze(y_pred)
+    s_w = np.ones(len(y_p))
+    if sample_weight is not None:
+        s_w = _convert_to_ndarray_and_squeeze(sample_weight)
+
+    return np.dot(y_p, s_w) / s_w.sum()
+
+
+def selection_rate(y_true, y_pred, *, pos_label: Any = 1, sample_weight=None) -> float:
+    """Calculate the fraction of predicted labels matching the 'good' outcome.
+
+    The argument `pos_label` specifies the 'good' outcome. For consistency with
+    other metric functions, the ``y_true`` argument is required, but ignored.
+
+    Read more in the :ref:`User Guide <custom_fairness_metrics>`.
+
+    Parameters
+    ----------
+    y_true : array_like
+        The true labels (ignored)
+
+    y_pred : array_like
+        The predicted labels
+
+    pos_label : Scalar
+        The label to treat as the 'good' outcome
+
+    sample_weight : array_like
+        Optional array of sample weights
+    """
+    selected = _convert_to_ndarray_and_squeeze(y_pred) == pos_label
+    if len(selected) == 0:
+        raise ValueError(_EMPTY_INPUT_PREDICTIONS_ERROR_MESSAGE)
+
+    s_w = np.ones(len(selected))
+    if sample_weight is not None:
+        s_w = np.squeeze(np.asarray(sample_weight))
+
+    return np.dot(selected, s_w) / s_w.sum()
