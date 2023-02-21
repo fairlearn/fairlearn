81b2f8eb5d1a10e6005f3d173420c9de13161f17
Saeid Tizpaz Niari <saeid@utep.edu>
2023-02-20 21:54:03 -0700

    Update fairlearn/datasets/_fetch_acs_income.py
    Co-authored-by: Roman Lutz <romanlutz13@gmail.com>

--- fairlearn/datasets/_fetch_acs_income.py
+++ fairlearn/datasets/_fetch_acs_income.py
@@ -96,7 +96,7 @@ def fetch_acs_income(
 
     Notes
     ----------
-    We follow the convention of `sklearn.datasets.fetch_openml` to document this function.
+    We follow the convention of :func:`sklearn.datasets.fetch_openml` to document this function.
 
     References
     ----------
