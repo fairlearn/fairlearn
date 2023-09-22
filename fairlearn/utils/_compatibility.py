a0cf545763df36791ccc21681ea8c9bfd0c92328
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2023-09-22 15:24:20 -0400

    Remove one compatibility thing

--- fairlearn/utils/_compatibility.py
+++ fairlearn/utils/_compatibility.py
@@ -5,15 +5,6 @@ from sklearn.datasets import fetch_openml
 from sklearn.preprocessing import OneHotEncoder
 import inspect
 
-# In sklearn 1.2, the function fetch_openml added a new keyword
-# argument 'parser', and also changed the default behavior. To
-# maintain compatibility, we therefore set the 'parser' argument
-# to 'liac-arff', which matches the behavior before version 1.2.
-
-_PARSER_KWARG = {"parser": "liac-arff"}
-if "parser" not in inspect.getfullargspec(fetch_openml).kwonlyargs:
-    _PARSER_KWARG = {}
-
 # In sklearn 1.2, the name of the 'sparse' kwarg to OneHotEncoder
 # is being changed to 'sparse_output'.
 
