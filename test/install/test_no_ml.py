525c20efad29d03110b094d8dc84453bf39f23e8
Tamara <atanasoska.tamara@gmail.com>
2024-09-24 15:18:56 +0200

    Fix test Y dimensionality

--- test/install/test_no_ml.py
+++ test/install/test_no_ml.py
@@ -11,9 +11,10 @@ from fairlearn.adversarial._constants import _IMPORT_ERROR_MESSAGE
 def test_no_library(library):
     expected_library = "torch or tensorflow" if library is None else library
     X = [[0]]
+    Y = [0]
     with pytest.raises(RuntimeError) as exc:
         AdversarialFairnessClassifier(
             backend=library, predictor_model=[5, 5], adversary_model=[2]
-        ).fit(X, X, sensitive_features=X)
+        ).fit(X, Y, sensitive_features=Y)
 
-        assert str(exc.value) == _IMPORT_ERROR_MESSAGE.format(expected_library)
+    assert str(exc.value) == _IMPORT_ERROR_MESSAGE.format(expected_library)
