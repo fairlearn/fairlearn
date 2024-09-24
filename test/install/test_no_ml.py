b7eeed65d6c8a1d66f65d953908a9a62fa5b049d
Tamara <atanasoska.tamara@gmail.com>
2024-09-24 20:46:25 +0200

    iter

--- test/install/test_no_ml.py
+++ test/install/test_no_ml.py
@@ -11,17 +11,16 @@ from fairlearn.adversarial._constants import _IMPORT_ERROR_MESSAGE
 def test_no_library(library):
     X = [[0]]
     Y = [0]
-    try:
+    with pytest.raises(RuntimeError) as exc:
         AdversarialFairnessClassifier(
             backend=library, predictor_model=[5, 5], adversary_model=[2]
         ).fit(X, Y, sensitive_features=Y)
-    except RuntimeError as error:
-        print(error)
-        if library == "auto":
-            assert (
-                str(error) == _IMPORT_ERROR_MESSAGE.format("torch or tensorflow")
-                or _IMPORT_ERROR_MESSAGE.format("torch")
-                or _IMPORT_ERROR_MESSAGE.format("tensorflow")
-            )
-        else:
-            assert str(error) == _IMPORT_ERROR_MESSAGE.format(library)
+
+    if library == "auto":
+        assert (
+            str(exc.value) == _IMPORT_ERROR_MESSAGE.format("torch or tensorflow")
+            or _IMPORT_ERROR_MESSAGE.format("torch")
+            or _IMPORT_ERROR_MESSAGE.format("tensorflow")
+        )
+    else:
+        assert str(exc.value) == _IMPORT_ERROR_MESSAGE.format(library)
