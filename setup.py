d7d6b98150103a2122322bb8cb8c4ef96f5a2b61
Tamara <atanasoska.tamara@gmail.com>
2024-12-04 15:17:45 +0100

    MNT Fix pipi page layout  (#1444)
    * Add myself to contributors
    
    * Add myself to authors
    
    * Fix pipy html hopefully
    
    * Fix bib target errors

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     version=fairlearn.__version__,
     author=(
         "Miroslav Dudik, Richard Edgar, Adrin Jalali, Roman Lutz, Michael Madaio, Hilde"
-        " Weerts, Allie Saizan"
+        " Weerts, Allie Saizan, Tamara Atanasoska"
     ),
     author_email="fairlearn-internal@python.org",
     description="A Python package to assess and improve fairness of machine learning models.",
