f29e64cbc11fc6d06f0f940f8401f42004b5f7d5
mosalah16 <allouchemohamedsalah1609@gmail.com>
2025-02-20 22:05:53 +0100

    darkmode logo added

--- docs/conf.py
+++ docs/conf.py
@@ -128,6 +128,8 @@ html_theme = "pydata_sphinx_theme"
 html_theme_options = {
     "logo": {
         "link": "https://fairlearn.org",
+        "image_light": "_static/images/fairlearn_full_color_lightmode.svg", 
+        "image_dark": "_static/images/fairlearn_full_color_darkmode.svg",
     },
     "icon_links": [
         {
@@ -170,7 +172,7 @@ html_theme_options = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "_static/images/fairlearn_full_color.svg"
+html_logo = None
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
