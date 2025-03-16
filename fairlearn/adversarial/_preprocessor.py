56349594312a5109c096a4d7f8afb29de5baa291
Stefanie Senger <stefanie.senger@posteo.de>
2025-03-16 08:49:12 +0100

    fix typo in 'continuous'

--- fairlearn/adversarial/_preprocessor.py
+++ fairlearn/adversarial/_preprocessor.py
@@ -96,7 +96,7 @@ class FloatTransformer(BaseEstimator, TransformerMixin):
                 self.n_features_out_ = sum(
                     len(cat) if len(cat) != 2 else 1 for cat in self.transform_.categories_
                 )
-            if self.inferred_type_ in ["continous-multioutput", "multiclass-multioutput"]:
+            if self.inferred_type_ in ["continuous-multioutput", "multiclass-multioutput"]:
                 raise ValueError("Multioutput not supported")
         return self
 
