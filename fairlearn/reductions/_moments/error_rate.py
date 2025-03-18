40c5b1a4b933a401a2b8c45fcd8dc7318d86f01a
Stefanie Senger <stefanie.senger@posteo.de>
2025-03-18 13:12:58 +0100

    remove control_features

--- fairlearn/reductions/_moments/error_rate.py
+++ fairlearn/reductions/_moments/error_rate.py
@@ -61,14 +61,13 @@ class ErrorRate(ClassificationMoment):
         else:
             raise ValueError(_MESSAGE_BAD_COSTS)
 
-    def load_data(self, X, y, *, sensitive_features, control_features=None) -> None:
+    def load_data(self, X, y, *, sensitive_features) -> None:
         """Load the specified data into the object."""
         _, y_train, sf_train, _ = _validate_and_reformat_input(
             X,
             y,
             enforce_binary_labels=True,
             sensitive_features=sensitive_features,
-            control_features=control_features,
         )
         # The following uses X  so that the estimators get X untouched
         super().load_data(X, y_train, sensitive_features=sf_train)
