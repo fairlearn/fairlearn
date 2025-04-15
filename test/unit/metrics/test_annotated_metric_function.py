141dafa52c010e06304b216d768e320a7a7997cd
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2025-04-15 23:06:19 +0200

    add 2d test case

--- test/unit/metrics/test_annotated_metric_function.py
+++ test/unit/metrics/test_annotated_metric_function.py
@@ -5,7 +5,7 @@ import functools
 
 import numpy as np
 import pytest
-from sklearn.metrics import accuracy_score, recall_score
+from sklearn.metrics import accuracy_score, recall_score, roc_auc_score
 
 from fairlearn.metrics._annotated_metric_function import AnnotatedMetricFunction
 
@@ -51,5 +51,25 @@ def test_dataframe_types(constructor, kw_argument_mapping, expected):
     df_native = constructor(data)
 
     fc = AnnotatedMetricFunction(func=accuracy_score, kw_argument_mapping=kw_argument_mapping)
-
     assert fc(df_native) == expected
+
+
+def test_dataframe_types_2d_array(constructor):
+    data = {
+        "y_true": [0, 1, 2, 0, 1, 2, 2, 1],
+        "y_pred": [
+            [0.5, 0.3, 0.2],
+            [0.1, 0.5, 0.4],
+            [0.1, 0.1, 0.8],
+            [0.2, 0.5, 0.3],
+            [0.8, 0.1, 0.1],
+            [0.7, 0.2, 0.1],
+            [0.2, 0.3, 0.5],
+            [0.6, 0.1, 0.3],
+        ],
+    }
+
+    df_native = constructor(data)
+    func = functools.partial(roc_auc_score, multi_class="ovr", labels=[0, 1, 2])
+    fc = AnnotatedMetricFunction(func=func, name="rod_auc_score")
+    assert round(fc(df_native), 3) == 0.508
