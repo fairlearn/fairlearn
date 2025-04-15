19c2d3474c4c252efea916b5a39723f339daf65b
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2025-04-15 22:51:27 +0200

    unit test with multiple dataframe types as input

--- test/unit/metrics/test_annotated_metric_function.py
+++ test/unit/metrics/test_annotated_metric_function.py
@@ -4,7 +4,8 @@
 import functools
 
 import numpy as np
-from sklearn.metrics import recall_score
+import pytest
+from sklearn.metrics import accuracy_score, recall_score
 
 from fairlearn.metrics._annotated_metric_function import AnnotatedMetricFunction
 
@@ -36,3 +37,19 @@ def test_constructor_named():
     assert np.array_equal(fc.postional_argument_names, ["y_true", "y_pred"])
     assert isinstance(fc.kw_argument_mapping, dict)
     assert len(fc.kw_argument_mapping) == 0
+
+
+@pytest.mark.parametrize(
+    ("kw_argument_mapping", "expected"), [({}, 0.6), ({"sample_weight": "weight"}, 0.75)]
+)
+def test_dataframe_types(constructor, kw_argument_mapping, expected):
+    data = {
+        "y_true": [0, 1, 1, 0, 1],
+        "y_pred": [0, 0, 1, 0, 0],
+        "weight": [1.0, 0.4, 0.4, 1.0, 0.4],
+    }
+    df_native = constructor(data)
+
+    fc = AnnotatedMetricFunction(func=accuracy_score, kw_argument_mapping=kw_argument_mapping)
+
+    assert fc(df_native) == expected
